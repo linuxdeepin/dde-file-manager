bfba6a25b8c5ec54b5ebe7c769f7f4dd202b0638
zccrs <ccrr1314@live.com>
2016-03-04 09:43:21 +0800

    Change FileItem to draw
    use QPainter draw icon and text
    
    Change-Id: I884d0fd48b45971ae23b45cca12dbe40ef57f75a

--- filemanager/app/global.h
+++ filemanager/app/global.h
@@ -1,12 +1,15 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
-#include <QGlobalStatic>
 #include "widgets/singleton.h"
 #include "signalmanager.h"
 #include "../controllers/dbuscontroller.h"
 #include "utils/utils.h"
 #include "filesignalmanager.h"
+
+#include <QFontMetrics>
+#include <QTextOption>
+#include <QTextLayout>
 
 #define gridManager  Singleton<GridManager>::instance()
 #define signalManager  Singleton<SignalManager>::instance()
@@ -43,5 +46,85 @@
     timer->start(Time);\
 }
 #endif
+
+class Global
+{
+public:
+    static QString wordWrapText(const QString &text, int width,
+                         const QFontMetrics &fontMetrics,
+                         QTextOption::WrapMode wrapMode,
+                         int *height = 0)
+    {
+        QTextLayout textLayout(text);
+        QTextOption &text_option = *const_cast<QTextOption*>(&textLayout.textOption());
+        text_option.setWrapMode(wrapMode);
+
+        textLayout.beginLayout();
+
+        QTextLine line = textLayout.createLine();
+        QString  str;
+
+        int text_height = 0;
+
+        while (line.isValid()) {
+            line.setLineWidth(width);
+            str += text.mid(line.textStart(), line.textLength());
+            text_height += (fontMetrics.leading() + fontMetrics.height());
+            line = textLayout.createLine();
+
+            if(line.isValid())
+                str.append("\n");
+        }
+
+        textLayout.endLayout();
+
+        if(height)
+            *height = text_height;
+
+        return str;
+    }
+
+    static QString elideText(const QString &text, const QSize &size,
+                      const QFontMetrics &fontMetrics,
+                      QTextOption::WrapMode wordWrap,
+                      Qt::TextElideMode mode,
+                      int flags = 0)
+    {
+        qreal height = 0;
+        int lineHeight = fontMetrics.height();
+        int interLine = fontMetrics.leading();
+
+        QTextLayout textLayout(text);
+        QString str;
+
+        const_cast<QTextOption*>(&textLayout.textOption())->setWrapMode(wordWrap);
+
+        textLayout.beginLayout();
+
+        QTextLine line = textLayout.createLine();
+
+        while (line.isValid()) {
+            height += (interLine + lineHeight);
+
+            if(height + interLine + lineHeight >= size.height()) {
+                str += fontMetrics.elidedText(text.mid(line.textStart() + line.textLength() + 1),
+                                              mode, size.width(), flags);
+                break;
+            }
+
+            line.setLineWidth(size.width());
+            str += text.mid(line.textStart(), line.textLength());
+
+            line = textLayout.createLine();
+
+            if(line.isValid())
+                str.append("\n");
+        }
+
+        textLayout.endLayout();
+
+        return str;
+    }
+};
 
 #endif // GLOBAL_H
