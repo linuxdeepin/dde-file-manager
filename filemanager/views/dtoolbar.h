5ee560c35297766db56eb88cfd3efd933598bc94
wang zhe <wzedmund@gmail.com>
2016-03-04 17:05:46 +0800

    add searchbar, remove widgets/utils
    Change-Id: I11b14240f1a153e2f473ce7e23e6e8a4b3956668

--- filemanager/views/dtoolbar.h
+++ filemanager/views/dtoolbar.h
@@ -2,10 +2,13 @@
 #define DTOOLBAR_H
 
 #include <QFrame>
+#include <QStackedWidget>
 #include "dstatebutton.h"
 
 class DIconTextButton;
 class DStateButton;
+class DSearchBar;
+class DTabBar;
 
 class DToolBar : public QFrame
 {
@@ -29,11 +32,15 @@ signals:
 
 public slots:
     void setLayoutButtonState(DStateButton::ButtonState state);
-
+    void searchBarSwitched();
+    void searchBarTextEntered();
+    void tabBarClicked(int index);
+    void upButtonClicked();
 private:
     QFrame* m_addressToolBar;
     DStateButton* m_backButton=NULL;
     DStateButton* m_forwardButton=NULL;
+    DStateButton* m_upButton=NULL;
     DStateButton* m_searchButton = NULL;
     DStateButton* m_refreshButton = NULL;
     QFrame* m_contollerToolBar;
@@ -43,6 +50,10 @@ private:
     DStateButton* m_sortButton=NULL;
     DStateButton* m_hideShowButton=NULL;
     DStateButton* m_viewSwitchButton=NULL;
+    DSearchBar * m_searchBar = NULL;
+    DTabBar * m_tabBar = NULL;
+    QStackedWidget * m_stackedWidget = NULL;
+    int m_switchState = 0;
 };
 
 #endif // DTOOLBAR_H
--- widgets/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-#include <QtCore>
-
-QString getQssFromFile(QString filename);
-QString joinPath(const QString& path, const QString& fileName);
-#endif // UTIL_H
-
