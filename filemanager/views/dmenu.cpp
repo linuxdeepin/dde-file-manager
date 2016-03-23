b013d639294d52766ba12e6bd28dfb5c4a52b333
dragondjf <ding465398889@163.com>
2016-03-23 17:31:34 +0800

    change menu style
    Change-Id: I422a0b7bbb48b809deb6acc5163afb4531a51161

--- filemanager/views/dmenu.cpp
+++ filemanager/views/dmenu.cpp
@@ -1,5 +1,6 @@
 #include "dmenu.h"
 #include <QPainter>
+#include <QGraphicsDropShadowEffect>
 
 DMenu::DMenu(QWidget *parent)
     :QMenu(parent)
@@ -11,27 +12,32 @@ DMenu::DMenu(QWidget *parent)
         color: black;\
         border-radius: 4px;\
         border-style: inset; \
-        border: 1px solid lightGray;\
-        padding-top: 10px;\
+        border: 1px solid lightgray;\
+        padding-top: 5px;\
         padding-left: 0px;\
         padding-right: 0px;\
-        padding-bottom: 10px;\
+        padding-bottom: 5px;\
     }\
     QMenu::item{\
-        padding-top: 4px;\
-        padding-left: 25px;\
-        padding-right: 25px;\
-        padding-bottom: 4px;\
+        padding: 6px 25px 6px 25px;\
         font: 13px;\
     }\
     QMenu::item:hover{\
-      background: black;\
+      background: #636363;\
+      color:white;\
+    }\
+    QMenu::item:disabled{\
+      color: gray;\
     }\
     QMenu::separator{\
+        height: 6px;\
+        margin-left: 0px;\
+        margin-right: 0px;\
         padding-left: -5px;\
         padding-right: -5px;\
     }\
     QMenu::item:selected {\
         background: #646464;\
+        color:white;\
     }");
 }
