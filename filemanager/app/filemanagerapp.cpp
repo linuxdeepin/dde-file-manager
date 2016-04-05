8d4fa325c8e53caa94f61763cf2c45e89843e360
dragondjf <ding465398889@163.com>
2016-04-05 18:16:40 +0800

    Add fileIO task dialog
    Change-Id: Ibb8827ff0b66fb84fa49fc3076f4e4bf70912bb9

--- filemanager/app/filemanagerapp.cpp
+++ filemanager/app/filemanagerapp.cpp
@@ -5,6 +5,7 @@
 #include "../controllers/appcontroller.h"
 #include "../app/define.h"
 #include "../app/global.h"
+#include "dialogs/dtaskdialog.h"
 
 
 FileManagerApp::FileManagerApp(QObject *parent) : QObject(parent)
@@ -12,10 +13,18 @@ FileManagerApp::FileManagerApp(QObject *parent) : QObject(parent)
     initLogger();
     initApp();
     initView();
+    initTaskDialog();
     initController();
     initCommandline();
     initGtk();
     initConnect();
+
+    for(int i=0; i<10; i++){
+        QMap<QString, QString> jobDetail;
+        jobDetail.insert("jobPath", QString("%1").arg(i));
+        jobDetail.insert("type", "delete");
+        emit fileSignalManager->jobAdded(jobDetail);
+    }
 }
 
 FileManagerApp::~FileManagerApp()
@@ -46,11 +55,15 @@ void FileManagerApp::initApp()
 void FileManagerApp::initView()
 {
     m_windowManager = new WindowManager;
-//#if DEEPIN_UI_ENABLE
-//    m_dFileMangerWindow = new DFileManagerWindow;
-//#else
-//    m_fileManagerWindow = new FileManagerWindow;
-//#endif
+}
+
+void FileManagerApp::initTaskDialog()
+{
+    m_taskDialog = new DTaskDialog;
+    m_timer = new QTimer;
+    m_timer->setInterval(1000);
+    connect(m_timer, SIGNAL(timeout()), this, SLOT(handleDataUpdated()));
+    m_timer->start();
 }
 
 void FileManagerApp::initController()
@@ -77,7 +90,10 @@ void FileManagerApp::initTranslation()
 
 void FileManagerApp::initConnect()
 {
-
+    connect(fileSignalManager, &FileSignalManager::jobAdded, m_taskDialog, &DTaskDialog::addTask);
+    connect(fileSignalManager, &FileSignalManager::jobRemoved, m_taskDialog, &DTaskDialog::removeTask);
+    connect(fileSignalManager, &FileSignalManager::jobDataUpdated, m_taskDialog, &DTaskDialog::handleUpdateTaskWidget);
+    connect(m_taskDialog, &DTaskDialog::abortTask, fileSignalManager, &FileSignalManager::abortTask);
 }
 
 AppController *FileManagerApp::getAppController() const
@@ -89,4 +105,24 @@ void FileManagerApp::show()
 {
     m_windowManager->showNewWindow();
 }
+
+void FileManagerApp::handleDataUpdated()
+{
+
+    for(int i=0; i<10; i++){
+
+        QMap<QString, QString> jobDetail;
+        jobDetail.insert("jobPath", QString("%1").arg(i));
+        jobDetail.insert("type", "copy");
+
+        QMap<QString, QString> jobDataDetail;
+        jobDataDetail.insert("speed", "1M/s");
+        jobDataDetail.insert("remainTime", QString("%1 s").arg(QString::number(10)));
+        jobDataDetail.insert("file", "111111111111");
+        jobDataDetail.insert("progress", "20");
+        jobDataDetail.insert("destination", "home");
+        emit fileSignalManager->jobDataUpdated(jobDetail, jobDataDetail);
+    }
+}
+
 
