df077854d87df3ca3da7f23007c23679a8974182
dragondjf <ding465398889@163.com>
2016-04-29 15:59:41 +0800

    Refactor toplevel MainWindow
    Change-Id: I1763e89d84937af5205fcb627d08c7d8711f0d05

--- filemanager/views/dmovablemainwindow.cpp
+++ filemanager/views/dmovablemainwindow.cpp
@@ -7,9 +7,7 @@
 
 DMovableMainWindow::DMovableMainWindow(QWidget *parent) : QMainWindow(parent)
 {
-    setAttribute(Qt::WA_Hover, true);
-    installEventFilter(this);
-    connect(this, SIGNAL(startMoving()), parentWidget(), SLOT(startMoving()));
+
 }
 
 DMovableMainWindow::~DMovableMainWindow()
@@ -43,85 +41,15 @@ void DMovableMainWindow::setDragMovableRect(const QRect &rect)
     m_dragMovableHeight = rect.height();
 }
 
-void DMovableMainWindow::moveCenter(){
-    QRect qr = frameGeometry();
-    QPoint cp;
-    cp = qApp->desktop()->availableGeometry().center();
-    qr.moveCenter(cp);
-    move(qr.topLeft());
-}
-
-void DMovableMainWindow::moveTopRight(){
-    QRect pRect;
-    pRect = qApp->desktop()->availableGeometry();
-    int x = pRect.width() - width();
-    move(QPoint(x, 0));
-}
-
-void DMovableMainWindow::moveTopRightByRect(QRect rect){
-    int x = rect.x() + rect.width() - width();
-    move(QPoint(x, 0));
-}
-
-void DMovableMainWindow::moveCenterByRect(QRect rect){
-    QRect qr = frameGeometry();
-    qr.moveCenter(rect.center());
-    move(qr.topLeft());
-}
-
-void DMovableMainWindow::mousePressEvent(QMouseEvent *event)
-{
-//    if (event->button() == Qt::LeftButton){
-//        m_pressedPosition = event->pos();
-//        if (m_dragMovableRect.contains(event->pos())){
-//            setCursor(QCursor(Qt::SizeAllCursor));
-//        }
-//    }else{
-//        m_pressedPosition = QPoint(0, 0);
-//    }
-    QMainWindow::mousePressEvent(event);
-}
-
-void DMovableMainWindow::mouseReleaseEvent(QMouseEvent *event)
-{
-//    if (event->button() == Qt::LeftButton){
-//        if (m_dragMovableRect.contains(event->pos())){
-//            setCursor(QCursor(Qt::ArrowCursor));
-//        }
-//    }
-    QMainWindow::mouseReleaseEvent(event);
-}
-
 void DMovableMainWindow::mouseMoveEvent(QMouseEvent *event)
 {
     if(isActiveWindow()){
         if (m_dragMovableRect.contains(event->pos())){
-            emit startMoving();
-//            QtX11::utils::MoveWindow(this->parentWidget(), event, m_dragMovableRect);
-        }else{
-//            QtX11::utils::ResizeWindow(this, event, m_borderCornerSize);
+            emit mouseMoved();
         }
     }
     QMainWindow::mouseMoveEvent(event);
 }
 
-void DMovableMainWindow::resizeEvent(QResizeEvent *event){
-//    setDragMovableRect(QRect(m_borderCornerSize, m_borderCornerSize,  width() - 2 * m_borderCornerSize, m_dragMovableRect.height()));
-    QMainWindow::resizeEvent(event);
-}
 
-bool DMovableMainWindow::eventFilter(QObject *obj, QEvent *event)
-{
-//    if (event->type() == QEvent::HoverMove){
-//        QRect availableRect = getAvailableRect();
-//        QPoint currentPos = mapFromGlobal(QCursor::pos());
-//        if (availableRect.contains(currentPos)){
-//            QtX11::utils::setNormalCursor(this);
-//        }else{
-//            QtX11::utils::UpdateCursorShape(this, currentPos.x(), currentPos.y(), m_borderCornerSize);
-//        }
-//    }else if (event->type() == QEvent::HoverLeave){
-//        QtX11::utils::setNormalCursor(this);
-//    }
-    return QMainWindow::eventFilter(obj, event);
-}
+
