2cdc2c7e1db0d55a1a38c3ab086dd13bd17acc96
dragondjf <ding465398889@163.com>
2016-05-11 18:42:21 +0800

    Add diskmount dbus interface
    Change-Id: Ia7759c1f01a1bcccb2533f97dbf532385e6a9769

---
+++ dbusinterface/dbustype.h
@@ -0,0 +1,45 @@
+#ifndef DBUSTYPE_H
+#define DBUSTYPE_H
+
+#include <QtCore>
+#include <QtDBus>
+
+
+class  DiskInfo
+{
+
+public:
+    QString ID;
+    QString Name;
+    QString Type;
+    QString Path;
+    QString MountPoint;
+    QString Icon;
+    bool CanEject;
+    bool CanUnmount;
+    qulonglong Used;
+    qulonglong Total;
+
+    DiskInfo();
+    ~DiskInfo();
+
+    friend QDBusArgument &operator<<(QDBusArgument &argument, const DiskInfo &obj);
+
+    friend const QDBusArgument &operator>>(const QDBusArgument &argument, DiskInfo &obj);
+
+    static void registerMetaType();
+
+
+};
+
+
+typedef QList<DiskInfo> DiskInfoList;
+
+Q_DECLARE_METATYPE(DiskInfo)
+Q_DECLARE_METATYPE(DiskInfoList)
+
+
+QDebug operator<<(QDebug dbg, const DiskInfo &info);
+
+#endif // DBUSTYPE_H
+
