838244a7a6fcecd7e208218098f95e1ee96473d6
zccrs <ccrr1314@live.com>
2016-06-29 11:18:51 +0800

    Finishing code
    Change-Id: I5804d78e53babdc62e8e79c5d3f10a832f9f0912

--- chinese2pinyin/chinese2pinyin.cpp
+++ chinese2pinyin/chinese2pinyin.cpp
@@ -10,46 +10,55 @@
 
 namespace Pinyin {
 
-static QHash<uint32_t, QString> dict = {
-};
+static QHash<uint, QString> dict = {};
 
 const char kDictFile[] = ":/misc/pinyin.dict";
 
 void InitDict() {
-  if (dict.size() != 0) {
-    return;
-  }
-
-  QFile file(kDictFile);
-
-  if (!file.open(QIODevice::ReadOnly))
-      return;
-
-  QByteArray content = file.readAll();
-  QTextStream stream(&content, QIODevice::ReadOnly);
-  while (!stream.atEnd()) {
-    const QString line = stream.readLine();
-    const QStringList items = line.split(QChar(':'));
-    if (items.size() == 2) {
-      dict.insert(static_cast<uint32_t>(items[0].toInt(nullptr, 16)), items[1]);
+    if (!dict.isEmpty()) {
+        return;
     }
-  }
+
+    dict.reserve(25333);
+
+    QFile file(kDictFile);
+
+    if (!file.open(QIODevice::ReadOnly))
+        return;
+
+    QByteArray content = file.readAll();
+
+    file.close();
+
+    QTextStream stream(&content, QIODevice::ReadOnly);
+
+    while (!stream.atEnd()) {
+        const QString line = stream.readLine();
+        const QStringList items = line.split(QChar(':'));
+
+        if (items.size() == 2) {
+            dict.insert(items[0].toInt(nullptr, 16), items[1]);
+        }
+    }
 }
 
 QString Chinese2Pinyin(const QString& words) {
-  InitDict();
+    InitDict();
 
-  QString result;
-  for (int i = 0; i < words.length(); ++i) {
-    const uint32_t key = static_cast<uint32_t>(words.at(i).unicode());
-    auto find_result = dict.find(key);
-    if (find_result != dict.end()) {
-      result.append(find_result.value());
-    } else {
-      result.append(words.at(i));
+    QString result;
+
+    for (int i = 0; i < words.length(); ++i) {
+        const uint key = words.at(i).unicode();
+        auto find_result = dict.find(key);
+
+        if (find_result != dict.end()) {
+            result.append(find_result.value());
+        } else {
+            result.append(words.at(i));
+        }
     }
-  }
-  return result;
+
+    return result;
 }
 
 }  // namespace Pinyin end
