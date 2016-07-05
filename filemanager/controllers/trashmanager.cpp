52ec9b001b09f3ab1197fa77c4aede82fdf64cca
zccrs <ccrr1314@live.com>
2016-07-05 18:14:08 +0800

    Fix clear trash failed
    Change-Id: I2047fbe95721e52db2ecbc4dc097c9a8dedaafc3

--- filemanager/controllers/trashmanager.cpp
+++ filemanager/controllers/trashmanager.cpp
@@ -101,16 +101,23 @@ const QList<AbstractFileInfoPointer> TrashManager::getChildren(const DUrl &fileU
 
     const QString &path = fileUrl.path();
 
-    QDir dir(TRASHFILEPATH + path);
+    QDir dir(TRASHINFOPATH + path);
     QList<AbstractFileInfoPointer> infoList;
 
     if(dir.exists()) {
         QFileInfoList fileInfoList = dir.entryInfoList(filter | QDir::NoDotAndDotDot | QDir::System | QDir::Hidden);
 
         for(const QFileInfo fileInfo : fileInfoList) {
-            const DUrl &fileUrl = DUrl::fromTrashFile(fileInfo.absoluteFilePath().mid((TRASHFILEPATH).size()));
+            QString fileBaseName = fileInfo.absoluteFilePath().mid((TRASHINFOPATH).size());
 
-            infoList.append(AbstractFileInfoPointer(new TrashFileInfo(fileUrl)));
+            if (fileBaseName.endsWith(".trashinfo"))
+                fileBaseName.chop(10);
+
+            const DUrl &fileUrl = DUrl::fromTrashFile(fileBaseName);
+            const AbstractFileInfoPointer trashInfo(new TrashFileInfo(fileUrl));
+
+            if (trashInfo->exists() || trashInfo->isSymLink())
+                infoList.append(trashInfo);
         }
     }
 
