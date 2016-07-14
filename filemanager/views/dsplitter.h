9c68ea1a4bccd520bfe71e941ec1cdb0d60ace2a
dragondjf <ding465398889@163.com>
2016-07-14 16:00:29 +0800

    Disable qspliter handle style
    Change-Id: I805996cac8258eab2b4dd49b40c02d67d06a2692

--- filemanager/views/dsplitter.h
+++ filemanager/views/dsplitter.h
@@ -2,16 +2,31 @@
 #define DSPLITTER_H
 
 #include <QSplitter>
+#include <QSplitterHandle>
+
+
+class SplitterHandle: public QSplitterHandle
+{
+    Q_OBJECT
+
+public:
+    explicit SplitterHandle(Qt::Orientation orientation, QSplitter* parent);
+
+protected:
+    void enterEvent(QEvent* e);
+};
+
 
 class DSplitter : public QSplitter
 {
     Q_OBJECT
+
 public:
     DSplitter(Qt::Orientation orientation, QWidget* parent = 0);
     void moveSplitter(int pos, int index);
+
 protected:
-    void mousePressEvent(QMouseEvent *e);
-    void mouseReleaseEvent(QMouseEvent *e);
+    QSplitterHandle* createHandle();
 };
 
 #endif // DSPLITTER_H
