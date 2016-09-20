9b9903a5d39b76fb5edd406a7fc85cee9ccab9b2
tommego <licaike@deepin.com>
2016-09-20 15:05:07 +0800

    Upgrade thumbnailer standard functions and fix fail to thumbnail an invalid pdf file causing crash problem
    Change-Id: Ib73df71d05ea10ffe9f4427312a064696817e773

--- thumbnailer/thumbnailmanager.cpp
+++ thumbnailer/thumbnailmanager.cpp
@@ -9,10 +9,8 @@
 #include <QImageWriter>
 #include <QDateTime>
 #include <QUrl>
-
-#include "app/global.h"
-#include "widgets/singleton.h"
-#include "controllers/fileservices.h"
+#include <QFileInfo>
+#include <QFile>
 
 ThumbnailManager::ThumbnailManager(QObject *parent)
     : QThread(parent)
@@ -53,7 +51,7 @@ void ThumbnailManager::initConnections()
     connect(m_watcher, &QFileSystemWatcher::fileChanged, this, &ThumbnailManager::onFileChanged);
 }
 
-QString ThumbnailManager::getThumbnailCachePath()
+QString ThumbnailManager::getThumbnailCachePath() const
 {
     QStringList systemEnvs = QProcess::systemEnvironment();
     foreach (QString it, systemEnvs) {
@@ -67,7 +65,7 @@ QString ThumbnailManager::getThumbnailCachePath()
     return cachePath;
 }
 
-QString ThumbnailManager::getThumbnailPath(const QString &name, ThumbnailGenerator::ThumbnailSize size)
+QString ThumbnailManager::getThumbnailPath(const QString &name, ThumbnailGenerator::ThumbnailSize size) const
 {
     if(size == ThumbnailGenerator::THUMBNAIL_NORMAL)
         return QString("%1/%2.png").arg(m_thumbnailNormalPath,name);
@@ -77,7 +75,12 @@ QString ThumbnailManager::getThumbnailPath(const QString &name, ThumbnailGenerat
         return m_thumbnailPath;
 }
 
-QIcon ThumbnailManager::getThumbnailIcon(const QUrl& fileUrl)
+QString ThumbnailManager::getThumbnailFailPath(const QString &name) const
+{
+    return QString("%1/%2.png").arg(m_thumbnailFailPath,name);
+}
+
+QIcon ThumbnailManager::getThumbnailIcon(const QUrl& fileUrl, ThumbnailGenerator::ThumbnailSize size)
 {
     QString fpath = fileUrl.path();
 
@@ -90,10 +93,29 @@ QIcon ThumbnailManager::getThumbnailIcon(const QUrl& fileUrl)
         }
     }
 
+
     // check last modify time
     if(m_md5ToIcon.contains(m_pathToMd5.value(fileUrl.toString()))&&fileUrl.isLocalFile()){
 
-        QString thumbnailPath = QString("%1").arg(getThumbnailPath(m_pathToMd5.value(fileUrl.toString()), ThumbnailGenerator::THUMBNAIL_LARGE));
+        // first of all , check fail thumbnail path
+        QString md5 = toMd5(fileUrl.toString());
+        QString thumbnailFailPath = QString("%1").arg(getThumbnailFailPath(md5));
+        QFileInfo failedFInfo(thumbnailFailPath);
+        if(failedFInfo.exists()){
+            QImage failedImg(thumbnailFailPath);
+            if(failedImg.text("Thumb::MTime")!=QString::number(failedFInfo.lastModified().toMSecsSinceEpoch()/1000)||
+                    failedImg.text("Thumb::Size")!= QString::number(QFile(thumbnailFailPath).size())){
+                m_md5ToIcon.take(m_pathToMd5.value(fileUrl.toString()));
+                m_pathToMd5.take(fileUrl.toString());
+                QFile::remove(thumbnailFailPath);
+                return QIcon();
+            }
+            else
+                return getDefaultIcon(fileUrl);
+        }
+
+        // check thumbnail path(normal or large)
+        QString thumbnailPath = QString("%1").arg(getThumbnailPath(m_pathToMd5.value(fileUrl.toString()), size));
         QImage reader(thumbnailPath);
         QString dateStr = reader.text("Thumb::MTime");
         QFileInfo info(fpath);
@@ -107,21 +129,29 @@ QIcon ThumbnailManager::getThumbnailIcon(const QUrl& fileUrl)
         return m_md5ToIcon.value(m_pathToMd5.value(fileUrl.toString()));
     }
 
-    return m_md5ToIcon.value(m_pathToMd5.value(fileUrl.toString()));
+    return QIcon();
 }
 
-void ThumbnailManager::requestThumbnailIcon(const QUrl& fileUrl)
+void ThumbnailManager::requestThumbnailIcon(const QUrl& fileUrl, ThumbnailGenerator::ThumbnailSize size)
 {
+    ThumbnailTask task(fileUrl,size);
+
     if (m_pathToMd5.contains(fileUrl.toString()))
         return;
 
-    if (taskQueue.contains(fileUrl.toString()))
-        return;
+//    if (taskQueue.contains(task))
+//        return;
 
-    taskQueue << fileUrl.toString();
+    taskQueue << task;
 
     if (!isRunning())
         start();
+}
+
+QIcon ThumbnailManager::getDefaultIcon(const QUrl &fileUrl)
+{
+    //TODO
+    return QIcon();
 }
 
 bool ThumbnailManager::canGenerateThumbnail(const QUrl& fileUrl)
@@ -148,17 +178,17 @@ void ThumbnailManager::onFileChanged(const QString &path)
 void ThumbnailManager::run()
 {
     while (!taskQueue.isEmpty()) {
-        const QString &fileUrl = taskQueue.dequeue();
+//        const QString &fileUrl = taskQueue.dequeue();
+        const ThumbnailTask task = taskQueue.dequeue();
 
-        QUrl url(fileUrl);
-        QString fpath = url.path();
+        QString fpath = task.fileUrl.path();
 
-        if(url.isLocalFile())
+        if(task.fileUrl.isLocalFile())
             m_watcher->addPath(fpath);
 
-        const QString &md5 = toMd5(fileUrl);
+        const QString &md5 = toMd5(task.fileUrl.toString());
 
-        m_pathToMd5[fileUrl] = md5;
+        m_pathToMd5[task.fileUrl.toString()] = md5;
 
         QIcon icon = m_md5ToIcon.value(md5);
 
@@ -168,28 +198,48 @@ void ThumbnailManager::run()
             continue;
         };
 
-        QString thumbnailPath = QString("%1").arg(getThumbnailPath(md5, ThumbnailGenerator::THUMBNAIL_LARGE));
+        QString thumbnailPath = QString("%1").arg(getThumbnailPath(md5, task.size));
 
         if (QFile(thumbnailPath).exists()) {
             icon = QIcon(thumbnailPath);
 
             m_md5ToIcon[md5] = icon;
         }
-        else if(m_thumbnailGenerator.canGenerateThumbnail(url)){
-            const QPixmap &pixmap = m_thumbnailGenerator.generateThumbnail(url, ThumbnailGenerator::THUMBNAIL_LARGE);
+        else if(m_thumbnailGenerator.canGenerateThumbnail(task.fileUrl)){
+            const QPixmap &pixmap = m_thumbnailGenerator.generateThumbnail(task.fileUrl, task.size);
+
+            if(pixmap.isNull()){
+                // deal with fail thumbnailing icon
+                QString thumbnailFailedPath = getThumbnailFailPath(md5);
+                QImage img(1,1,QImage::Format_ARGB32_Premultiplied);
+
+                QMap<QString,QString> attributeSet = m_thumbnailGenerator.getAttributeSet(task.fileUrl);
+                QList<QString> keys = attributeSet.keys();
+
+                foreach (QString key, keys) {
+                    img.setText(key,attributeSet.value(key));
+                }
+                m_md5ToIcon[md5] = QIcon();
+
+                qDebug()<<"save failed thumbnail:"<<img.save(thumbnailFailedPath,"png")<<thumbnailFailedPath<<","<<task.fileUrl;
+
+                //do not emit icon changed, otherwise it will turn to a death loops
+                break;
+            }
+
             icon = QIcon(pixmap);
             m_md5ToIcon[md5] = icon;
             QImage img  = pixmap.toImage();
 
             //write extra attribute messages
-            QMap<QString,QString> attributeSet = m_thumbnailGenerator.getAttributeSet(fileUrl);
+            QMap<QString,QString> attributeSet = m_thumbnailGenerator.getAttributeSet(task.fileUrl);
             QList<QString> keys = attributeSet.keys();
 
             foreach (QString key, keys) {
                 img.setText(key,attributeSet.value(key));
             }
 
-            img.save(thumbnailPath,"png",20);
+            qDebug()<<"save thumbnail:"<<img.save(thumbnailPath,"png",20)<<thumbnailPath<<","<<task.fileUrl;
         }
         emit iconChanged(fpath, icon);
     }
