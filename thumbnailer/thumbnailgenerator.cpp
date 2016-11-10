40b57e1fe3f8c0781121625f1d2a46653fc1db89
tommego <licaike@deepin.com>
2016-11-10 15:23:38 +0800

    Fix poppler crash problem
    Change-Id: I8e6f37b0cf8d4b2729710370d2530988f5559809

--- thumbnailer/thumbnailgenerator.cpp
+++ thumbnailer/thumbnailgenerator.cpp
@@ -10,9 +10,17 @@
 #include <QFileInfo>
 #include <QUrl>
 #include <QPainter>
+#include <QDateTime>
+#include <QThread>
 
 // poppler for pdf
-#include <poppler/qt5/poppler-qt5.h>
+//#include <poppler/qt5/poppler-qt5.h>
+
+// use original poppler api
+#include <poppler-document.h>
+#include <poppler-image.h>
+#include <poppler-page.h>
+#include <poppler-page-renderer.h>
 
 // ffmpeg
 #include "libffmpegthumbnailer/videothumbnailer.h"
@@ -125,14 +133,14 @@ QMap<QString,QString> ThumbnailGenerator::getAttributeSet(const QUrl&  fileUrl)
 
         if(isPDFFile(fileName)){
             // handle fail thumbnailing files
-            Poppler::Document* document = Poppler::Document::load(fileName);
-            if((!document || document->isLocked())){
-                delete document;
+            auto doc = poppler::document::load_from_file(fileName.toStdString());
+            if((!doc || doc->is_locked())){
+                delete doc;
                 return set;
             }
 
-            set.insert("Thumb::Document::Pages",QString::number(document->numPages()));
-            delete document;
+            set.insert("Thumb::Document::Pages",QString::number(doc->pages()));
+            delete doc;
             return set;
         }
 
@@ -200,36 +208,64 @@ QPixmap ThumbnailGenerator::getTextplainThumbnail(const QString &fpath, const Th
 
 QPixmap ThumbnailGenerator::getPDFThumbnail(const QString &fpath, const ThumbnailGenerator::ThumbnailSize &size)
 {
-    Poppler::Document* document = Poppler::Document::load(fpath);
-    if((!document || document->isLocked())){
-        delete document;
-        qDebug()<<"file reading error...."<<fpath;
+    poppler::document *doc = poppler::document::load_from_file(fpath.toStdString());
+    if(!doc || doc->is_locked()){
+        qDebug () << "Cannot read this pdf file:" << fpath;
         return QPixmap();
     }
-    qDebug()<<"pdf document read successfully!";
-
-    if(document == 0){
-        qDebug()<<"pdf page is null";
+    if(doc->pages()<1){
+        qDebug () << "This stream is invalid";
         return QPixmap();
     }
 
-    Poppler::Page* pdfPage = document->page(0); //Document start at page 0
+    const poppler::page *page = doc->create_page(0);
 
+    if(!page){
+        qDebug () << "Cannot get this page at index 0";
+        return QPixmap();
+    }
+
+    poppler::page_renderer pr;
+    pr.set_render_hint(poppler::page_renderer::antialiasing, true);
+    pr.set_render_hint(poppler::page_renderer::text_antialiasing, true);
+
+    poppler::image imageData = pr.render_page(page);
+
+    if(!imageData.is_valid()){
+        qDebug () << "Render error";
+        return QPixmap();
+    }
+
+    poppler::image::format_enum format = imageData.format();
     QImage img;
-    img = pdfPage->thumbnail();
-    //It has a inner bug for using this function
-//    if(img.isNull()){
-//        img = pdfPage->renderToImage(72,72,0,0,pdfPage->pageSize().width(),pdfPage->pageSize().height());
-//    }
+
+    switch (format) {
+    case poppler::image::format_invalid:
+        qDebug () << "Image format is invalid";
+        break;
+    case poppler::image::format_mono:
+        qDebug () << "Image format is mono";
+        img = QImage((uchar*)imageData.data(),imageData.width(),imageData.height(),QImage::Format_Mono);
+        break;
+    case poppler::image::format_rgb24:
+        qDebug () << "Image format is argb24";
+        img = QImage((uchar*)imageData.data(),imageData.width(),imageData.height(),QImage::Format_ARGB6666_Premultiplied);
+        break;
+    case poppler::image::format_argb32:
+        qDebug () << "Image format is argb32";
+        img = QImage((uchar*)imageData.data(),imageData.width(),imageData.height(),QImage::Format_ARGB32);
+        break;
+    default:
+        break;
+    }
+
     if(img.isNull()){
         qDebug()<<"render error";
         return QPixmap();
     }
 
     img = img.scaled(QSize(size,size),Qt::KeepAspectRatio,Qt::SmoothTransformation);
-    delete pdfPage;
     return QPixmap::fromImage(img);
-
 }
 
 QPixmap ThumbnailGenerator::getVideoThumbnail(const QString &fpath, const ThumbnailGenerator::ThumbnailSize &size)
