39eb516317f962e71aac8f49f0ea99d498c21b09
tommego <licaike@deepin.com>
2016-12-22 10:49:15 +0800

    Upgrade launch sh file including dde-property-dialog, dde-file-manager.sh, dde-computer.sh,dde-trash.sh for checking if is dde-filemanger process existed whether use fast boot plan
    Change-Id: Ic4269ad601263c7fb0e32649365df7adf8c2225e

--- dde-file-manager/mips/dde-trash.sh
+++ dde-file-manager/mips/dde-trash.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 process=`ps ax -o 'cmd' |grep 'dde-file-manager$'`;
+processd=`ps ax -o 'cmd' |grep 'dde-file-manager -d$'`;
   
-if [ "$process" == "" ]; then
+if [[ "$process" == ""&&"$processd" == "" ]]; then
     dde-file-manager trash:///
 else
     echo "{\"paths\":[\"trash:///\"]}" |socat - $XDG_RUNTIME_DIR/dde-file-manager
