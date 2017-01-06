075bc0d7581534c51496363c00c4e4ca0b04ea2e
tommego <licaike@deepin.com>
2017-01-06 09:53:49 +0800

    Add formatter worker
    Change-Id: Ib9dd2a3687284a30accec9bd42bfdc3690a61f8b

--- usb-device-formatter/formatingpage.cpp
+++ usb-device-formatter/formatingpage.cpp
@@ -3,7 +3,6 @@
 #include <QIcon>
 #include <QVBoxLayout>
 #include <QLabel>
-#include "widgets/progressbox.h"
 
 FormatingPage::FormatingPage(QWidget *parent) : QFrame(parent)
 {
@@ -13,18 +12,29 @@ FormatingPage::FormatingPage(QWidget *parent) : QFrame(parent)
 void FormatingPage::initUI()
 {
     QVBoxLayout* mainLayout = new QVBoxLayout;
-    ProgressBox* progressBox = new ProgressBox(this);
-    progressBox->setFixedSize(100, 100);
+    m_progressBox = new ProgressBox(this);
+    m_progressBox->setFixedSize(100, 100);
     QLabel* formatingLabel = new QLabel(this);
     QString formatingText = tr("Formating,please wait for a minute...");
     formatingLabel->setText(formatingText);
 
-    connect(progressBox, &ProgressBox::finished, this, &FormatingPage::finished);
+    connect(m_progressBox, &ProgressBox::finished, this, &FormatingPage::finished);
 
     mainLayout->addStretch(1);
-    mainLayout->addWidget(progressBox, 0 , Qt::AlignHCenter);
+    mainLayout->addWidget(m_progressBox, 0 , Qt::AlignHCenter);
     mainLayout->addSpacing(20);
     mainLayout->addWidget(formatingLabel, 0 , Qt::AlignHCenter);
     mainLayout->addStretch(1);
     setLayout(mainLayout);
+}
+
+void FormatingPage::animateToFinish(const bool &result)
+{
+    m_progressBox->finishedTask(result);
+}
+
+void FormatingPage::startAnimate()
+{
+    m_progressBox->setValue(0);
+    m_progressBox->startTask();
 }
