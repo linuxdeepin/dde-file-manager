e820c9121de7d4c022c043be9408dded1cfce2ef
zccrs <ccrr1314@live.com>
2017-01-09 15:23:16 +0800

    Use the rlocate tool for search file
    Change-Id: Iabdc263a35881943b85de55891c7d4b913eb713c

--- dde-file-manager-lib/interfaces/ddiriterator.h
+++ dde-file-manager-lib/interfaces/ddiriterator.h
@@ -6,16 +6,18 @@
 class DDirIterator
 {
 public:
-    virtual ~DDirIterator() {};
+    virtual ~DDirIterator() {}
 
     virtual DUrl next() = 0;
     virtual bool hasNext() const = 0;
-    virtual void close() {};
+    virtual void close() {}
 
     virtual QString fileName() const = 0;
     virtual QString filePath() const = 0;
     virtual const DAbstractFileInfoPointer fileInfo() const = 0;
     virtual QString path() const = 0;
+
+    virtual bool hasIteratorOfSubdir() const {return false;}
 };
 
 typedef QSharedPointer<DDirIterator> DDirIteratorPointer;
