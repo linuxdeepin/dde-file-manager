b24243e8d188e49f724fea117a89899a58d7afb0
tommego <licaike@deepin.com>
2017-02-15 15:59:34 +0800

    Ajust usb-device-formatter ui
    Change-Id: I0d09bc71f279a53ecfca7339c5b780e3a608d15b

--- usb-device-formatter/widgets/progressbox.h
+++ usb-device-formatter/widgets/progressbox.h
@@ -5,6 +5,8 @@
 #include <QTimer>
 #include <QVariantAnimation>
 
+class QImage;
+
 struct Bled{
     QPointF pos;
 };
@@ -15,6 +17,7 @@ class ProgressBox : public ProgressLine
 public:
     explicit ProgressBox(QWidget *parent = 0);
     void initConnections();
+    void initUI();
 
 signals:
     void finished(const bool& successful);
@@ -32,6 +35,28 @@ private:
     QTimer* m_updateTimer;
     QList<Bled> m_bleds;
     QVariantAnimation* m_taskAni;
+    QLabel* m_textLabel;
+
+    int m_ForntXOffset1;
+    int m_ForntXOffset2;
+    int m_BackXOffset1;
+    int m_BackXOffset2;
+    int m_YOffset;
+    int m_FrontWidth;
+    int m_BackWidth;
+
+    double m_Pop7YOffset;
+    double m_Pop7XOffset;
+
+    double m_Pop8YOffset;
+    double m_Pop8XOffset;
+
+    double m_Pop11YOffset;
+    double m_Pop11XOffset;
+
+    QImage* m_BackImage;
+    QImage* m_FrontImage;
+    QImage* m_shadow;
 };
 
 #endif // PROGRESSBOX_H
