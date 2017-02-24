7e3574490db4c71a6c70756527a5d293751ab74e
dragondjf <ding465398889@163.com>
2017-02-24 12:30:58 +0800

    Make set dde-file-dialog effective as soon as changed
    Change-Id: Icdf1b5c279753d1dd37eb113be9dae1835c70f31

--- dde-file-manager-lib/interfaces/dfmsetting.h
+++ dde-file-manager-lib/interfaces/dfmsetting.h
@@ -4,8 +4,12 @@
 #include <QObject>
 #include "views/dfileview.h"
 #include "dtk_global.h"
+#include "interfaces/durl.h"
 #include <QJsonObject>
 #include <QDir>
+
+
+class DAbstractFileWatcher;
 
 DTK_BEGIN_NAMESPACE
 
@@ -46,10 +50,12 @@ signals:
 
 public slots:
     void onValueChanged(const QString& key, const QVariant& value);
+    void onConfigFileChanged(const DUrl &fromUrl, const DUrl &toUrl);
 
 private:
     Settings* m_settings;
     QStringList paths;
+    DAbstractFileWatcher* m_fileSystemWathcer;
 };
 
 #endif // DFMSETTING_H
