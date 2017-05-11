16d6cd9c152ddb601e00416eec5a0e349e57550b
Hualet Wang <mr.asianwang@gmail.com>
2017-05-11 14:11:07 +0800

    Complement zone & wallpaper settings translation
    Change-Id: I94920248a06db1ce34a3920dabca41192b133ba6

--- dde-desktop/translations/dde-desktop.ts
+++ dde-desktop/translations/dde-desktop.ts
@@ -4,29 +4,80 @@
 <context>
     <name>CanvasGridView</name>
     <message>
-        <location filename="../view/canvasgridview.cpp" line="1688"/>
+        <location filename="../view/canvasgridview.cpp" line="1740"/>
         <source>Auto arrange</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../view/canvasgridview.cpp" line="1696"/>
-        <location filename="../view/canvasgridview.cpp" line="1818"/>
+        <location filename="../view/canvasgridview.cpp" line="1748"/>
+        <location filename="../view/canvasgridview.cpp" line="1870"/>
         <source>Property</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../view/canvasgridview.cpp" line="1727"/>
+        <location filename="../view/canvasgridview.cpp" line="1779"/>
         <source>Display Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../view/canvasgridview.cpp" line="1732"/>
+        <location filename="../view/canvasgridview.cpp" line="1784"/>
         <source>Corner Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../view/canvasgridview.cpp" line="1737"/>
+        <location filename="../view/canvasgridview.cpp" line="1789"/>
         <source>Set Wallpaper</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WallpaperItem</name>
+    <message>
+        <location filename="../../dde-wallpaper-chooser/wallpaperitem.cpp" line="73"/>
+        <source>Only desktop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dde-wallpaper-chooser/wallpaperitem.cpp" line="76"/>
+        <source>Only lock screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ZoneMainWindow</name>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="53"/>
+        <source>Fast Screen Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="53"/>
+        <source>Control Center</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="53"/>
+        <source>All Windows</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="53"/>
+        <source>Launcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="53"/>
+        <source>Desktop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="53"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="57"/>
+        <source>Close Window</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
