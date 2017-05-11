22ff7282d7aadfa03c90a977fe7699bd94c58a96
Hualet Wang <mr.asianwang@gmail.com>
2017-05-11 14:11:07 +0800

    Complement zone & wallpaper settings translation
    Change-Id: I94920248a06db1ce34a3920dabca41192b133ba6

--- dde-desktop/translations/dde-desktop_zh_CN.ts
+++ dde-desktop/translations/dde-desktop_zh_CN.ts
@@ -1,31 +1,84 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
 <context>
     <name>CanvasGridView</name>
     <message>
-        <location filename="../view/canvasgridview.cpp" line="1688"/>
+        <location filename="../view/canvasgridview.cpp" line="1740"/>
         <source>Auto arrange</source>
         <translation>自动排列</translation>
     </message>
     <message>
-        <location filename="../view/canvasgridview.cpp" line="1696"/>
-        <location filename="../view/canvasgridview.cpp" line="1818"/>
+        <location filename="../view/canvasgridview.cpp" line="1748"/>
+        <location filename="../view/canvasgridview.cpp" line="1870"/>
         <source>Property</source>
         <translation>属性</translation>
     </message>
     <message>
-        <location filename="../view/canvasgridview.cpp" line="1727"/>
+        <location filename="../view/canvasgridview.cpp" line="1779"/>
         <source>Display Settings</source>
         <translation>显示设置</translation>
     </message>
     <message>
-        <location filename="../view/canvasgridview.cpp" line="1732"/>
+        <location filename="../view/canvasgridview.cpp" line="1784"/>
         <source>Corner Settings</source>
         <translation>热区设置</translation>
     </message>
     <message>
-        <location filename="../view/canvasgridview.cpp" line="1737"/>
+        <location filename="../view/canvasgridview.cpp" line="1789"/>
         <source>Set Wallpaper</source>
         <translation>设置壁纸</translation>
+    </message>
+</context>
+<context>
+    <name>WallpaperItem</name>
+    <message>
+        <location filename="../../dde-wallpaper-chooser/wallpaperitem.cpp" line="73"/>
+        <source>Only desktop</source>
+        <translation>仅设置桌面</translation>
+    </message>
+    <message>
+        <location filename="../../dde-wallpaper-chooser/wallpaperitem.cpp" line="76"/>
+        <source>Only lock screen</source>
+        <translation>仅设置锁屏</translation>
+    </message>
+</context>
+<context>
+    <name>ZoneMainWindow</name>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="53"/>
+        <source>Fast Screen Off</source>
+        <translation>快速黑屏</translation>
+    </message>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="53"/>
+        <source>Control Center</source>
+        <translation>控制中心</translation>
+    </message>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="53"/>
+        <source>All Windows</source>
+        <translation>所有窗口</translation>
+    </message>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="53"/>
+        <source>Launcher</source>
+        <translation>启动器</translation>
+    </message>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="53"/>
+        <source>Desktop</source>
+        <translation>显示桌面</translation>
+    </message>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="53"/>
+        <source>None</source>
+        <translation>无</translation>
+    </message>
+    <message>
+        <location filename="../../dde-zone/mainwindow.cpp" line="57"/>
+        <source>Close Window</source>
+        <translation>关闭窗口</translation>
     </message>
 </context>
 </TS>
