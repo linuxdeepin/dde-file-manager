9e19f0a042a83420efbffc2d97254d4e82520b20
zccrs <zccrs@live.com>
2017-05-17 10:53:16 +0800

    Add translates for dde-file-manager project
    Change-Id: If75d8f69b003673701d60629e3325e92f5884b40

---
+++ dde-file-manager/translations/dde-file-manager-app.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Application</name>
+    <message>
+        <location filename="../main.cpp" line="110"/>
+        <source>Deepin File Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="115"/>
+        <source>File Manager is a file management tool independently developed by Deepin Technology, featured with searching, copying, trash, compression/decompression, file property and other file management functions.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
