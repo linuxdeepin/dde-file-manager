e7ddacb7d41d3f72c411d67d9bcf21a58cc0202e
zccrs <ccrr1314@live.com>
2017-05-25 17:37:11 +0800

    Fix the file-manager.sh arguments can not contain space character
    Change-Id: Ib54159473ed72853746a15184455160cd9bb28e4

--- dde-file-manager/mips/file-manager.sh
+++ dde-file-manager/mips/file-manager.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 data="ZmlsZS1tYW5hZ2VyLnNo"
-for i in $@
+for i in "$@"
 do
         arg_base64=`echo -n $i|base64 -w 0`
         data=$data" "$arg_base64
 done
 
-echo -n $data|socat - $XDG_RUNTIME_DIR/dde-file-manager;
+echo -n $data|socat - $XDG_RUNTIME_DIR/dde-file-manager
 
 if [ $? != 0 ]; then
-        dde-file-manager $@
+        dde-file-manager "$@"
 fi
