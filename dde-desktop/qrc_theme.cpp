28134e5394650d0f73c5efa388bb8e969396c4bc
drgondjf <ding465398889@163.com>
2017-09-18 13:58:27 +0800

    update Licence
    Change-Id: I7a02f4992048254839987f10be4b5a0abd04274c

--- dde-desktop/qrc_theme.cpp
+++ dde-desktop/qrc_theme.cpp
@@ -1,10 +1,17 @@
-/****************************************************************************
-** Resource object code
-**
-** Created by: The Resource Compiler for Qt version 5.6.1
-**
-** WARNING! All changes made in this file will be lost!
-*****************************************************************************/
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 static const unsigned char qt_resource_data[] = {
   // /home/djf/dde-workspace/dde-file-manager/dde-desktop/resource/theme/light/CanvasGridView.theme
