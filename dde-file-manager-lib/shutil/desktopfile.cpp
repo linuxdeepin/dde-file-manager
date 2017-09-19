02a0cd25e3c388f70ebbd46dd33dd25a50dbc0db
drgondjf <ding465398889@163.com>
2017-09-19 17:12:11 +0800

    Fix crash for desktopFile analysis param mime_tyep
    Change-Id: I5145f69dca18eaf8fffd813d222320f21b0a8d9e

--- dde-file-manager-lib/shutil/desktopfile.cpp
+++ dde-file-manager-lib/shutil/desktopfile.cpp
@@ -61,7 +61,7 @@ DesktopFile::DesktopFile(const QString &fileName) {
     m_type = desktop.value("Type", settings.value("Type", "Application")).toString();
     m_categories = desktop.value("Categories", settings.value("Categories").toString()).toString().remove(" ").split(";");
 
-    const QString &mime_type = desktop.value("MimeType", settings.value("MimeType").toString()).toString().remove(" ");
+    QString mime_type = desktop.value("MimeType", settings.value("MimeType").toString()).toString().remove(" ");
 
     if (!mime_type.isEmpty())
         m_mimeType = mime_type.split(";");
