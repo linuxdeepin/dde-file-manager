9088e6cbb7d47e8a7fa872f0ac2c7606ece5d584
drgondjf <ding465398889@163.com>
2017-09-21 10:45:34 +0800

    Update translations
    Change-Id: I6073ce699aad5a0b180a34c7807f6a7bd59c0e0d

--- dde-file-manager-lib/configure/dfmconfigdict.cpp
+++ dde-file-manager-lib/configure/dfmconfigdict.cpp
@@ -1,19 +1,4 @@
-/*
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include <QObject>
+#include <DSettings>
 
 void GenerateSettingTranslate()
 {
@@ -66,5 +51,5 @@ void GenerateSettingTranslate()
     auto group_base_hidden_filesName = QObject::tr("Hidden files");
     auto group_base_new_tab_windowsName = QObject::tr("New window and tab");
     auto group_base_open_actionName = QObject::tr("Open behavior");
-    auto reset_button_name = QObject::tr("Restore to default");
+    auto reset_button_name = QObject::tr("Restore Defaults");
 }
