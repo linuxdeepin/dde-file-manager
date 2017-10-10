449acf3651e437ccb8314093f4692554c25b9609
kirigaya <kirigaya@mkacg.com>
2017-10-10 14:51:33 +0800

    Revert "feat: hot zone support hidpi and update new demo video"
    This reverts commit c3b6c7a5c7108cdd12e0a76cc22ce87cd14d5017.
    
    Change-Id: Ia91507aa6536f1861058f7c93494fe4bc5732adf

--- dde-zone/mainwindow.cpp
+++ dde-zone/mainwindow.cpp
@@ -46,7 +46,6 @@ ZoneMainWindow::ZoneMainWindow(QWidget *parent)
 
 #ifndef DISABLE_DEMO_VIDEO
     m_videoWidget = new DVideoWidget(this);
-    m_videoWidget->setSourceVideoPixelRatio(devicePixelRatioF());
     QTimer::singleShot(1000, this, &ZoneMainWindow::onDemoVideo);
 #endif
 
@@ -105,11 +104,9 @@ void ZoneMainWindow::onDemoVideo()
     list->addMedia(QUrl("qrc:/images/Prompt.mov"));
     list->setPlaybackMode(QMediaPlaylist::Loop);
 
-    const qreal ratio = devicePixelRatioF();
-
-    int x = (rect().right() - 450 * ratio) / 2;
-    int y = (rect().bottom() - 348 * ratio) / 2;
-    m_videoWidget->setGeometry(x, y, 450 * ratio, 348 * ratio);
+    int x = (this->rect().right() - 450) / 2;
+    int y = (this->rect().bottom() - 348) / 2;
+    m_videoWidget->setGeometry(x, y, 450, 348);
 
     m_videoWidget->setSource(player);
     player->setPlaylist(list);
