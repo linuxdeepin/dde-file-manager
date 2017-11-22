417afa30ae8ee06ad71e9886234d1f6d0baa0daf
transifex <transifex@linuxdeepin.com>
2017-11-22 08:54:11 +0000

    auto sync po files from transifex
    Change-Id: I02ae9f90e01638ce5af2e7041c146932bfe02c2c

---
+++ dde-file-manager/translations/dde-file-manager-app_ca.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ca" version="2.1">
+<context>
+    <name>Application</name>
+    <message>
+        <location filename="../main.cpp" line="113"/>
+        <source>Deepin File Manager</source>
+        <translation>Gestor de fitxers del Deepin</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="118"/>
+        <source>File Manager is a file management tool independently developed by Deepin Technology, featured with searching, copying, trash, compression/decompression, file property and other file management functions.</source>
+        <translation>El gestor de fitxers és una eina desenvolupada independentment per Deepin Technology, que permet cercar, copiar, eliminar, comprimir i descomprimir fitxers, i gestionar-ne les propietats, entre altres funcions.</translation>
+    </message>
+</context>
+</TS>
---
+++ usb-device-formatter/translations/usb-device-formatter_ca.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ca" version="2.1">
+<context>
+    <name>FinishPage</name>
+    <message>
+        <location filename="../view/finishpage.cpp" line="18"/>
+        <source>Format successfully</source>
+        <translation>Format correcte</translation>
+    </message>
+</context>
+<context>
+    <name>FormatingPage</name>
+    <message>
+        <location filename="../view/formatingpage.cpp" line="19"/>
+        <source>Formating, please wait for a minute...</source>
+        <translation>Formatant. Espereu, si us plau...</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../view/mainpage.cpp" line="85"/>
+        <source>Type</source>
+        <translation>Tipus</translation>
+    </message>
+    <message>
+        <location filename="../view/mainpage.cpp" line="104"/>
+        <source>Label</source>
+        <translation>Etiqueta</translation>
+    </message>
+    <message>
+        <location filename="../view/mainpage.cpp" line="113"/>
+        <source>Fast format</source>
+        <translation>Formatació ràpida</translation>
+    </message>
+    <message>
+        <location filename="../view/mainpage.cpp" line="130"/>
+        <source>This operation will clear all data from your device.</source>
+        <translation>Aquesta operació eliminarà totes les dades del dispositiu.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="56"/>
+        <location filename="../view/mainwindow.cpp" line="78"/>
+        <source>Format</source>
+        <translation>Formata</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="165"/>
+        <source>Continue</source>
+        <translation>Continua</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="170"/>
+        <source>Formatting...</source>
+        <translation>Formatant...</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="194"/>
+        <source>Done</source>
+        <translation>Fet</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="205"/>
+        <source>Failed to format</source>
+        <translation>Ha fallat la formatació.</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="201"/>
+        <source>Quit</source>
+        <translation>Surt</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="202"/>
+        <source>Your disk is removed when formatting</source>
+        <translation>No es pot accedir al disc durant la formatació.</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="206"/>
+        <source>Reformat</source>
+        <translation>Reformata</translation>
+    </message>
+</context>
+<context>
+    <name>MessageDialog</name>
+    <message>
+        <location filename="../dialogs/messagedialog.cpp" line="19"/>
+        <source>OK</source>
+        <translation>D&apos;acord</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="71"/>
+        <source>Device does not exist</source>
+        <translation>El dispositiu no existeix.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="80"/>
+        <source>Cannot format local device</source>
+        <translation>No es pot formatar el dispositiu local.</translation>
+    </message>
+</context>
+<context>
+    <name>WarnPage</name>
+    <message>
+        <location filename="../view/warnpage.cpp" line="19"/>
+        <source>Formating operation will clear all data from your disk, are you sure to continue? This option can not be restored.</source>
+        <translation>L&apos;operació de formatació eliminarà totes les dades del disc. Segur que voleu continuar? Aquesta opció no es pot restaurar.</translation>
+    </message>
+</context>
+</TS>
