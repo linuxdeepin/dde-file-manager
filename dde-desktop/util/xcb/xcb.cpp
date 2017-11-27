4d7a3793cd014511f964e2fa5d5b55d706dc62e4
Iceyer <me@iceyer.net>
2017-11-27 17:45:16 +0800

    feat: add desktop geometry debug info
    Change-Id: Id4ecbd87f376c2552489b8eb6d52849c93284bbb

--- dde-desktop/util/xcb/xcb.cpp
+++ dde-desktop/util/xcb/xcb.cpp
@@ -91,9 +91,9 @@ bool XcbMisc::is_dock_window(xcb_window_t winId)
     return is_dock;
 }
 
-xcb_window_t XcbMisc::find_dock_window(int screen_nbr)
+DockInfo XcbMisc::find_dock_window(int screen_nbr)
 {
-    QList<xcb_window_t> docks;
+    QList<DockInfo> docks;
     for (int i = 0; i < screen_nbr; ++i) {
         xcb_ewmh_get_windows_reply_t clients;
         xcb_generic_error_t *e;
@@ -102,13 +102,17 @@ xcb_window_t XcbMisc::find_dock_window(int screen_nbr)
             for (uint32_t wi = 0; wi < clients.windows_len; ++wi) {
                 xcb_window_t winId = clients.windows[wi];
                 if (is_dock_window(winId)) {
-                    docks << winId;
+                    DockInfo info;
+                    info.winId = winId;
+                    info.screenId = i;
+                    docks << info;
+                    qDebug() << "find dock:" << info.winId << "on screen" << info.screenId;
                 }
             }
         }
     }
 
-    return docks.length() > 0 ? docks.value(0) : 0;
+    return docks.length() > 0 ? docks.value(0) : DockInfo();
 }
 
 }
