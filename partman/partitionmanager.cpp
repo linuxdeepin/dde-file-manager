9a960d6b93cecfbaa0bfb1ea7b2f0c498bfe62a1
drgondjf <ding465398889@163.com>
2018-02-07 12:18:58 +0800

    update licence
    Change-Id: I3c44562a47bc4505a8735654c64c27a0c67871f9

--- partman/partitionmanager.cpp
+++ partman/partitionmanager.cpp
@@ -1,4 +1,13 @@
 /*
+ * Copyright (C) 2016 ~ 2018 Deepin Technology Co., Ltd.
+ *               2016 ~ 2018 dragondjf
+ *
+ * Author:     dragondjf<dingjiangfeng@deepin.com>
+ *
+ * Maintainer: dragondjf<dingjiangfeng@deepin.com>
+ *             zccrs<zhangjide@deepin.com>
+ *             Tangtong<tangtong@deepin.com>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -12,7 +21,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 
 #include "partitionmanager.h"
 #include "partition.h"
