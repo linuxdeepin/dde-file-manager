50be2a2f66e613631a03f4a7da7e49bb1dd68a6c
zccrs <ccrr1314@live.com>
2018-03-20 19:14:16 +0800

    fix: can not move the invalid symlink to trash
    Change-Id: Iecd673a22375edfb9339f8dee00fadeef53190d2

--- fileoperations/filejob.cpp
+++ fileoperations/filejob.cpp
@@ -253,9 +253,9 @@ DUrlList FileJob::doMoveCopyJob(const DUrlList &files, const DUrl &destination)
     DUrlList list;
     QString tarDirPath = destination.toLocalFile();
     QDir tarDir(tarDirPath);
-    QStorageInfo tarStorageInfo(tarDirPath);
+    QStorageInfo tarStorageInfo = getStorageInfo(tarDirPath);
     if (files.count() > 0){
-        QStorageInfo srcStorageInfo(files.at(0).toLocalFile());
+        QStorageInfo srcStorageInfo = getStorageInfo(files.at(0).toLocalFile());
         if (srcStorageInfo.rootPath() != tarStorageInfo.rootPath()){
             m_isInSameDisk = false;
         }else if (FileUtils::isGvfsMountFile(destination.toLocalFile())){
@@ -303,7 +303,7 @@ DUrlList FileJob::doMoveCopyJob(const DUrlList &files, const DUrl &destination)
         }
         QFileInfo srcInfo(srcPath);
 
-        if (!srcInfo.exists())
+        if (!srcInfo.exists() && !srcInfo.isSymLink())
             continue;
 
         QString targetPath;
@@ -461,8 +461,8 @@ DUrlList FileJob::doMoveToTrash(const DUrlList &files)
     }
 
     if (files.count() > 0){
-        QStorageInfo storageInfo(files.at(0).toLocalFile());
-        QStorageInfo trashStorageInfo(DFMStandardPaths::standardLocation(DFMStandardPaths::TrashFilesPath));
+        QStorageInfo storageInfo = getStorageInfo(files.at(0).toLocalFile());
+        QStorageInfo trashStorageInfo = getStorageInfo(DFMStandardPaths::standardLocation(DFMStandardPaths::TrashFilesPath));
         if(storageInfo.rootPath() != trashStorageInfo.rootPath()){
             m_isInSameDisk = false;
         }
@@ -511,9 +511,9 @@ void FileJob::doTrashRestore(const QString &srcFilePath, const QString &tarFileP
     m_totalSize = FileUtils::totalSize(files);
     jobPrepared();
 
-    QStorageInfo srcStorageInfo(srcFilePath);
+    QStorageInfo srcStorageInfo = getStorageInfo(srcFilePath);
     QString tarDir = DUrl::fromLocalFile(tarFilePath).parentUrl().toLocalFile();
-    QStorageInfo tarStorageInfo(tarDir);
+    QStorageInfo tarStorageInfo = getStorageInfo(tarDir);
     if (srcStorageInfo.rootPath() != tarStorageInfo.rootPath()){
         m_isInSameDisk = false;
     }
@@ -1645,7 +1645,7 @@ bool FileJob::handleMoveJob(const QString &srcPath, const QString &tarDir, QStri
                         }
                     }else{
                         QFile tarFile(m_tarPath);
-                        if(tarFile.exists()){
+                        if(tarFile.exists() || QFileInfo(tarFile).isSymLink()){
                             QFile(m_tarPath).remove();
                         }
                     }
@@ -1806,11 +1806,11 @@ bool FileJob::restoreTrashFile(const QString &srcFile, const QString &tarFile)
     QFileInfo toInfo(tarFile);
     m_srcFileName = toInfo.fileName();
     m_srcPath = srcFile;
-    m_tarPath = toInfo.absoluteDir().path();
+    m_tarPath = toInfo.absoluteFilePath();
     m_tarDirName = toInfo.absoluteDir().dirName();
     m_status = Started;
 
-    if(toInfo.exists())
+    if(toInfo.exists() || toInfo.isSymLink())
     {
         jobConflicted();
     }
@@ -1833,7 +1833,7 @@ bool FileJob::restoreTrashFile(const QString &srcFile, const QString &tarFile)
                 if (m_isReplaced){
                     m_tarPath = m_tarPath + "/" + toInfo.fileName();
 
-                    if(to.exists()){
+                    if(to.exists() || toInfo.isSymLink()){
                         if (toInfo.isDir()){
 //                            bool result = QDir(tarFile).removeRecursively();
 
@@ -1987,7 +1987,7 @@ bool FileJob::deleteDir(const QString &dir)
 
     while (iterator.hasNext()) {
         const QFileInfo &fileInfo = iterator.next();
-        if (fileInfo.exists()){
+        if (fileInfo.exists() || fileInfo.isSymLink()){
             if (fileInfo.isFile() || fileInfo.isSymLink()) {
                 if (!deleteFile(fileInfo.filePath())) {
                     qDebug() << "Unable to remove file:" << fileInfo.filePath();
@@ -2172,7 +2172,7 @@ bool FileJob::checkDiskSpaceAvailable(const DUrlList &files, const DUrl &destina
     }
 
     qint64 freeBytes;
-    freeBytes = QStorageInfo(destination.toLocalFile()).bytesFree();
+    freeBytes = getStorageInfo(destination.toLocalFile()).bytesFree();
 
     m_isCheckingDisk = true;
 
@@ -2273,6 +2273,15 @@ bool FileJob::checkUseGvfsFileOperation(const DUrlList &files, const DUrl &desti
 bool FileJob::checkUseGvfsFileOperation(const QString &path)
 {
     return FileUtils::isGvfsMountFile(path);
+}
+
+QStorageInfo FileJob::getStorageInfo(const QString &file)
+{
+    QFileInfo info(file);
+
+    return (info.isSymLink() && !info.exists())
+            ? QStorageInfo(info.absolutePath())
+            : QStorageInfo(info.absoluteFilePath());
 }
 
 #ifdef SW_LABEL
