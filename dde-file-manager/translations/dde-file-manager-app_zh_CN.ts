cb4afffbc0d329ee05df28a4a374b8278647d532
zccrs <ccrr1314@live.com>
2018-03-23 17:01:30 +0800

    fix: translateion
    Change-Id: I683c7fa226878d0c70a75dfb2e018a3a6e6ac67b

--- dde-file-manager/translations/dde-file-manager-app_zh_CN.ts
+++ dde-file-manager/translations/dde-file-manager-app_zh_CN.ts
@@ -1,13 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
 <context>
     <name>Application</name>
     <message>
-        <location filename="../main.cpp" line="113"/>
+        <location filename="../main.cpp" line="142"/>
         <source>Deepin File Manager</source>
         <translation>深度文件管理器</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="118"/>
+        <location filename="../main.cpp" line="147"/>
         <source>File Manager is a file management tool independently developed by Deepin Technology, featured with searching, copying, trash, compression/decompression, file property and other file management functions.</source>
         <translation>文件管理器是深度科技自主研发的一款管理文件的工具， 它包括搜索、复制、回收站、压缩/解压缩， 文件属性等管理文件的功能。</translation>
     </message>
