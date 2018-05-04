9e7219607d788705e0cc162db87e0d91a6e66452
shihua <878694891@qq.com>
2018-05-04 11:08:59 +0800

    fix: after cutting files and moving file to trash remove old file.
    Change-Id: Id6054567c7eee373d0604b1fa7c98608bf85b8f2

--- dde-file-manager-lib/tag/tagmanager.cpp
+++ dde-file-manager-lib/tag/tagmanager.cpp
@@ -311,6 +311,14 @@ void TagManager::init_connect()noexcept
         DFileService::instance()->setFileTags(this, to, tags);
     });
 
+    QObject::connect(DFileService::instance(), &DFileService::fileMovedToTrash, [this](const DUrl& from, const DUrl& to){
+        (void)to;
+
+        if(from.isValid()){
+            deleteFiles({from});
+        }
+    });
+
     QObject::connect(TagManagerDaemonController::instance(), &TagManagerDaemonController::addNewTags,[this](const QVariant& new_tags){
 
         emit this->addNewTag(new_tags.toStringList());
