11a2d4da17dd089521313f989803ea710e83b715
shihua <878694891@qq.com>
2018-05-21 11:45:19 +0800

    feat(fileview-sortorder): now showing files supports to configure through a json file.
    Change-Id: Ic2e6bed8e23f9fd0dc0324e742467746efeb0f93

--- dde-file-manager-lib/controllers/fmstatemanager.cpp
+++ dde-file-manager-lib/controllers/fmstatemanager.cpp
@@ -22,6 +22,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <ios>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
 #include "fmstatemanager.h"
 
 #include "qobjecthelper.h"
@@ -29,13 +34,24 @@
 #include "app/define.h"
 #include "app/filesignalmanager.h"
 #include "shutil/fileutils.h"
+#include "shutil/viewstatesmanager.h"
+#include "dfileinfo.h"
+#include "dfilesystemmodel.h"
+#include "interfaces/dfmstandardpaths.h"
 
 #include <QJsonParseError>
 #include <QJsonObject>
+#include <QDebug>
 #include <QFile>
 #include <QDir>
 
 QMap<DUrl, QPair<int, int>> FMStateManager::SortStates;
+
+
+///###: this configure record how to show files under specify dir(by last modified time of desending or asending).
+static QString config_path{ QString("%1/%2/%3").arg(DFMStandardPaths::standardLocation(DFMStandardPaths::ApplicationSharePath),
+                               "config",
+                               "default-view-states.json") };
 
 
 FMStateManager::FMStateManager(QObject *parent)
@@ -66,6 +82,18 @@ QString FMStateManager::sortCacheFilePath()
 {
 //    return QString("%1/%2").arg(StandardPath::getCachePath(), "sort.json");
     return getConfigPath("sort");
+}
+
+QPair<int, int> FMStateManager::getRoleAndSortOrderByUrl(const DUrl& url)
+{
+    QMap<DUrl, QPair<int, int>> url_role_sort_order{ FMStateManager::getUrlsForFilttering() };
+
+    if (url_role_sort_order.contains(url)) {
+        QPair<int, int> role_and_sort{ url_role_sort_order[url] };
+        return FMStateManager::SortStates.value(url, role_and_sort);
+    }
+
+    return FMStateManager::SortStates.value(url, QPair<int, int>{DFileSystemModel::FileDisplayNameRole, Qt::AscendingOrder});
 }
 
 void FMStateManager::loadCache()
@@ -130,6 +158,70 @@ void FMStateManager::cacheSortState(const DUrl &url, int role, Qt::SortOrder ord
 {
     FMStateManager::SortStates.insert(url, QPair<int, int>(role, order));
     FMStateManager::saveSortCache();
+}
+
+QMap<DUrl, QPair<int, int>> FMStateManager::getUrlsForFilttering() noexcept
+{
+    QMap<DUrl, QPair<int, int>> url_role_sort_order{};
+
+    if (QFileInfo::exists(config_path)) {
+        std::basic_ifstream<char> read_json{ config_path.toStdString() };
+
+        if (read_json.is_open() && (read_json.rdstate() == std::ios_base::goodbit
+                                    || read_json.rdstate() == std::ios_base::eofbit)) {
+            std::istream_iterator<char> input_itr{ read_json };
+            std::basic_ostringstream<char> store_content{};
+            std::copy(input_itr, std::istream_iterator<char>{}, std::ostream_iterator<char>{store_content});
+            std::basic_string<char> content{store_content.str()};
+            QJsonParseError error{};
+            QJsonDocument json_document{ QJsonDocument::fromJson(QByteArray::fromStdString(content), &error) };
+
+            if (error.error == QJsonParseError::NoError && !json_document.isNull()) {
+
+                if (json_document.isObject()) {
+                    QJsonObject json_object{ json_document.object() };
+                    QList<QString> keys{ json_object.keys() };
+
+                    for (const QString key : keys) {
+                        QJsonValue value{ json_object.take(key) };
+
+                        if (value.isObject()) {
+                            QJsonObject object{ value.toObject() };
+                            QPair<int, int> role_sort_order{};
+
+                            if (object.contains("sortRole")) {
+                                QJsonValue sort_role{ object.take("sortRole") };
+                                int int_value{ sort_role.toInt() };
+                                role_sort_order.first = int_value;
+                            }
+
+                            if (object.contains("sortOrder")) {
+                                QJsonValue sort_order{ object.take("sortOrder") };
+                                QString sort_order_str{ sort_order.toString() };
+
+                                if (sort_order_str == "DescendingOrder") {
+                                    role_sort_order.second = Qt::DescendingOrder;
+
+                                } else if (sort_order_str == "AscendingOrder") {
+                                    role_sort_order.second = Qt::AscendingOrder;
+                                }
+                            }
+
+                            url_role_sort_order[DUrl::fromUserInput(key)] = role_sort_order;
+                        }
+                    }
+                }
+
+            } else {
+                qErrnoWarning("Some errors occured, when is opening default-view-states.json.");
+            }
+
+        } else {
+            qErrnoWarning("Some errors occured, when is opening default-view-states.json.");
+        }
+    }
+
+    return url_role_sort_order;
 }
 
 
