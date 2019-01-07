dfa567da1aa5366b271c7e6a540718f186845142
zccrs <ccrr1314@live.com>
2019-01-07 15:51:42 +0800

    fix(dde-desktop): translate error: "Wallpaper" on chinese
    Change-Id: I58edf44cbc183436ba7df5ef7a5e432ac868b5a5

--- dde-desktop/translations/dde-desktop_zh_CN.ts
+++ dde-desktop/translations/dde-desktop_zh_CN.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
 <context>
     <name>CanvasGridView</name>
     <message>
@@ -14,7 +16,7 @@
     <message>
         <location filename="../view/canvasgridview.cpp" line="2351"/>
         <source>Wallpaper</source>
-        <translation>壁纸</translation>
+        <translation>设置壁纸</translation>
     </message>
     <message>
         <location filename="../view/canvasgridview.cpp" line="2353"/>
