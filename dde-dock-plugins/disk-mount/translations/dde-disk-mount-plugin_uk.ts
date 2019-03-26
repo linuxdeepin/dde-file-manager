c22b4a629eed5a19aa8f0a540f76cd493bbcd6d3
electricface1 <songwentai@deepin.com>
2019-03-26 03:34:18 +0000

    chore: auto pull translation files from transifex

---
+++ dde-dock-plugins/disk-mount/translations/dde-disk-mount-plugin_uk.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="uk" version="2.1">
+<context>
+    <name>DeepinStorage</name>
+    <message>
+        <location filename="../dattachedudisks2device.cpp" line="91"/>
+        <location filename="../dattachedudisks2device.cpp" line="106"/>
+        <source>%1 Volume</source>
+        <translation>Том %1</translation>
+    </message>
+</context>
+<context>
+    <name>DiskMountPlugin</name>
+    <message>
+        <location filename="../diskmountplugin.cpp" line="48"/>
+        <source>Disk</source>
+        <translation>Диск</translation>
+    </message>
+    <message>
+        <location filename="../diskmountplugin.cpp" line="102"/>
+        <source>Open</source>
+        <translation>Відкрити</translation>
+    </message>
+    <message>
+        <location filename="../diskmountplugin.cpp" line="108"/>
+        <source>Unmount all</source>
+        <translation>Демонтувати усі</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../diskcontrolwidget.cpp" line="292"/>
+        <source>Device has been removed</source>
+        <translation>Пристрій вилучено</translation>
+    </message>
+</context>
+</TS>
