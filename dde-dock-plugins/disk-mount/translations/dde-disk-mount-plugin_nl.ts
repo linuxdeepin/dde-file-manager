a0809dea4c15a19db78821fc92892da898a4f7ee
electricface1 <songwentai@deepin.com>
2019-03-26 03:34:18 +0000

    chore: auto pull translation files from transifex

---
+++ dde-dock-plugins/disk-mount/translations/dde-disk-mount-plugin_nl.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="nl" version="2.1">
+<context>
+    <name>DeepinStorage</name>
+    <message>
+        <location filename="../dattachedudisks2device.cpp" line="91"/>
+        <location filename="../dattachedudisks2device.cpp" line="106"/>
+        <source>%1 Volume</source>
+        <translation>Volume %1</translation>
+    </message>
+</context>
+<context>
+    <name>DiskMountPlugin</name>
+    <message>
+        <location filename="../diskmountplugin.cpp" line="48"/>
+        <source>Disk</source>
+        <translation>Schijf</translation>
+    </message>
+    <message>
+        <location filename="../diskmountplugin.cpp" line="102"/>
+        <source>Open</source>
+        <translation>Openen</translation>
+    </message>
+    <message>
+        <location filename="../diskmountplugin.cpp" line="108"/>
+        <source>Unmount all</source>
+        <translation>Alles ontkoppelen</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../diskcontrolwidget.cpp" line="292"/>
+        <source>Device has been removed</source>
+        <translation>Apparaat is verwijderd</translation>
+    </message>
+</context>
+</TS>
---
+++ dde-dock-plugins/disk-mount/translations/dde-disk-mount-plugin_pt.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="pt" version="2.1">
+<context>
+    <name>DeepinStorage</name>
+    <message>
+        <location filename="../dattachedudisks2device.cpp" line="91"/>
+        <location filename="../dattachedudisks2device.cpp" line="106"/>
+        <source>%1 Volume</source>
+        <translation>%1 Volume</translation>
+    </message>
+</context>
+<context>
+    <name>DiskMountPlugin</name>
+    <message>
+        <location filename="../diskmountplugin.cpp" line="48"/>
+        <source>Disk</source>
+        <translation>Disco</translation>
+    </message>
+    <message>
+        <location filename="../diskmountplugin.cpp" line="102"/>
+        <source>Open</source>
+        <translation>Abrir</translation>
+    </message>
+    <message>
+        <location filename="../diskmountplugin.cpp" line="108"/>
+        <source>Unmount all</source>
+        <translation>Desmontar tudo</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../diskcontrolwidget.cpp" line="292"/>
+        <source>Device has been removed</source>
+        <translation>O dispositivo foi removido</translation>
+    </message>
+</context>
+</TS>
