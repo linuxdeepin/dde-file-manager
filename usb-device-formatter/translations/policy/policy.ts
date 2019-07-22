74a192b8572c40e05c6fa9ab549451aa7df1902b
Gary Wang <wzc782970009@gmail.com>
2019-07-22 16:25:31 +0800

    i18n: usb-device-formatter policy file translation
    (#258)

---
+++ usb-device-formatter/translations/policy/policy.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+	<context>
+		<name>policy</name>
+		<message>
+			<location filename="com.deepin.pkexec.usb-device-formatter!message" line="0" />
+			<source>Authentication is required to run the USB Device Formatter</source>
+			<translation type="unfinished" />
+		</message>
+	</context>
+</TS>
