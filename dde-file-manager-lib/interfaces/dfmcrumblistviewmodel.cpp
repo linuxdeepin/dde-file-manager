b42c3c9e48f6dacf3572ca66482085110ba6aecf
ck <kegechen@gmail.com>
2019-08-16 17:59:07 +0800

    refactor: replace crumblist to listview
    替换地址栏面包屑为listview,  新增listview的model

---
+++ dde-file-manager-lib/interfaces/dfmcrumblistviewmodel.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     Mike Chen <kegechen@gmail.com>
+ *
+ * Maintainer: Mike Chen <chenke_cm@deepin.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "dfmcrumblistviewmodel.h"
+#include "views/themeconfig.h"
+#include "dtkwidget_global.h"
+
+DWIDGET_USE_NAMESPACE
+
+DFM_BEGIN_NAMESPACE
+
+DFMCrumbListviewModel::DFMCrumbListviewModel(QObject *parent)
+    :QStandardItemModel (parent)
+{
+
+}
+
+DFMCrumbListviewModel::~DFMCrumbListviewModel()
+{
+
+}
+
+
+bool DFMCrumbListviewModel::appendItem(const CrumbData &data)
+{
+    auto icon = ThemeConfig::instace()->pixmap(data.iconName, data.iconKey, ThemeConfig::Normal);
+    auto item = new QStandardItem(icon, data.displayText);
+    item->setData(data.url, Qt::UserRole+1);
+    appendRow(item);
+    return true;
+}
+
+void DFMCrumbListviewModel::removeAll()
+{
+    removeRows(0, rowCount());
+}
+
+DFM_END_NAMESPACE
