e53deda726e351de84a2478f5a2e6bb01f09d300
ck <kegechen@gmail.com>
2019-09-16 14:22:32 +0800

    refactor: add mediainfo files

---
+++ dde-file-manager-lib/mediainfo/dfmmediainfo.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     Mike Chen <kegechen@gmail.com>
+ *
+ * Maintainer: Mike Chen <chenke_cm@deepin.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <QObject>
+#include <QScopedPointer>
+
+class DFMMediaInfo : public QObject
+{
+    Q_OBJECT
+public:
+    DFMMediaInfo(const QString &filename, QObject *parent=nullptr);
+    QString generalInformation();
+    QString Value(const QString &key);
+};
+
+
---
+++ dde-file-manager-lib/mediainfo/mediainfo.pri
@@ -0,0 +1,8 @@
+HEADERS += \
+    $$PWD/dfmmediainfo.h
+
+SOURCES += \
+    $$PWD/dfmmediainfo.cpp
+
+#CONFIG          += plugin c++11 link_pkgconfig
+#PKGCONFIG += libmediainfo
