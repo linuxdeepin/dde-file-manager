691a0276b9c8addcf5d1a679f0d26d5ebade563f
Gary Wang <wzc782970009@gmail.com>
2019-10-16 15:30:22 +0800

    refactor: use DWaterProgress widget

--- usb-device-formatter/widgets/progressbox.h
+++ usb-device-formatter/widgets/progressbox.h
@@ -25,7 +25,7 @@
 #ifndef PROGRESSBOX_H
 #define PROGRESSBOX_H
 
-#include "progressline.h"
+#include <DWaterProgress>
 #include <QTimer>
 #include <QVariantAnimation>
 
@@ -35,11 +35,13 @@ struct Bled{
     QPointF pos;
 };
 
-class ProgressBox : public ProgressLine
+DWIDGET_USE_NAMESPACE
+
+class ProgressBox : public DWaterProgress
 {
     Q_OBJECT
 public:
-    explicit ProgressBox(QWidget *parent = 0);
+    explicit ProgressBox(QWidget *parent = nullptr);
     void initConnections();
     void initUI();
 
@@ -51,36 +53,11 @@ public slots:
     void taskTimeOut();
     void startTask();
     void finishedTask(const bool result);
-protected:
-    void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
 
 private:
-    QTimer* m_taskTimer = NULL;
-    QTimer* m_updateTimer = NULL;
-    QList<Bled> m_bleds;
-    QVariantAnimation* m_taskAni = NULL;
-    QLabel* m_textLabel = NULL;
-    QImage* m_BackImage = NULL;
-    QImage* m_FrontImage = NULL;
-    QImage* m_shadow = NULL;
-
-    int m_ForntXOffset1;
-    int m_ForntXOffset2;
-    int m_BackXOffset1;
-    int m_BackXOffset2;
-    int m_YOffset;
-    int m_FrontWidth;
-    int m_BackWidth;
-
-    double m_Pop7YOffset;
-    double m_Pop7XOffset;
-
-    double m_Pop8YOffset;
-    double m_Pop8XOffset;
-
-    double m_Pop11YOffset;
-    double m_Pop11XOffset;
-
+    QTimer* m_taskTimer = nullptr;
+    QTimer* m_updateTimer = nullptr;
+    QVariantAnimation* m_taskAni = nullptr;
 };
 
 #endif // PROGRESSBOX_H
