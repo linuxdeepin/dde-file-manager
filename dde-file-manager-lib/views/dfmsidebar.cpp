9499d52bf1f03df24b268ce8ce0f3ab8c9fdad5d
ck <kegechen@gmail.com>
2019-10-22 09:17:09 +0800

    fix: bug 8494, set sidebar viewport margins
    8494 【左侧边栏】【显示】左侧边栏的滚动条和目录选中效果出现叠加  来源Bug-2119

--- dde-file-manager-lib/views/dfmsidebar.cpp
+++ dde-file-manager-lib/views/dfmsidebar.cpp
@@ -42,7 +42,7 @@
 #include "app/filesignalmanager.h"
 
 #include <DApplicationHelper>
-
+#include <QScrollBar>
 #include <QVBoxLayout>
 #include <QDebug>
 
@@ -65,6 +65,7 @@ DFMSideBar::DFMSideBar(QWidget *parent)
     // init view.
     m_sidebarView->setModel(m_sidebarModel);
     m_sidebarView->setItemDelegate(new DFMSideBarItemDelegate(m_sidebarView));
+    m_sidebarView->setViewportMargins(0, 0, m_sidebarView->verticalScrollBar()->sizeHint().width(), 0);
     m_sidebarView->setContextMenuPolicy(Qt::CustomContextMenu);
     m_sidebarView->setFrameShape(QFrame::Shape::NoFrame);
     m_sidebarView->setAutoFillBackground(true);
