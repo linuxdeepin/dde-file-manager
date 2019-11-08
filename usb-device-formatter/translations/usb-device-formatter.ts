679dfbd5f4688037d63942de4906aa2f09cb0988
Gary Wang <wzc782970009@gmail.com>
2019-11-08 09:17:45 +0800

    i18n: update ts files

--- usb-device-formatter/translations/usb-device-formatter.ts
+++ usb-device-formatter/translations/usb-device-formatter.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
 <context>
     <name>FinishPage</name>
     <message>
@@ -10,7 +12,7 @@
 <context>
     <name>FormatingPage</name>
     <message>
-        <location filename="../view/formatingpage.cpp" line="43"/>
+        <location filename="../view/formatingpage.cpp" line="47"/>
         <source>Formatting the disk, please wait...</source>
         <translation>Formatting the disk, please wait...</translation>
     </message>
@@ -18,22 +20,22 @@
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="../view/mainpage.cpp" line="108"/>
+        <location filename="../view/mainpage.cpp" line="104"/>
         <source>Type</source>
         <translation>Type</translation>
     </message>
     <message>
-        <location filename="../view/mainpage.cpp" line="127"/>
+        <location filename="../view/mainpage.cpp" line="122"/>
         <source>Label</source>
         <translation>Label</translation>
     </message>
     <message>
-        <location filename="../view/mainpage.cpp" line="136"/>
+        <location filename="../view/mainpage.cpp" line="131"/>
         <source>Quick Format</source>
         <translation>Quick Format</translation>
     </message>
     <message>
-        <location filename="../view/mainpage.cpp" line="153"/>
+        <location filename="../view/mainpage.cpp" line="148"/>
         <source>Formatting will erase all data on the disk.</source>
         <translation>Formatting will erase all data on the disk.</translation>
     </message>
@@ -41,43 +43,43 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../view/mainwindow.cpp" line="85"/>
-        <location filename="../view/mainwindow.cpp" line="108"/>
+        <location filename="../view/mainwindow.cpp" line="88"/>
+        <location filename="../view/mainwindow.cpp" line="106"/>
         <source>Format</source>
         <translation>Format</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="185"/>
+        <location filename="../view/mainwindow.cpp" line="161"/>
         <source>Continue</source>
         <translation>Continue</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="190"/>
+        <location filename="../view/mainwindow.cpp" line="166"/>
         <source>Formatting...</source>
         <translation>Formatting...</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="217"/>
+        <location filename="../view/mainwindow.cpp" line="191"/>
         <source>Done</source>
         <translation>Done</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="223"/>
+        <location filename="../view/mainwindow.cpp" line="197"/>
         <source>Quit</source>
         <translation>Quit</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="224"/>
+        <location filename="../view/mainwindow.cpp" line="198"/>
         <source>Your disk is removed when formatting</source>
         <translation>Your disk is removed when formatting</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="227"/>
+        <location filename="../view/mainwindow.cpp" line="201"/>
         <source>Failed to format the device</source>
         <translation>Failed to format the device</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="228"/>
+        <location filename="../view/mainwindow.cpp" line="202"/>
         <source>Reformat</source>
         <translation>Reformat</translation>
     </message>
@@ -91,16 +93,38 @@
     </message>
 </context>
 <context>
+    <name>QCoreApplication</name>
+    <message>
+        <location filename="../utils/udisksutils.cpp" line="55"/>
+        <source>System Disk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../utils/udisksutils.cpp" line="59"/>
+        <source>%1 Encrypted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../utils/udisksutils.cpp" line="61"/>
+        <source>%1 Volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="108"/>
+        <location filename="../main.cpp" line="112"/>
         <source>Device does not exist</source>
         <translation>Device does not exist</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="117"/>
+        <location filename="../main.cpp" line="121"/>
+        <source>Device is read-only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>Cannot format local device</source>
-        <translation>Cannot format local device</translation>
+        <translation type="vanished">Cannot format local device</translation>
     </message>
 </context>
 <context>
--- usb-device-formatter/translations/usb-device-formatter_zh_CN.ts
+++ usb-device-formatter/translations/usb-device-formatter_zh_CN.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
 <context>
     <name>FinishPage</name>
     <message>
@@ -10,7 +12,7 @@
 <context>
     <name>FormatingPage</name>
     <message>
-        <location filename="../view/formatingpage.cpp" line="43"/>
+        <location filename="../view/formatingpage.cpp" line="47"/>
         <source>Formatting the disk, please wait...</source>
         <translation>正在格式化，请稍候……</translation>
     </message>
@@ -18,22 +20,22 @@
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="../view/mainpage.cpp" line="108"/>
+        <location filename="../view/mainpage.cpp" line="104"/>
         <source>Type</source>
         <translation>类型</translation>
     </message>
     <message>
-        <location filename="../view/mainpage.cpp" line="127"/>
+        <location filename="../view/mainpage.cpp" line="122"/>
         <source>Label</source>
         <translation>卷标</translation>
     </message>
     <message>
-        <location filename="../view/mainpage.cpp" line="136"/>
+        <location filename="../view/mainpage.cpp" line="131"/>
         <source>Quick Format</source>
         <translation>快速格式化</translation>
     </message>
     <message>
-        <location filename="../view/mainpage.cpp" line="153"/>
+        <location filename="../view/mainpage.cpp" line="148"/>
         <source>Formatting will erase all data on the disk.</source>
         <translation>执行格式化后会彻底清空该磁盘内容。</translation>
     </message>
@@ -41,43 +43,43 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../view/mainwindow.cpp" line="85"/>
-        <location filename="../view/mainwindow.cpp" line="108"/>
+        <location filename="../view/mainwindow.cpp" line="88"/>
+        <location filename="../view/mainwindow.cpp" line="106"/>
         <source>Format</source>
         <translation>格式化</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="185"/>
+        <location filename="../view/mainwindow.cpp" line="161"/>
         <source>Continue</source>
         <translation>继续</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="190"/>
+        <location filename="../view/mainwindow.cpp" line="166"/>
         <source>Formatting...</source>
         <translation>格式化中……</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="217"/>
+        <location filename="../view/mainwindow.cpp" line="191"/>
         <source>Done</source>
         <translation>完成</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="223"/>
+        <location filename="../view/mainwindow.cpp" line="197"/>
         <source>Quit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="224"/>
+        <location filename="../view/mainwindow.cpp" line="198"/>
         <source>Your disk is removed when formatting</source>
         <translation>您的磁盘在格式化过程中被移除</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="227"/>
+        <location filename="../view/mainwindow.cpp" line="201"/>
         <source>Failed to format the device</source>
         <translation>格式化失败</translation>
     </message>
     <message>
-        <location filename="../view/mainwindow.cpp" line="228"/>
+        <location filename="../view/mainwindow.cpp" line="202"/>
         <source>Reformat</source>
         <translation>重新格式化</translation>
     </message>
@@ -91,16 +93,38 @@
     </message>
 </context>
 <context>
+    <name>QCoreApplication</name>
+    <message>
+        <location filename="../utils/udisksutils.cpp" line="55"/>
+        <source>System Disk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../utils/udisksutils.cpp" line="59"/>
+        <source>%1 Encrypted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../utils/udisksutils.cpp" line="61"/>
+        <source>%1 Volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="108"/>
+        <location filename="../main.cpp" line="112"/>
         <source>Device does not exist</source>
         <translation>设备不存在</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="117"/>
+        <location filename="../main.cpp" line="121"/>
+        <source>Device is read-only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>Cannot format local device</source>
-        <translation>不能格式化本地设备</translation>
+        <translation type="vanished">不能格式化本地设备</translation>
     </message>
 </context>
 <context>
