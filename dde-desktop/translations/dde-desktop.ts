92333150c7df1f5bc767d1aedc03adb00301a9e5
Gary Wang <wzc782970009@gmail.com>
2019-12-16 10:29:48 +0800

    i18n: update ts files

--- dde-desktop/translations/dde-desktop.ts
+++ dde-desktop/translations/dde-desktop.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
 <context>
     <name>CanvasGridView</name>
     <message>
@@ -27,7 +29,7 @@
         <translation>Wallpaper and Screensaver</translation>
     </message>
     <message>
-        <location filename="../view/canvasgridview.cpp" line="2616"/>
+        <location filename="../view/canvasgridview.cpp" line="2613"/>
         <source>Properties</source>
         <translation>Properties</translation>
     </message>
