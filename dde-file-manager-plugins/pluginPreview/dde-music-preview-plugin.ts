a57e8bbd8183cf0ed7b9692775fa3686dd0a425a
zhangsheng <zhangsheng@uniontech.com>
2020-04-27 11:48:50 +0800

    Title: fix 歌曲预览歌手、专辑未翻译成功，依旧显示英文
    Description: 【KLU-KK】【ARM】【build17】【DDE】音频预览界面显示不正常(17697)
    
    RootCause: 翻译文件生成错误、与文件管理系统调用的qm文件名不同、未安装翻译文件到文件管理系统调用翻译文件的目录
    
    Solution: 重新修改并生成翻译文件、修改pro文件新增安装命令与生成ts文件与qm文件、并完善无歌手或专辑、标题时，为空的问题

---
+++ dde-file-manager-plugins/pluginPreview/dde-music-preview-plugin.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MusicMessageView</name>
+    <message>
+        <location filename="dde-music-preview-plugin/musicmessageview.cpp" line="118"/>
+        <source>Artist：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dde-music-preview-plugin/musicmessageview.cpp" line="121"/>
+        <source>Album：</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
