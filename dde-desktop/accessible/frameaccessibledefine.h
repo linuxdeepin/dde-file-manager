219dbb234d6f9e691213cc470cbadbb760a377f3
zhengyouge <zhengyouge@uniontech.com>
2020-05-08 12:07:33 +0800

    Title:chore add accessible file to project
    Description:
    
    RootCause:Null
    
    Solution:

---
+++ dde-desktop/accessible/frameaccessibledefine.h
@@ -0,0 +1,18 @@
+/**
+ * Copyright (C) 2020 UOS Technology Co., Ltd.
+ *
+ * This is a static file that used to define the UI object name that will used to auto-test
+ **/
+
+#ifndef DESKTOP_FRAME_ACCESSIBLE_DEFINE_H
+#define DESKTOP_FRAME_ACCESSIBLE_DEFINE_H
+
+#include <QString>
+
+static const QString SCREEN_BACKGROUND = "screen_background";
+static const QString OBJ_NAME_CANVAS_GRID_VIEW = "screen_frame";
+static const QString OBJ_NAME_WATER_MASK_FRAME = "water_mask_frame";
+
+static const QString WATER_MASK_LOGO_LABEL = "water_mask_logo";
+static const QString WATER_MASK_TEXT_LABEL = "water_mask_text";
+#endif // DESKTOP_FRAME_ACCESSIBLE_DEFINE_H
