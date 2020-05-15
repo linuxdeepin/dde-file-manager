7accde748cb586bc6bbb2ecdfeee4c948fc507db
Yang Hao <yanghao@uniontech.com>
2020-05-15 12:04:18 +0800

    Title: fix null pointer crash
    Description:
    
    RootCause:Null
    
    Solution:

--- dde-file-manager-lib/models/dfmrootfileinfo.cpp
+++ dde-file-manager-lib/models/dfmrootfileinfo.cpp
@@ -102,8 +102,7 @@ DFMRootFileInfo::DFMRootFileInfo(const DUrl &url) :
         if (pathList.size() == 0) {
             qWarning() << url << "not existed";
             //fix 临时解决方案，彻底解决需要DDiskManager::resolveDeviceNode往下追踪
-            for (int i = 0;i < 20; ++i)
-            {
+            for (int i = 0; i < 20; ++i) {
                 QThread::msleep(50);
 
                 pathList = DDiskManager::resolveDeviceNode("/dev" + url.path().chopped(QString("." SUFFIX_UDISKS).length()), {});
@@ -507,13 +506,10 @@ void DFMRootFileInfo::checkCache()
     //FAT32的卷标编码为ascii，idLabel中取不到中文，这里需要从symlink中取出label
     //symlink中的label编码有问题，这里做进一步处理
     QString idVersion = blk->idVersion().toUpper();
-    if(idVersion == "FAT32")
-    {
-        for (QByteArray ba : blk->symlinks())
-        {
+    if (idVersion == "FAT32") {
+        for (QByteArray ba : blk->symlinks()) {
             QString link(ba);
-            if (link.contains("/by-label/"))
-            {
+            if (link.contains("/by-label/")) {
                 QByteArray t_destByteArray;
                 QByteArray t_tmpByteArray;
                 for (int i = 0; i < ba.size(); i++) {
@@ -630,8 +626,7 @@ bool DFMRootFileInfo::checkMpsStr(const QString &path) const
 {
     Q_D(const DFMRootFileInfo);
 
-    for (QByteArray ba : d->mps)
-    {
+    for (QByteArray ba : d->mps) {
         QString baStr(ba.data());
         if (baStr == path)
             return true;
@@ -655,10 +650,10 @@ bool DFMRootFileInfo::typeCompare(const DAbstractFileInfoPointer &a, const DAbst
         {ItemType::GvfsGPhoto2,  6},
         {ItemType::GvfsGeneric,  7}
     };
-    if (!a->exists()) {
+    if (!a || !a->exists()) {
         return false;
     }
-    if (!b->exists()) {
+    if (!b || !b->exists()) {
         return true;
     }
     return priomap[static_cast<DFMRootFileInfo::ItemType>(a->fileType())] < priomap[static_cast<DFMRootFileInfo::ItemType>(b->fileType())];
