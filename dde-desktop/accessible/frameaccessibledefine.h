56d3a86f9f15b5afa9dbf4c642925b16c889a555
lvwujun <lvwujun@uniontech.com>
2020-05-18 11:05:46 +0800

    Title: feat 添加标签说明
    Description: 添加标签说明
    
    RootCause:Null
    
    Solution: 添加标签说明

--- dde-desktop/accessible/frameaccessibledefine.h
+++ dde-desktop/accessible/frameaccessibledefine.h
@@ -12,11 +12,11 @@
 
 // 使用宏定义，方便国际化操作
 
-#define SCREEN_BACKGROUND QObject::tr("screen_background")
-#define OBJ_NAME_CANVAS_GRID_VIEW QObject::tr("screen_frame")
-#define OBJ_NAME_WATER_MASK_FRAME QObject::tr("water_mask_frame")
+#define SCREEN_BACKGROUND QObject::tr("screen_background") // 设置桌面
+#define OBJ_NAME_CANVAS_GRID_VIEW QObject::tr("screen_frame") // 桌面视图画布
+#define OBJ_NAME_WATER_MASK_FRAME QObject::tr("water_mask_frame") // 桌面水印
 
-#define WATER_MASK_LOGO_LABEL QObject::tr("water_mask_logo")
-#define WATER_MASK_TEXT_LABEL QObject::tr("water_mask_text")
+#define WATER_MASK_LOGO_LABEL QObject::tr("water_mask_logo") // 水印logo
+#define WATER_MASK_TEXT_LABEL QObject::tr("water_mask_text") // 水印文字描述
 
 #endif // DESKTOP_FRAME_ACCESSIBLE_DEFINE_H
