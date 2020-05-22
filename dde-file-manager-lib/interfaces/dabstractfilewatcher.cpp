cc68b6bfc43c5490eedc8eb96843ea33dbd679fc
lvwujun <lvwujun@uniontech.com>
2020-05-22 15:16:42 +0800

    Title: fix bug:27384
    Description:【文件管理器】【5.1.1.39-1】远程连接FTP,列表视图，上边栏目点击排序“大小”，再去点击左侧栏目，崩溃
    
    RootCause:Null
    
    Solution:对共性资源加锁处理，对吃速度的地方 快速处理（这里优化退出时候 排序的问题）

--- dde-file-manager-lib/interfaces/dabstractfilewatcher.cpp
+++ dde-file-manager-lib/interfaces/dabstractfilewatcher.cpp
@@ -168,6 +168,8 @@ DAbstractFileWatcher::DAbstractFileWatcher(DAbstractFileWatcherPrivate &dd,
 {
     Q_ASSERT(url.isValid());
 
+    qDebug() << "add file watcher for: " << url;
+
     d_ptr->url = url;
     DAbstractFileWatcherPrivate::watcherList << this;
 }
