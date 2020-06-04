58a6d42fe88a460d529217638aeba156a5db263e
xushitong <xushitong@uniontech.com>
2020-06-04 15:16:52 +0800

    Type: fix bug#27164 【5.1.1.39-1】挂载光驱，更换用户，点击“卸载卸载/弹出/安全移除”，直接关闭弹出授权提示框，报错
    Description: 切换用户后对已挂载的光盘文件执行弹出操作，提示内容有误
    
    RootCause: 上次解决该问题时遗漏了对弹出操作的处理
    
    Solution:

--- dde-file-manager-lib/controllers/appcontroller.cpp
+++ dde-file-manager-lib/controllers/appcontroller.cpp
@@ -697,9 +697,14 @@ void AppController::actionEject(const QSharedPointer<DFMUrlBaseEvent> &event)
                 }
                 drv->eject({});
                 err |= drv->lastError().isValid();
+                QDBusError dbusError = drv->lastError();
                 if (err) {
-                    qDebug() << "disc eject error: " << drv->lastError().message() << drv->lastError().name() << drv->lastError().type();
-                    QMetaObject::invokeMethod(dialogManager, "showErrorDialog", Qt::QueuedConnection, Q_ARG(QString, tr("Disk is busy, cannot eject now")),  Q_ARG(QString, ""));
+                    // fix bug #27164 用户在操作其他用户挂载上的设备的时候需要进行提权操作，此时需要输入用户密码，如果用户点击了取消，此时返回 QDBusError::Other
+                    // 所以暂时这样处理，处理并不友好。这个 errorType 并不能准确的反馈出用户的操作与错误直接的关系。这里笼统的处理成“设备正忙”也不准确。
+                    if (dbusError.isValid() && dbusError.type() != QDBusError::Other) {
+                        qDebug() << "disc eject error: " << dbusError.message() << dbusError.name() << dbusError.type();
+                        QMetaObject::invokeMethod(dialogManager, "showErrorDialog", Qt::QueuedConnection, Q_ARG(QString, tr("Disk is busy, cannot eject now")),  Q_ARG(QString, ""));
+                    }
                 }
             }
         });
