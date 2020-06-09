256ba7807019915f6bb4dd5978941f56ef213d6c
zhangsheng <zhangsheng@uniontech.com>
2020-06-09 10:26:26 +0800

    Title:chore, more accurately progress for MoveMode
    Description:避免删除文件时，因统计类太慢的原因刷不出进度
    
    RootCause:Null
    
    Solution:增加自行统计文件数量完成的标识

--- dde-file-manager-lib/io/dfilecopymovejob.cpp
+++ dde-file-manager-lib/io/dfilecopymovejob.cpp
@@ -1587,6 +1587,10 @@ void DFileCopyMoveJobPrivate::updateMoveProgress()
         qreal realProgress = qreal(completedFilesCount) / totalCount;
         if (realProgress > lastProgress)
             lastProgress = realProgress;
+    } else if (countStatisticsFinished && totalMoveFilesCount > 0) {
+        qreal realProgress = qreal(completedFilesCount) / totalMoveFilesCount;
+        if (realProgress > lastProgress)
+            lastProgress = realProgress;
     } else {
         if (completedFilesCount < totalMoveFilesCount && totalMoveFilesCount > 0) {
             qreal fuzzyProgress = qreal(completedFilesCount) / totalMoveFilesCount;
@@ -1823,11 +1827,13 @@ void DFileCopyMoveJob::start(const DUrlList &sourceUrls, const DUrl &targetUrl)
     // DFileStatisticsJob 统计数量很慢，自行统计
     QtConcurrent::run([sourceUrls, d] () {
         if (d->mode == MoveMode) {
+            d->countStatisticsFinished = false;
             for (const auto &url : sourceUrls) {
                 QStringList list;
                 FileUtils::recurseFolder(url.toLocalFile(), "", &list);
                 d->totalMoveFilesCount += list.size();
             }
+            d->countStatisticsFinished = true;
         }
     });
 
