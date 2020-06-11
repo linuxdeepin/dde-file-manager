2aa5dfe78627f698dfa6145ea2e8520f9f91d1b5
liyigang <rensi19870226@163.com>
2020-06-11 19:58:55 +0800

    Title:fix 27828 27828 【2353】【桌面专业版】【清华同方】【华为鲲鹏920】【超翔TK630-V050】【 8核台式机】【UOS SP1版】（L主板）UOS系统下使用文件管理器访问远程文件系统异常
    Description:27828 【2353】【桌面专业版】【清华同方】【华为鲲鹏920】【超翔TK630-V050】【 8核台式机】【UOS SP1版】（L主板）UOS系统下使用文件管理器访问远程文件系统异常
    
    RootCause:Null
    
    Solution:在进入ftp下一级目录或者更深层目录，点击其他会卡死，在setrooturl时判断当前的rooturl是否可以访问，不可访问就不去current_file_info->isAncestorsUrl

--- dde-file-manager-lib/interfaces/dfileservices.h
+++ dde-file-manager-lib/interfaces/dfileservices.h
@@ -170,8 +170,8 @@ public:
     //set cursor busy status
     void setCursorBusyState(const bool bbusy);
     //check networkfile is busy(or network is unline)
-    bool checkGvfsMountfileBusy(const DUrl &url);
-    bool checkGvfsMountfileBusy(const DUrl &rootUrl,const QString &rootfilename);
+    bool checkGvfsMountfileBusy(const DUrl &url,const bool showdailog = true);
+    bool checkGvfsMountfileBusy(const DUrl &rootUrl,const QString &rootfilename,const bool showdailog = true);
     //chang rootfile
     void changRootFile(const QList<DAbstractFileInfoPointer> &rootinfo);
     //judge me net work is online
