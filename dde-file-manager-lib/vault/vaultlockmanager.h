e33e50657ff455da76c80d9772d5dc4d8e3148fc
luzhen <luzhen@uniontech.com>
2020-06-17 15:14:58 +0800

    Title:chore 重构保险箱代码
    Description:重构保险箱代码
    
    RootCause:代码冗余
    
    Solution:添加注释、拆分函数等

--- dde-file-manager-lib/vault/vaultlockmanager.h
+++ dde-file-manager-lib/vault/vaultlockmanager.h
@@ -115,6 +115,11 @@ protected:
      */
     quint64 dbusGetSelfTime() const;
 
+    /**
+     * @brief terminateTask 终止拷贝、删除等操作
+     */
+    void terminateTask();
+
 private:
     explicit VaultLockManager(QObject *parent = nullptr);
     QSharedPointer<VaultLockManagerPrivate> d_ptr;
