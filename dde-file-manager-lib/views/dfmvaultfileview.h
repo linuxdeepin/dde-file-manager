6beedb9062783baa5f312686907072616ae394bf
luzhen <luzhen@uniontech.com>
2020-06-17 15:53:17 +0800

    Title:chore 添加备注
    Description:在保险箱主页面视图类添加注释
    
    RootCause:之前未添加注释
    
    Solution:添加注释

--- dde-file-manager-lib/views/dfmvaultfileview.h
+++ dde-file-manager-lib/views/dfmvaultfileview.h
@@ -1,36 +1,50 @@
-/*
- * Copyright (C) 2019 Deepin Technology Co., Ltd.
- *
- * Author:     Gary Wang <wzc782970009@gmail.com>
- *
- * Maintainer: Gary Wang <wangzichong@deepin.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/**
+ ** This file is part of the filemanager project.
+ ** Copyright 2020 luzhen <luzhen@uniontech.com>.
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 #pragma once
 
 #include "dfileview.h"
 #include "interfaces/dfilemenu.h"
 
+/**
+ * @brief The DFMVaultFileView class
+ * The class is vault main view, list files in vault directory.
+ * It will open pages or dialog according to current encrypted
+ * state when setRootUrl invoked.
+ */
 class DFMVaultFileView : public DFileView
 {
     Q_OBJECT
 public:
     explicit DFMVaultFileView(QWidget *parent = nullptr);
 
+    /**
+     * @brief setRootUrl 设置根目录地址
+     * @param url        根目录地址
+     * @return           是否设置成功，如果是page相关的地址，返回false
+     */
     bool setRootUrl(const DUrl &url) override;
 
 protected:
+    /**
+     * @brief eventFilter 事件过滤器，用于获取保险箱操作事件
+     * @param obj
+     * @param event       事件指针
+     * @return
+     */
     bool eventFilter(QObject *obj, QEvent *event) override;
 };
