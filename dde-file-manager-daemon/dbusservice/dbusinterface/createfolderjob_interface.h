93e99119c302f31a29c1f4da3c39d30e986ed4d9
liqiang <liqianga@uniontech.com>
2020-07-10 15:28:09 +0800

    Type:chore:部分警告处理
    Description:chore:部分警告处理
    
    RootCause:Null
    
    Solution:

--- dde-file-manager-daemon/dbusservice/dbusinterface/createfolderjob_interface.h
+++ dde-file-manager-daemon/dbusservice/dbusinterface/createfolderjob_interface.h
@@ -31,7 +31,7 @@ public:
     { return "com.deepin.filemanager.daemon.CreateFolderJob"; }
 
 public:
-    CreateFolderJobInterface(const QString &service, const QString &path, const QDBusConnection &connection, QObject *parent = 0);
+    CreateFolderJobInterface(const QString &service, const QString &path, const QDBusConnection &connection, QObject *parent = nullptr);
 
     ~CreateFolderJobInterface();
 
