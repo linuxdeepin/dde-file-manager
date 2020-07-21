68c6d23f2cdc52cbe040a4a6e7a3f13312194582
zhangyub <zhangyub@uniontech.com>
2020-07-21 19:59:54 +0800

    Title:fix task#22385
    Description:同步dde-dcok,dde-launcher的修复方案
    
    RootCause:Null
    
    Solution:

--- dde-desktop/util/util.cpp
+++ dde-desktop/util/util.cpp
@@ -8,8 +8,30 @@
  **/
 
 #include "util.h"
+#include <QPixmap>
+#include <QPainter>
+#include <QImageReader>
+#include <QCursor>
+
+#include <X11/Xcursor/Xcursor.h>
 
 namespace DesktopUtil
 {
+    QCursor *loadQCursorFromX11Cursor(const char* theme, const char* cursorName, int cursorSize){
+        if (theme == nullptr || cursorName == nullptr || cursorSize <= 0)
+            return nullptr;
 
+        XcursorImages *images = XcursorLibraryLoadImages(cursorName, theme, cursorSize);
+        if (images == nullptr || images->images[0] == nullptr) {
+            qWarning() << "loadCursorFalied, theme =" << theme << ", cursorName=" << cursorName;
+            return nullptr;
+        }
+        const int imgW = images->images[0]->width;
+        const int imgH = images->images[0]->height;
+        QImage img((const uchar*)images->images[0]->pixels, imgW, imgH, QImage::Format_ARGB32);
+        QPixmap pixmap = QPixmap::fromImage(img);
+        QCursor *cursor = new QCursor(pixmap, images->images[0]->xhot, images->images[0]->yhot);
+        delete images;
+        return cursor;
+    }
 }
