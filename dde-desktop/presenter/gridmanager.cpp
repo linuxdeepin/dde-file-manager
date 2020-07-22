67dfd6588ebea900d9a6b3c518d0572512aa77d4
liqianga <liqianga@uniontech.com>
2020-07-22 14:48:39 +0800

    Update gridmanager.cpp

--- dde-desktop/presenter/gridmanager.cpp
+++ dde-desktop/presenter/gridmanager.cpp
@@ -918,9 +918,7 @@ class GridManagerPrivate
 public:
     GridManagerPrivate()
     {
-        if(!m_desktopSettings){
-            m_desktopSettings = new DGioSettings("com.deepin.dde.filemanager.desktop", "/com/deepin/dde/filemanager/desktop/");
-        }
+        m_desktopSettings = new DGioSettings("com.deepin.dde.filemanager.desktop", "/com/deepin/dde/filemanager/desktop/");
         autoArrange = Config::instance()->getConfig(Config::groupGeneral, Config::keyAutoAlign).toBool();
 #ifdef ENABLE_AUTOMERGE  //sp2需求调整，屏蔽自动整理
         autoMerge = Config::instance()->getConfig(Config::groupGeneral, Config::keyAutoMerge, false).toBool();
