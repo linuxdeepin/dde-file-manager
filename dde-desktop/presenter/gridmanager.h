6ede5382e565d2d0a78315014e830daf3cf98023
liqiang <liqianga@uniontech.com>
2020-07-22 14:21:14 +0800

    Type:fea新功能：新增gsetting控制右键菜单显示与隐藏
    Description:新增gsetting控制右键菜单显示与隐藏
    
    RootCause:Null
    
    Solution:

--- dde-desktop/presenter/gridmanager.h
+++ dde-desktop/presenter/gridmanager.h
@@ -155,6 +155,7 @@ public:
     void setCurrentVirtualExpandUrl(const DUrl url);
     DUrl getCurrentVirtualExpandUrl();
     void setCurrentAllItems(const QList<DAbstractFileInfoPointer> &infoList);
+    bool isGsettingShow(const QString targetkey, const bool defaultValue);
 
 public:
     void dump();
