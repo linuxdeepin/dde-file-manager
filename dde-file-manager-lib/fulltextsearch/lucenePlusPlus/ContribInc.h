0a94625521466b4097fd7ffef50103acb9b8b76e
hujianzhong <hujianzhong@uniontech.com>
2020-08-06 16:12:40 +0800

    Title:feat 全文搜索引擎合并到sp3
    Description:全文搜索代码合并到sp3
    
    RootCause:Null
    
    Solution:

---
+++ dde-file-manager-lib/fulltextsearch/lucenePlusPlus/ContribInc.h
@@ -0,0 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2009-2014 Alan Wright. All rights reserved.
+// Distributable under the terms of either the Apache License (Version 2.0)
+// or the GNU Lesser General Public License.
+/////////////////////////////////////////////////////////////////////////////
+
+#ifdef _WIN32
+
+#include "targetver.h"
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+#include <windows.h>
+
+#endif
+
+#include "LuceneContrib.h"
