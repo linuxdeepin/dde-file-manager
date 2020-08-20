eb56d9f58debcd0c8b32ea50c1c5142a0c62de99
xushitong <xushitong@uniontech.com>
2020-08-20 12:39:23 +0800

    Type: chore 添加单元测试
    Description:
    
    RootCause:Null
    
    Solution:

---
+++ dde-file-manager-lib/tests/mediainfo/ut_dfmmediainfo.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+#include "mediainfo/dfmmediainfo.h"
+
+DFM_USE_NAMESPACE
+
+namespace {
+class TestDFMMediaInfo : public testing::Test
+{
+public:
+    void SetUp() override
+    {
+        std::cout << "start TestDFMMediaInfo";
+        info = new DFMMediaInfo("test.mp3", nullptr);
+    }
+
+    void TearDown() override
+    {
+        std::cout << "end TestDFMMediaInfo";
+        delete info;
+        info = nullptr;
+    }
+
+public:
+    DFMMediaInfo *info = nullptr;
+};
+} // namespace
+
+TEST_F(TestDFMMediaInfo, StringPropertyTest)
+{
+    info->startReadInfo();
+    EXPECT_STREQ("", DFMMediaInfo::generalInformation("test.mp3").toStdString().c_str());
+    EXPECT_STREQ("", info->Value("").toStdString().c_str());
+}
