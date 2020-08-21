e831cd3efa3358a3e5fc9aed992af4cfb6de4b07
李义刚 <liyigang@uniontech.com>
2020-08-21 17:22:52 +0800

    Title:feat 单元测试Dloclfilehandler
    Description:
    
    RootCause:Null
    
    Solution:

--- dde-file-manager-lib/tests/io/ut_dfilecopymovejob.cpp
+++ dde-file-manager-lib/tests/io/ut_dfilecopymovejob.cpp
@@ -2,6 +2,7 @@
 #include <QDateTime>
 
 #include "dfilecopymovejob.h"
+#include "dfmglobal.h"
 
 using namespace testing;
 DFM_USE_NAMESPACE
@@ -22,5 +23,18 @@ public:
 };
 
 TEST_F(DFileCopyMoveJobTest,can_job_running) {
+    job->setMode(DFileCopyMoveJob::CopyMode);
+    DUrl urlsour,target;
+    urlsour.setScheme(FILE_SCHEME);
+    target.setScheme(FILE_SCHEME);
+    urlsour.setPath("~/Pictures/Wallpapers");
+    target.setPath("~/Pictures/");
+    if (QThread::currentThread()->loopLevel() <= 0) {
+        // 确保对象所在线程有事件循环
+        job->moveToThread(qApp->thread());
+    }
+    job->start(DUrlList() << urlsour, target);
+    while(!job->isFinished()) {
 
+    }
 }
