7a150c9c18057c71d792401399de82cdbb5d85af
xlk <2823431560@qq.com>
2020-08-31 11:17:58 +0800

    Type:chore (ut-wallpaper-chooser) 单元测试
    Description:
    
    RootCause:Null
    
    Solution:

--- dde-desktop/tests/src/dde-wallpaper-chooser/ut-thumbnailmanager-test.cpp
+++ dde-desktop/tests/src/dde-wallpaper-chooser/ut-thumbnailmanager-test.cpp
@@ -1,70 +1,108 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
+
 #include <QFileInfo>
 #include <QThread>
 #include <QDateTime>
 #include <QDir>
 #include <QFile>
 #include <QPixmap>
+#include <QEventLoop>
+#include <QObject>
+#include <QTimer>
+#include <QtConcurrent>
+
 #define private public
 
 #include "../dde-wallpaper-chooser/thumbnailmanager.h"
 
 
+
+
 using namespace testing;
 
-
 namespace  {
-     class ThumbnailManagerTest : public Test{
+     class ThumbnailManagerTest : public Test {
      public:
-         ThumbnailManagerTest():Test(){
+
+         ThumbnailManagerTest() : Test()
+         {
 
          }
 
-
-         virtual void SetUp() override{
-             Tmanager = new ThumbnailManager(2.1);
+         virtual void SetUp() override
+         {
+             m_manager = new ThumbnailManager(2.1);
          }
 
-         virtual void TearDown() override{
-             delete Tmanager;
+         virtual void TearDown() override
+         {
+             delete m_manager;
          }
 
-         ThumbnailManager* Tmanager;
+         ThumbnailManager *m_manager = nullptr;
      };
 }
 
-TEST_F(ThumbnailManagerTest,scale)
+TEST_F(ThumbnailManagerTest, scale)
 {
-    qreal temp = Tmanager->scale();
-    EXPECT_EQ(temp,Tmanager->m_scale);
+    qreal result = m_manager->scale();
+    EXPECT_EQ(result, m_manager->m_scale);
 }
 
 
-TEST_F(ThumbnailManagerTest,stop)
+TEST_F(ThumbnailManagerTest, stop)
 {
-    Tmanager->stop();
-    EXPECT_TRUE(Tmanager->m_queuedRequests.empty());
+    m_manager->stop();
+    EXPECT_TRUE(m_manager->m_queuedRequests.empty());
 }
 
 
-TEST_F(ThumbnailManagerTest,clear)
+TEST_F(ThumbnailManagerTest, clear_filepath)
 {
-   QFileInfo dir1(Tmanager->m_cacheDir);
-   QThread::sleep(1);
-   Tmanager->clear();
-   QFileInfo dir2(Tmanager->m_cacheDir);
-   QDateTime dt1 = dir1.lastRead();
-   QDateTime dt2 = dir2.lastRead();
-   int dint1 = dt1.toString("ss").toInt();
-   int dint2 = dt2.toString("ss").toInt();
-   EXPECT_EQ(dint1,dint2);
+  m_manager->clear();
+  bool bexist = QFile::exists(m_manager->m_cacheDir);
+  EXPECT_TRUE(bexist);
 }
 
-TEST_F(ThumbnailManagerTest,remove)
+
+TEST_F(ThumbnailManagerTest, remove_byfilepath)
 {
-    Tmanager->remove("my_test");
-    QString file = QDir(Tmanager->m_cacheDir).absoluteFilePath("my_test");
-    ASSERT_FALSE(QFile::exists(file));
+   m_manager->remove("test");
+   QString filepath = QDir(m_manager->m_cacheDir).absoluteFilePath("test");
+   EXPECT_FALSE(QFile::exists(filepath));
 }
+
+TEST_F(ThumbnailManagerTest, processnextreq_byfuture)
+{
+    ASSERT_TRUE(m_manager->m_futureWatcher.isFinished());
+    m_manager->m_queuedRequests << "jjj";
+    QFuture<QPixmap> future1 = m_manager->m_futureWatcher.future();
+    m_manager->processNextReq();
+    QFuture<QPixmap> future2 = m_manager->m_futureWatcher.future();
+    EXPECT_NE(future1, future2);
+}
+
+TEST_F(ThumbnailManagerTest, find_bysize_singnal)
+{
+    int size = m_manager->m_queuedRequests.size();
+    bool bjudge = false;
+    QTimer timer;
+    m_manager->find("test");
+
+    timer.start(2000);
+    QEventLoop loop;
+    QObject::connect(m_manager, &ThumbnailManager::thumbnailFounded, &loop, [&]{
+        EXPECT_NE(size, m_manager->m_queuedRequests.size());
+        bjudge = true;
+        loop.exit();
+    });
+    QObject::connect(&timer, &QTimer::timeout, [&]{
+        timer.stop();
+        loop.exit();
+    });
+    loop.exec();
+    EXPECT_TRUE(bjudge);
+}
+
 
