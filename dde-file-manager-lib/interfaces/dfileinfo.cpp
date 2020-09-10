f85c2ad909e89e0889adaa0274bd71c2632c060c
Yang Hao <yanghao@uniontech.com>
2020-09-10 18:52:08 +0800

    Title: fix bug number 44093 使用大文件读写时，出现短暂桌面黑屏现象
    Description: 该bug是脚本会多次创建删除文件导致崩溃
    
    RootCause:DFileInfo析构了，获取额外属性的线程访问了非法数据
    
    Solution: 对DFileInfo 析构处理，析构了就不必去访问了
    Change-Id: I5295a72cd47affb9fafc14a70e84a103b2edc2c0
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/dde-file-manager/+/4316
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: hujianzhong <hujianzhong@uniontech.com>
    Reviewed-by: liyigang <liyigang@uniontech.com>
    Reviewed-by: liuyangming <liuyangming@uniontech.com>
    Reviewed-by: zhengyouge <zhengyouge@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- dde-file-manager-lib/interfaces/dfileinfo.cpp
+++ dde-file-manager-lib/interfaces/dfileinfo.cpp
@@ -79,6 +79,8 @@ public:
     QQueue<QPair<DUrl, DFileInfoPrivate *>> requestEPFiles;
     QReadWriteLock requestEPFilesLock;
     QSet<DFileInfoPrivate *> dirtyFileInfos;
+    QMutex requestEPCancelLock;
+    bool isCanceled;
 
     void run() override;
     void requestEP(const DUrl &url, DFileInfoPrivate *info);
@@ -189,6 +191,10 @@ void RequestEP::requestEP(const DUrl &url, DFileInfoPrivate *info)
 
 void RequestEP::cancelRequestEP(DFileInfoPrivate *info)
 {
+    requestEPCancelLock.lock();
+    isCanceled = true;
+    requestEPCancelLock.unlock();
+
     dirtyFileInfos << info;
     requestEPFilesLock.lockForRead();
 
@@ -216,7 +222,16 @@ void RequestEP::processEPChanged(const DUrl &url, DFileInfoPrivate *info, const 
     if (info == nullptr) {
         return;
     }
+    //fix bug 44093 和 task 36486 文件的多次创建和删除崩溃
+    if (isCanceled) {
+        return;
+    }
 
+    requestEPCancelLock.lock();
+    if (isCanceled) {
+      	requestEPCancelLock.unlock();
+        return;
+    }
     QVariantHash oldEP;
 
     if (!dirtyFileInfos.contains(info)) {
@@ -237,6 +252,7 @@ void RequestEP::processEPChanged(const DUrl &url, DFileInfoPrivate *info, const 
             info->epInitialized = true;
         }
     }
+    requestEPCancelLock.unlock();
 }
 
 DFileInfoPrivate::DFileInfoPrivate(const DUrl &url, DFileInfo *qq, bool hasCache)
