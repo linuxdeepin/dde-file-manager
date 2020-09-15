ad0ecf71010c89df062e4076e125c619411d6f70
liuyangming <liuyangming@uniontech.com>
2020-09-15 18:01:11 +0800

    chore: 修改书签单元测试，新增bookmarksideberitem相关case
    Description:
    
    RootCause: Null
    
    Solution:
    
    Log:
    Change-Id: I3091407cf1ed4dee401ef54e785edbc39d06d28c
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/dde-file-manager/+/4876
    Reviewed-by: liuyangming <liuyangming@uniontech.com>
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: yanghao <yanghao@uniontech.com>
    Reviewed-by: liuzhangjian <liuzhangjian@uniontech.com>
    Reviewed-by: liyigang <liyigang@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- dde-file-manager-lib/tests/controllers/ut_bookmarkmanager.cpp
+++ dde-file-manager-lib/tests/controllers/ut_bookmarkmanager.cpp
@@ -1,4 +1,8 @@
 #include <controllers/bookmarkmanager.h>
+#include "views/dfmsidebar.h"
+#include "interfaces/dfmsidebaritem.h"
+#include "controllers/dfmsidebarbookmarkitemhandler.h"
+#include "views/dfilemanagerwindow.h"
 #include <dfmevent.h>
 
 #include <QProcess>
@@ -10,6 +14,9 @@
 
 #define BOOKMARK_STR "TestBookMark"
 #define BOOKMARK_NEW_STR "TestNewBookMark"
+
+DFM_USE_NAMESPACE
+
 namespace  {
     class TestBookMarkManager : public testing::Test
     {
@@ -17,26 +24,36 @@ namespace  {
         void SetUp() override
         {
             m_pTester = new BookMarkManager();
+            m_pHandler = new DFMSideBarBookmarkItemHandler();
             std::cout << "start TestBookMarkManager";
             tempDirPath =  QStandardPaths::standardLocations(QStandardPaths::HomeLocation).first() + "/" + BOOKMARK_STR;
-            QProcess::execute("mkdir " + tempDirPath);
         }
         void TearDown() override
         {
-            QProcess::execute("rm -r " + tempDirPath);
+            if (m_pItem) {
+                delete m_pItem;
+                m_pItem = nullptr;
+            }
+
+            delete m_pHandler;
+            m_pHandler = nullptr;
 
             delete m_pTester;
             m_pTester = nullptr;
             std::cout << "end TestBookMarkManager";
         }
     public:
-        BookMarkManager *m_pTester;
+        BookMarkManager *m_pTester = nullptr;
+        DFMSideBarBookmarkItemHandler * m_pHandler = nullptr;
+        DFMSideBarItem *m_pItem = nullptr;
         QString tempDirPath;
     };
 }
 
 TEST_F(TestBookMarkManager, can_touch_a_new_bookmark)
 {
+    QProcess::execute("mkdir " + tempDirPath);
+
     DUrl fileUrl = DUrl::fromLocalFile(tempDirPath);
     QStorageInfo si(tempDirPath);
     QList<QPair<QString, QString>> queryItems;
@@ -50,6 +67,56 @@ TEST_F(TestBookMarkManager, can_touch_a_new_bookmark)
     QSharedPointer<DFMTouchFileEvent> event(new DFMTouchFileEvent(nullptr, bookMarkFrom));
 
     EXPECT_TRUE(m_pTester->touch(event));
+}
+
+TEST_F(TestBookMarkManager, can_create_bookmark_sidebar_item)
+{
+    ASSERT_NE(m_pHandler, nullptr);
+
+    DUrl url = DUrl::fromBookMarkFile(DUrl("file://" + tempDirPath), BOOKMARK_STR);
+    m_pItem = m_pHandler->createItem(url);
+    ASSERT_NE(m_pItem, nullptr);
+    EXPECT_EQ(m_pItem->url(), url);
+}
+
+TEST_F(TestBookMarkManager, can_call_cd_action)
+{
+    ASSERT_NE(m_pHandler, nullptr);
+
+    DFileManagerWindow window;
+    const DFMSideBar *bar = window.getLeftSideBar();
+    DUrl url = DUrl::fromBookMarkFile(DUrl("file://" + tempDirPath), BOOKMARK_STR);
+    m_pItem = m_pHandler->createItem(url);
+    ASSERT_NE(m_pItem, nullptr);
+
+    m_pHandler->cdAction(bar, m_pItem);
+}
+
+TEST_F(TestBookMarkManager, can_call_context_menu)
+{
+    ASSERT_NE(m_pHandler, nullptr);
+
+    DFileManagerWindow window;
+    const DFMSideBar *bar = window.getLeftSideBar();
+    DUrl url = DUrl::fromBookMarkFile(DUrl("file://" + tempDirPath), BOOKMARK_STR);
+    m_pItem = m_pHandler->createItem(url);
+    ASSERT_NE(m_pItem, nullptr);
+
+    QMenu *menu = m_pHandler->contextMenu(bar, m_pItem);
+
+    EXPECT_NE(menu, nullptr);
+    delete menu;
+}
+
+TEST_F(TestBookMarkManager, can_rename_bookmark_sidebar_item)
+{
+    ASSERT_NE(m_pHandler, nullptr);
+
+    DUrl url = DUrl::fromBookMarkFile(DUrl("file://" + tempDirPath), BOOKMARK_STR);
+    m_pItem = m_pHandler->createItem(url);
+    ASSERT_NE(m_pItem, nullptr);
+
+    m_pHandler->rename(m_pItem, BOOKMARK_NEW_STR);
 }
 
 TEST_F(TestBookMarkManager, can_rename_bookmark)
@@ -75,7 +142,9 @@ TEST_F(TestBookMarkManager, can_remove_bookmark)
 {
     DUrl fileUrl = DUrl::fromBookMarkFile(DUrl::fromLocalFile(tempDirPath.replace(BOOKMARK_STR, BOOKMARK_NEW_STR)), BOOKMARK_NEW_STR);
 
-    QSharedPointer<DFMRemoveBookmarkEvent> event(new DFMRemoveBookmarkEvent(nullptr, fileUrl));
+    QSharedPointer<DFMDeleteEvent> event(new DFMDeleteEvent(nullptr, DUrlList{fileUrl}));
 
-    EXPECT_TRUE(m_pTester->removeBookmark(event));
+    EXPECT_TRUE(m_pTester->deleteFiles(event));
+
+    QProcess::execute("rm -rf " + tempDirPath);
 }
