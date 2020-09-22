b11881466ca5341a8567322e6cf23acf8a8f3606
zhangyu <zhangyub@uniontech.com>
2020-09-22 13:52:15 +0800

    Title: fix bug#22277
    Description:
    
    RootCause: Null
    
    Solution: 按Qt组提供的解决方案修改代码
    Change-Id: I57f02f63010678b9e3aca68d29b10e188b75f028
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/dde-file-manager/+/5828
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: wangchunlin <wangchunlin@uniontech.com>
    Reviewed-by: zhengyouge <zhengyouge@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- dde-desktop/view/canvasgridview.cpp
+++ dde-desktop/view/canvasgridview.cpp
@@ -877,6 +877,9 @@ void CanvasGridView::mousePressEvent(QMouseEvent *event)
             }
         }
     }
+    //fix bug#22277 后续影响待观察
+    clearFocus();
+
     update();
 }
 
@@ -1722,7 +1725,9 @@ void CanvasGridView::paintEvent(QPaintEvent *event)
 
 void CanvasGridView::focusInEvent(QFocusEvent *event)
 {
-    QAbstractItemView::focusInEvent(event);
+    //fix bug#22277 后续影响待观察
+    //QAbstractItemView::focusInEvent(event);
+    QAbstractScrollArea::focusInEvent(event);
     itemDelegate()->commitDataAndCloseActiveEditor();
 
     /// set menu actions filter
