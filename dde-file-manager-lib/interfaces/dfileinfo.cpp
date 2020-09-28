51564647d74c9fd0aeddff42c0a060d2d8b4900e
gongheng <gongheng@uniontech.com>
2020-09-28 16:58:20 +0800

    Title:KLU TASK-38720 【4.19自测】【桌面专业版】【SP2】【KLU】【Kirin990】【uos-20-KLU-daliy-20200921-build419016】【文本编辑器】新建文本文档，修改文本文档名称，保存文本文档图标显示异常 （一般＋常用＋必现
    Description:重命名文件时，文件图标显示不对，会出现小锁图表
    
    RootCause:重命名文件时，文件先被删除，此时刷新界面，导致文件图标出现小锁状态。
    
    Solution: 绘制小锁图标时，先判断文件是否存在，如果不存在，不绘制小锁。
    Change-Id: Id3e935a2c3dfa12b4bf8e7ef869acc5d47d3bdb7
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/dde-file-manager/+/6561
    Reviewed-by: luzhen <luzhen@uniontech.com>
    Reviewed-by: lixiang <lixianga@uniontech.com>
    Reviewed-by: liuzhangjian <liuzhangjian@uniontech.com>
    Reviewed-by: <mailman@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- dde-file-manager-lib/interfaces/dfileinfo.cpp
+++ dde-file-manager-lib/interfaces/dfileinfo.cpp
@@ -423,6 +423,13 @@ QList<QIcon> DFileInfo::additionalIcon() const
 
     QList<QIcon> icons;
 
+    // KLU TASK-38720 修复重命名文件时，文件图标有小锁显示的问题，
+    // 当为快捷方式时，有源文件文件不存在的情况，所以增加特殊判断
+    if(!isSymLink()){
+        if(access(fileUrl().toLocalFile().toStdString().c_str(), F_OK) == -1)
+            return icons;
+    }
+
     if (isSymLink()) {
         icons << QIcon::fromTheme("emblem-symbolic-link", DFMGlobal::instance()->standardIcon(DFMGlobal::LinkIcon));
     }
