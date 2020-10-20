8428fdd208afa4c5ae9c2853e7b1bb0544ac813b
luzhen <luzhen@uniontech.com>
2020-10-20 14:47:40 +0800

    Title:fix 保险箱无法拖出计算机图标
    Description:51469 【KLU1022】【文管5.2.0102.42】将计算机图标拖进保险箱，再将计算机拖出到任意目录，系统报错
    
    RootCause:保险箱文件路径没有进行转换
    
    Solution: 转换保险箱路径
    Change-Id: Idf92d341e7fe982934e1f9ff2da23a1fc9b8e651
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/dde-file-manager/+/8066
    Reviewed-by: gongheng <gongheng@uniontech.com>
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: lixiang <lixianga@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- dde-file-manager-lib/io/dfilecopymovejob.cpp
+++ dde-file-manager-lib/io/dfilecopymovejob.cpp
@@ -2181,6 +2181,11 @@ void DFileCopyMoveJob::run()
             source = source.searchedFileUrl();
         }
 
+        //! fix: 将保险箱路径转换为本地路径
+        if (source.isVaultFile()) {
+            source = VaultController::vaultToLocalUrl(source);
+        }
+
         const DAbstractFileInfoPointer &source_info = DFileService::instance()->createFileInfo(nullptr, source);
         if (!source_info) {
             qWarning() << "Url not yet supported: " << source;
