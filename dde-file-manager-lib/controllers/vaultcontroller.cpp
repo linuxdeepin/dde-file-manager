8bb628e69e1a2b6aba037be2bf3fbf4de1d63c1c
gongheng <gongheng@uniontech.com>
2020-10-28 13:48:17 +0800

    Title:fix bug-52351 解锁保险箱时,有时会弹出"挂载点不存在"的提示窗口
    Description:上锁保险箱时,有时会弹出"挂载点不存在"的提示窗口
    
    RootCause:保险箱上锁后，用户由于误操作，在保险箱的挂载目录创建了文件，导致挂载点不为空，
    从而导致保险箱解锁失败。
    
    Solution: 保险箱上锁后，删除挂载点目录；保险箱解锁前，创建挂载点目录。
    Change-Id: Ic625124d89f92599ed2eef47afd8ac56085ba01a
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/dde-file-manager/+/9333
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: luzhen <luzhen@uniontech.com>
    Reviewed-by: lixiang <lixianga@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- dde-file-manager-lib/controllers/vaultcontroller.cpp
+++ dde-file-manager-lib/controllers/vaultcontroller.cpp
@@ -975,6 +975,23 @@ void VaultController::createVault(const DSecureString &passWord, QString lockBas
 
 void VaultController::unlockVault(const DSecureString &passWord, QString lockBaseDir, QString unlockFileDir)
 {
+    // 修复bug-52351
+    // 保险箱解锁前,创建挂载目录
+    QString strPath = makeVaultLocalPath("", VAULT_DECRYPT_DIR_NAME);
+    if(QFile::exists(strPath)) {    // 如果存在,则清空目录
+        QDir dir(strPath);
+        if(!dir.isEmpty()) {
+            QDirIterator dirsIterator(strPath, QDir::AllEntries | QDir::NoDotAndDotDot);
+            while(dirsIterator.hasNext()) {
+                if(!dir.remove(dirsIterator.next())) {
+                    QDir(dirsIterator.filePath()).removeRecursively();
+                }
+            }
+        }
+    } else {    // 如果不存在,则创建目录
+        QDir().mkpath(strPath);
+    }
+
     if (lockBaseDir.isEmpty() || unlockFileDir.isEmpty()) {
         if (state() != Encrypted) {
             emit signalUnlockVault(static_cast<int>(ErrorCode::MountpointNotEmpty));
