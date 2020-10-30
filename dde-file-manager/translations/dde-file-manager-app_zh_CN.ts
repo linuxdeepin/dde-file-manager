9547b5a46567cbaa431a881b07e0813868304956
gongheng <gongheng@uniontech.com>
2020-10-30 10:23:05 +0800

    Title:调整代码位置,方便通用.
    Description:调整代码位置,方便通用.
    
    RootCause:Null
    
    Solution:
    Change-Id: Icae35735e009a206dc2aaa36c4727e6236bf07d2
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/dde-file-manager/+/9625
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: liuzhangjian <liuzhangjian@uniontech.com>
    Reviewed-by: luzhen <luzhen@uniontech.com>
    Reviewed-by: lixiang <lixianga@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- dde-file-manager/translations/dde-file-manager-app_zh_CN.ts
+++ dde-file-manager/translations/dde-file-manager-app_zh_CN.ts
@@ -1,13 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
 <context>
     <name>Application</name>
     <message>
-        <location filename="../main.cpp" line="150"/>
+        <location filename="../main.cpp" line="167"/>
         <source>File Manager</source>
         <translation>文件管理器</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="154"/>
+        <location filename="../main.cpp" line="171"/>
         <source>File Manager is a powerful and easy-to-use file management tool, featured with searching, copying, trash, compression/decompression, file property and other useful functions.</source>
         <translation>文件管理器是一款功能强大的文件管理工具， 它包括搜索、复制、回收站、压缩/解压缩， 文件属性等管理功能。</translation>
     </message>
