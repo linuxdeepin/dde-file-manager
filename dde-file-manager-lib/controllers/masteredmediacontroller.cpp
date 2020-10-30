53c3450016134c62834a0aabdf2618dca3e094a6
Zhang Sheng <zhangsheng@uniontech.com>
2020-10-30 13:45:09 +0800

    Title:fix, jump to home path when close WPS file in optical disk.
    Description:bug-52758. 在光驱中选择一个doc,execel文档，使用wps打开，立即关闭wps,文管自动跳转“主目录”
    
    RootCause:光盘挂载点中的文件可能被删除 (如:wps 打开文件时会创建一个临时文件, 关闭时删除) 导致文件删除信号被触发, 信号传输的路径是 burn 的特殊路径
    
    Solution: 仅对挂载点被删除才使用 burn 的特殊路径,文件删除使用该文件的路径.
    Change-Id: I7232c0f94a158f441c2fd240d5f0aeabafc1dac2
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/dde-file-manager/+/9706
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: zhangsheng <zhangsheng@uniontech.com>
    Reviewed-by: lvwujun <lvwujun@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- dde-file-manager-lib/controllers/masteredmediacontroller.cpp
+++ dde-file-manager-lib/controllers/masteredmediacontroller.cpp
@@ -210,7 +210,17 @@ MasteredMediaFileWatcher::MasteredMediaFileWatcher(const DUrl &url, QObject *par
         d->proxyOnDisk = QPointer<DAbstractFileWatcher>(new DFileWatcher(url_mountpoint.path()));
         d->proxyOnDisk->moveToThread(thread());
         d->proxyOnDisk->setParent(this);
-        connect(d->proxyOnDisk, &DAbstractFileWatcher::fileDeleted, this, [this, url] {emit fileDeleted(url);});
+        connect(d->proxyOnDisk, &DAbstractFileWatcher::fileDeleted, this, [this, url, url_mountpoint] (DUrl deletedUrl) {
+            // 光盘挂载点中的文件可能被删除 (如:wps 打开文件时会创建一个临时文件, 关闭时删除) 导致此信号被触发
+            // 此时若使用 url 会导致意外跳转主目录
+            QString mountPath = url_mountpoint.toLocalFile();
+            QString deletedPath = deletedUrl.toLocalFile();
+            if (QUrl(mountPath) == QUrl(deletedPath)) {
+                emit fileDeleted(url);
+            } else {
+                emit fileDeleted(deletedUrl);
+            }
+        });
     }
     /*
      * blank disc doesn't mount
