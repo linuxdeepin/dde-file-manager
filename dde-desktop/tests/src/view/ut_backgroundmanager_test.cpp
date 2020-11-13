e9f6b0b24639efca3144227674ce51915b1033b0
Wang ChunLin <wangchunlin@uniontech.com>
2020-11-13 10:50:12 +0800

    Title:chore 桌面背景管理类单元测试修改
    Description:
    
    RootCause:Null
    
    Solution:
    Change-Id: I03fcd2409a018cfb42e0b4352feeeb2d6198a4de

--- dde-desktop/tests/src/view/ut_backgroundmanager_test.cpp
+++ dde-desktop/tests/src/view/ut_backgroundmanager_test.cpp
@@ -26,17 +26,14 @@ namespace  {
         }
 
         virtual void SetUp() override {
-            p_preview_true = new BackgroundManager(true, nullptr);
-            p_preview_false = new BackgroundManager(false, nullptr);
+            m_manager = new BackgroundManager(false, nullptr);
         }
 
         virtual void TearDown() override {
-            delete p_preview_true;
-            delete p_preview_false;
+            delete m_manager;
         }
 
-        BackgroundManager * p_preview_true;
-        BackgroundManager * p_preview_false;
+        BackgroundManager * m_manager;
         DWindowManagerHelper* windowManagerHelper = nullptr;
         QVector<ScreenPointer> screens;
         ScreenPointer primaryScreen;
@@ -45,48 +42,41 @@ namespace  {
 
 TEST_F(BackgroundManagerTest, BackgroundManager)
 {
-    EXPECT_EQ(p_preview_true->m_preview, true);
-    EXPECT_EQ(p_preview_false->m_preview, false);
+    EXPECT_EQ(m_manager->m_preview, false);
     ASSERT_NE(primaryScreen, nullptr);
     ASSERT_NE(screens.size(), 0);
 }
 
 TEST_F(BackgroundManagerTest, isEnabled)
 {
-    bool result = windowManagerHelper->windowManagerName() == DWindowManagerHelper::KWinWM || !windowManagerHelper->hasComposite();
-    EXPECT_EQ(p_preview_true->isEnabled(), result);
-    EXPECT_EQ(p_preview_false->isEnabled(), result);
+    bool result = true;
+    EXPECT_EQ(m_manager->isEnabled(), result);
 }
 
 TEST_F(BackgroundManagerTest, visible)
 {
-    p_preview_true->setVisible(true);
-    p_preview_false->setVisible(false);
+    m_manager->setVisible(true);
 
-    EXPECT_EQ(p_preview_true->isVisible(), true);
-    EXPECT_EQ(p_preview_false->isVisible(), false);
+    EXPECT_EQ(m_manager->isVisible(), true);
 
-    for(BackgroundWidgetPointer w: p_preview_true->m_backgroundMap.values()) {
+    for(BackgroundWidgetPointer w: m_manager->m_backgroundMap.values()) {
         EXPECT_EQ(w->isVisible(), true);
-    }
-    for(BackgroundWidgetPointer w: p_preview_false->m_backgroundMap.values()) {
-        EXPECT_EQ(w->isVisible(), false);
     }
 }
 
 TEST_F(BackgroundManagerTest, backgroundWidget)
 {
-    BackgroundWidgetPointer bwp = p_preview_true->backgroundWidget(primaryScreen);
+    BackgroundWidgetPointer bwp = m_manager->backgroundWidget(primaryScreen);
     ASSERT_NE(bwp, nullptr);
-    EXPECT_EQ(bwp->property("isPreview").toBool(), true);
+    EXPECT_EQ(bwp->property("isPreview").toBool(), false);
     EXPECT_EQ(bwp->property("myScreen").toString(), primaryScreen->name());
     EXPECT_EQ(bwp->geometry(), primaryScreen->geometry());
 }
 
 TEST_F(BackgroundManagerTest, allbackgroundWidgets)
 {
-    QMap<ScreenPointer,BackgroundWidgetPointer> bwps = p_preview_true->allbackgroundWidgets();
-    EXPECT_EQ(bwps.size(), p_preview_true->m_backgroundMap.size());
+    QMap<ScreenPointer,BackgroundWidgetPointer> bwps = m_manager->allbackgroundWidgets();
+    EXPECT_EQ(bwps.size(), m_manager->m_backgroundMap.size());
     for(BackgroundWidgetPointer w : bwps) {
         EXPECT_NE(w, nullptr);
 
@@ -94,7 +84,7 @@ TEST_F(BackgroundManagerTest, allbackgroundWidgets)
         ScreenPointer s = bwps.key(w);
         for (ScreenPointer obj : screens){
             if (s->name() == obj->name()){
-                EXPECT_EQ(w->property("isPreview"), true);
+                EXPECT_EQ(w->property("isPreview"), false);
                 EXPECT_EQ(w->property("myScreen").toString(), obj->name());
                 EXPECT_EQ(w->geometry(), obj->geometry());
                 find = true;
@@ -107,10 +97,13 @@ TEST_F(BackgroundManagerTest, allbackgroundWidgets)
 
 TEST_F(BackgroundManagerTest, setBackgroundImage)
 {
-    QString path("imagePathTest");
-    p_preview_true->setBackgroundImage(primaryScreen->name(), path);
+    QString path;
+    m_manager->setBackgroundImage(primaryScreen->name(), path);
 
-    QMap<QString,QString> images = p_preview_true->backgroundImages();
+    path = "imagePathTest";
+    m_manager->setBackgroundImage(primaryScreen->name(), path);
+
+    QMap<QString,QString> images = m_manager->backgroundImages();
 
     EXPECT_FALSE(images.isEmpty());
     if(!images.isEmpty()) {
@@ -118,122 +111,122 @@ TEST_F(BackgroundManagerTest, setBackgroundImage)
         EXPECT_EQ(images.value(primaryScreen->name()), path);
     }
 
-    p_preview_true->setBackgroundImage(QString("testName"), path);
-    images = p_preview_true->backgroundImages();
+    m_manager->setBackgroundImage(QString("testName"), path);
+    images = m_manager->backgroundImages();
     EXPECT_FALSE(images.keys().contains(QString("testName")));
 }
 
 TEST_F(BackgroundManagerTest, onBackgroundBuild)
 {
-    QObject::connect(p_preview_true, &BackgroundManager::sigBackgroundBuilded, [=](int mode){
+    QObject::connect(m_manager, &BackgroundManager::sigBackgroundBuilded, [=](int mode){
         EXPECT_EQ(mode, ScreenMrg->displayMode());
     });
-    p_preview_true->onBackgroundBuild();
-    EXPECT_EQ(p_preview_true->m_backgroundMap.size(), screens.size());
-    ASSERT_EQ(p_preview_true->m_backgroundMap.keys().contains(primaryScreen), true);
-    EXPECT_EQ(p_preview_true->m_backgroundMap.value(primaryScreen)->geometry(), primaryScreen->geometry());
+    m_manager->onBackgroundBuild();
+    EXPECT_EQ(m_manager->m_backgroundMap.size(), screens.size());
+    ASSERT_EQ(m_manager->m_backgroundMap.keys().contains(primaryScreen), true);
+    EXPECT_EQ(m_manager->m_backgroundMap.value(primaryScreen)->geometry(), primaryScreen->geometry());
 }
 
 TEST_F(BackgroundManagerTest, onSkipBackgroundBuild)
 {
-    QObject::connect(p_preview_true, &BackgroundManager::sigBackgroundBuilded, [=](int mode){
+    QObject::connect(m_manager, &BackgroundManager::sigBackgroundBuilded, [=](int mode){
         EXPECT_EQ(mode, ScreenMrg->displayMode());
     });
-    auto oldMap = p_preview_true->m_backgroundMap;
-    p_preview_true->onSkipBackgroundBuild();
-    auto newMap = p_preview_true->m_backgroundMap;
+    auto oldMap = m_manager->m_backgroundMap;
+    m_manager->onSkipBackgroundBuild();
+    auto newMap = m_manager->m_backgroundMap;
     EXPECT_EQ(oldMap, newMap);
+}
+
+TEST_F(BackgroundManagerTest, onWmDbusStarted)
+{
+    QString name("com.deepin.wm");
+    m_manager->onWmDbusStarted(name, QString(), QString());
 }
 
 TEST_F(BackgroundManagerTest, onResetBackgroundImage)
 {
     QString path("imagePathTest");
-    p_preview_true->m_backgroundImagePath[primaryScreen->name()] = path;
+    m_manager->m_backgroundImagePath[primaryScreen->name()] = path;
 
-    auto oldImages = p_preview_true->m_backgroundImagePath;
-    p_preview_true->onResetBackgroundImage();
-    auto newImages = p_preview_true->m_backgroundImagePath;
+    auto oldImages = m_manager->m_backgroundImagePath;
+    m_manager->onResetBackgroundImage();
+    auto newImages = m_manager->m_backgroundImagePath;
     EXPECT_EQ(oldImages, newImages);
-
-    p_preview_false->m_backgroundImagePath[QString("testName")] = path;
-    oldImages = p_preview_false->m_backgroundImagePath;
-    p_preview_false->onResetBackgroundImage();
-    newImages = p_preview_false->m_backgroundImagePath;
-    EXPECT_NE(oldImages, newImages);
 }
 
 TEST_F(BackgroundManagerTest, onRestBackgroundManager)
 {
-    bool unknow = p_preview_true->m_preview || p_preview_true->isEnabled();
-    p_preview_true->onRestBackgroundManager();
+    bool unknow = m_manager->m_preview || m_manager->isEnabled();
+    m_manager->onRestBackgroundManager();
     if(unknow) {
-        EXPECT_NE(p_preview_true->wmInter, nullptr);
-        EXPECT_NE(p_preview_true->gsettings, nullptr);
+        EXPECT_NE(m_manager->wmInter, nullptr);
+        EXPECT_NE(m_manager->gsettings, nullptr);
     } else {
-        EXPECT_EQ(p_preview_true->wmInter, nullptr);
-        EXPECT_EQ(p_preview_true->gsettings, nullptr);
+        EXPECT_EQ(m_manager->wmInter, nullptr);
+        EXPECT_EQ(m_manager->gsettings, nullptr);
     }
 }
 
 TEST_F(BackgroundManagerTest, onScreenGeometryChanged)
 {
-    p_preview_true->onScreenGeometryChanged();
-    for (ScreenPointer sp : p_preview_true->m_backgroundMap.keys()) {
-        BackgroundWidgetPointer bw = p_preview_true->m_backgroundMap.value(sp);
+    m_manager->onScreenGeometryChanged();
+    for (ScreenPointer sp : m_manager->m_backgroundMap.keys()) {
+        BackgroundWidgetPointer bw = m_manager->m_backgroundMap.value(sp);
         ASSERT_NE(bw, nullptr);
         EXPECT_EQ(bw->geometry(), sp->geometry());
+        QRect tmpRect(sp->geometry().x()+10, sp->geometry().y()+10, sp->geometry().width(), sp->geometry().height());
+        bw->setGeometry(tmpRect);
     }
+    m_manager->onScreenGeometryChanged();
 }
 
 TEST_F(BackgroundManagerTest, init)
 {
-    bool unknow = p_preview_true->m_preview || p_preview_true->isEnabled();
-    p_preview_true->init();
+    bool unknow = m_manager->m_preview || m_manager->isEnabled();
+    m_manager->init();
     if(unknow) {
-        EXPECT_NE(p_preview_true->wmInter, nullptr);
-        EXPECT_NE(p_preview_true->gsettings, nullptr);
+        EXPECT_NE(m_manager->wmInter, nullptr);
+        EXPECT_NE(m_manager->gsettings, nullptr);
     } else {
-        EXPECT_EQ(p_preview_true->wmInter, nullptr);
-        EXPECT_EQ(p_preview_true->gsettings, nullptr);
+        EXPECT_EQ(m_manager->wmInter, nullptr);
+        EXPECT_EQ(m_manager->gsettings, nullptr);
     }
 }
 
 TEST_F(BackgroundManagerTest, pullImageSettings)
 {
-    p_preview_true->pullImageSettings();
-    EXPECT_EQ(p_preview_true->m_backgroundImagePath.count(), screens.count());
+    m_manager->pullImageSettings();
+    EXPECT_EQ(m_manager->m_backgroundImagePath.count(), screens.count());
 }
 
 TEST_F(BackgroundManagerTest, getBackgroundFromWm)
 {
-    EXPECT_FALSE(p_preview_true->getBackgroundFromWm(primaryScreen->name()).isEmpty());
-    EXPECT_FALSE(p_preview_false->getBackgroundFromWm(primaryScreen->name()).isEmpty());
+    EXPECT_FALSE(m_manager->getBackgroundFromWm(primaryScreen->name()).isEmpty());
+}
+
+TEST_F(BackgroundManagerTest, getBackgroundFromWmConfig)
+{
+    EXPECT_FALSE(m_manager->getBackgroundFromWmConfig(primaryScreen->name()).isEmpty());
 }
 
 TEST_F(BackgroundManagerTest, getDefaultBackground)
 {
-    EXPECT_FALSE(p_preview_true->getDefaultBackground().isEmpty());
-    EXPECT_FALSE(p_preview_false->getDefaultBackground().isEmpty());
+    EXPECT_FALSE(m_manager->getDefaultBackground().isEmpty());
 }
 
 TEST_F(BackgroundManagerTest, createBackgroundWidget)
 {
-    BackgroundWidgetPointer bwp = p_preview_true->createBackgroundWidget(primaryScreen);
-    EXPECT_EQ(bwp->property("isPreview").toBool(), true);
+    BackgroundWidgetPointer bwp = m_manager->createBackgroundWidget(primaryScreen);
+    EXPECT_EQ(bwp->property("isPreview").toBool(), false);
     EXPECT_EQ(bwp->property("myScreen").toString(), primaryScreen->name());
     EXPECT_EQ(bwp->geometry(), primaryScreen->geometry());
 }
 
 TEST_F(BackgroundManagerTest, sigBackgroundManager)
 {
-    QObject::connect(p_preview_true, &BackgroundManager::sigBackgroundBuilded, [=](int mode) {
+    QObject::connect(m_manager, &BackgroundManager::sigBackgroundBuilded, [=](int mode) {
         EXPECT_EQ(mode, 0);
     });
-    emit p_preview_true->sigBackgroundBuilded(0);
-
-    QObject::connect(p_preview_false, &BackgroundManager::sigBackgroundBuilded, [=](int mode) {
-        EXPECT_EQ(mode, -1);
-    });
-    emit p_preview_false->sigBackgroundBuilded(-1);
-
+    emit m_manager->sigBackgroundBuilded(0);
 }
