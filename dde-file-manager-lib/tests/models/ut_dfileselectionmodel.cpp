e58c251519cf9c08336aac9048853137acb0123b
Xu Shitong <xushitong@uniontech.com>
2020-11-23 17:42:27 +0800

    Title: chore UT of models / add stub module
    Description: unit test
    
    RootCause:Null
    
    Solution: new test cases
    Change-Id: If22b9e4c59226ee5325a546e05c23efeb0f53e00
    
    Former-commit-id: f115d06336f96ed26b98c5d0ac3f686f8f1495b2

--- dde-file-manager-lib/tests/models/ut_dfileselectionmodel.cpp
+++ dde-file-manager-lib/tests/models/ut_dfileselectionmodel.cpp
@@ -1,6 +1,13 @@
 #include "models/dfileselectionmodel.h"
+#include "stub.h"
+#include "addr_pri.h"
 
 #include <gtest/gtest.h>
+
+ACCESS_PRIVATE_FIELD(DFileSelectionModel, QItemSelectionModel::SelectionFlags, m_currentCommand);
+ACCESS_PRIVATE_FIELD(DFileSelectionModel, QTimer, m_timer);
+ACCESS_PRIVATE_FUN(DFileSelectionModel, void(const QItemSelection &selection, QItemSelectionModel::SelectionFlags command), select);
+ACCESS_PRIVATE_FUN(DFileSelectionModel, void(), clear);
 
 namespace {
 class TestDFileSelectionModel : public testing::Test
@@ -16,6 +23,7 @@ public:
     void TearDown() override
     {
         std::cout << "end TestDFileSelectionModel";
+        delete model;
     }
 
 public:
@@ -23,17 +31,65 @@ public:
 };
 } // namespace
 
-TEST_F(TestDFileSelectionModel, isSelected)
+TEST_F(TestDFileSelectionModel, tstConstructWithParent)
 {
-    EXPECT_FALSE(model->isSelected(QModelIndex()));
+    auto m = new DFileSelectionModel(nullptr, nullptr);
+    delete m;
 }
 
-TEST_F(TestDFileSelectionModel, selectedCount)
+TEST_F(TestDFileSelectionModel, tstIsSelected)
 {
+    auto &cmd = access_private_field::DFileSelectionModelm_currentCommand(*model);
+    cmd = QItemSelectionModel::SelectionFlags(QItemSelectionModel::Current);
+    QModelIndex idx;
+    EXPECT_FALSE(model->isSelected(idx));
+    cmd = QItemSelectionModel::SelectionFlags(QItemSelectionModel::Current
+                                              | QItemSelectionModel::Rows
+                                              | QItemSelectionModel::ClearAndSelect);
+    EXPECT_FALSE(model->isSelected(idx));
+}
+
+
+TEST_F(TestDFileSelectionModel, tstSelectedCount)
+{
+    auto &cmd = access_private_field::DFileSelectionModelm_currentCommand(*model);
+    cmd = QItemSelectionModel::SelectionFlags(QItemSelectionModel::Current
+                                              | QItemSelectionModel::Rows
+                                              | QItemSelectionModel::ClearAndSelect);
+    EXPECT_EQ(0, model->selectedCount());
+    cmd = QItemSelectionModel::SelectionFlags(QItemSelectionModel::Current
+                                              | QItemSelectionModel::Rows);
     EXPECT_EQ(0, model->selectedCount());
 }
 
-TEST_F(TestDFileSelectionModel, selectedIndexes)
+TEST_F(TestDFileSelectionModel, tstSelectedIndexes)
 {
+    auto &cmd = access_private_field::DFileSelectionModelm_currentCommand(*model);
+    cmd = QItemSelectionModel::SelectionFlags(QItemSelectionModel::Current
+                                              | QItemSelectionModel::Rows
+                                              | QItemSelectionModel::ClearAndSelect);
     EXPECT_EQ(0, model->selectedIndexes().count());
+    cmd = QItemSelectionModel::SelectionFlags(QItemSelectionModel::Current
+                                              | QItemSelectionModel::Rows);
+    EXPECT_EQ(0, model->selectedIndexes().count());
+}
+
+TEST_F(TestDFileSelectionModel, tstSelect)
+{
+    QItemSelection selection;
+    QItemSelectionModel::SelectionFlags cmd = QItemSelectionModel::SelectionFlags(QItemSelectionModel::Current
+                                                                                  | QItemSelectionModel::Rows);;
+    auto &timer = access_private_field::DFileSelectionModelm_timer(*model);
+    timer.start();
+    call_private_fun::DFileSelectionModelselect(*model, selection, cmd);
+
+    cmd = QItemSelectionModel::SelectionFlags(QItemSelectionModel::Current
+                                                  | QItemSelectionModel::Rows
+                                                  | QItemSelectionModel::ClearAndSelect);
+    call_private_fun::DFileSelectionModelselect(*model, selection, cmd);
+}
+
+TEST_F(TestDFileSelectionModel, tstClear)
+{
+    call_private_fun::DFileSelectionModelclear(*model);
 }
---
+++ third-party/cpp-stub/stub.pri
@@ -0,0 +1,31 @@
+INCLUDEPATH += $$PWD
+
+#isEmpty(GOOGLETEST_DIR):GOOGLETEST_DIR=$$(GOOGLETEST_DIR)
+
+#isEmpty(GOOGLETEST_DIR) {
+#    warning("Using googletest src dir specified at Qt Creator wizard")
+#    message("set GOOGLETEST_DIR as environment variable or qmake variable to get rid of this message")
+#    GOOGLETEST_DIR = $$PWD
+#}
+
+#!isEmpty(GOOGLETEST_DIR): {
+#    GTEST_SRCDIR = $$GOOGLETEST_DIR/googletest
+#    GMOCK_SRCDIR = $$GOOGLETEST_DIR/googlemock
+#}
+
+#requires(exists($$GTEST_SRCDIR):exists($$GMOCK_SRCDIR))
+
+#!exists($$GOOGLETEST_DIR):message("No googletest src dir found - set GOOGLETEST_DIR to enable.")
+
+
+#INCLUDEPATH *= \
+#    $$GTEST_SRCDIR \
+#    $$GTEST_SRCDIR/include \
+#    $$GMOCK_SRCDIR \
+#    $$GMOCK_SRCDIR/include
+
+SOURCES += $$PWD/elfio.hpp
+
+HEADERS += $$PWD/addr_any.h \
+            $$PWD/addr_pri.h \
+            $$PWD/stub.h
