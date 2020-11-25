cb3ace77a3b8a66dadc9ad29834bfcb5be5fadf8
Zhang Sheng <zhangsheng@uniontech.com>
2020-11-25 13:05:56 +0800

    Title:chore, 为了便于单元测试，将部分私有类声明转移到头文件中
    Description:
    
    RootCause:Null
    
    Solution:
    Change-Id: I76ccfa1449a27e66cf1d8ac587b438e57440febc
    
    Former-commit-id: c29ebd5879daf3749528358303df2b510cd382c7

---
+++ dde-file-manager-lib/interfaces/vfs/private/dfmvfsdevice_p.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2019 ~ 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     zhangsheng <zhangsheng@uniontech.com>
+ *
+ * Maintainer: zhangsheng <zhangsheng@uniontech.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef DFMVFSDEVICE_P_H
+#define DFMVFSDEVICE_P_H
+
+#include "dfmvfsdevice.h"
+#include "private/dfmgiowrapper_p.h"
+
+DFM_BEGIN_NAMESPACE
+
+class DFMVfsDevicePrivate
+{
+    Q_DECLARE_PUBLIC(DFMVfsDevice)
+
+public:
+    explicit DFMVfsDevicePrivate(const QUrl &url, void *gmountObjectPtr, DFMVfsDevice *qq);
+    explicit DFMVfsDevicePrivate(const QUrl &url, DFMVfsDevice *qq);
+    ~DFMVfsDevicePrivate();
+
+    GMount* createGMount() const;
+    GFile* createRootFile() const;
+    GFileInfo* createRootFileInfo() const;
+
+    GMount* getGMount() const; // promise will always return a value, or throw an exception
+    GFile* getGFile() const; // promise will always return a value, or throw an exception
+    GFileInfo* getGFileInfo() const; // promise will always return a value, or throw an exception
+
+    static QStringList getThemedIconName(GThemedIcon *icon);
+
+    static GMountOperation *GMountOperationNewMountOp(DFMVfsDevice *devicePtr);
+    static void GMountOperationAskPasswordCb(GMountOperation *op, const char *message, const char *default_user, const char *default_domain, GAskPasswordFlags flags, gpointer vfsDevicePtr);
+    static void GMountOperationAskQuestionCb(GMountOperation *op, const char *message, const GStrv choices, gpointer vfsDevicePtr);
+    static void GFileMountDoneCb(GObject *object, GAsyncResult *res, gpointer vfsDevicePtr);
+    static void GFileUnmountDoneCb(GObject *object, GAsyncResult *res, gpointer vfsDevicePtr);
+
+    QString m_setupUrl; // url used to setup this DFMVfsDevice.
+    QPointer<QEventLoop> m_eventLoop; // to make async event sync.
+
+    DFMVfsAbstractEventHandler *m_handler = nullptr;
+    QPointer<QThread> m_threadOfEventHandler;
+
+    DFMVfsDevice *q_ptr = nullptr;
+
+private:
+    mutable DFMGMount c_GMount; // cached value, default null, mountpoint file.
+    mutable DFMGFile c_GFile; // cached value, default null, mountpoint file.
+    mutable DFMGFileInfo c_GFileInfo; // cached value, default null, mountpoint fileinfo.
+};
+
+DFM_END_NAMESPACE
+
+#endif // DFMVFSDEVICE_P_H
---
+++ dde-file-manager-lib/interfaces/vfs/private/dfmvfsmanager_p.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2019 ~ 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     zhangsheng <zhangsheng@uniontech.com>
+ *
+ * Maintainer: zhangsheng <zhangsheng@uniontech.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef DFMVFSMANAGER_P_H
+#define DFMVFSMANAGER_P_H
+
+#include "dfmvfsmanager.h"
+#include "dfmvfsdevice.h"
+#include "private/dfmgiowrapper_p.h"
+
+DFM_BEGIN_NAMESPACE
+
+class DFMVfsManagerPrivate
+{
+    Q_DECLARE_PUBLIC(DFMVfsManager)
+
+public:
+    explicit DFMVfsManagerPrivate(DFMVfsManager *qq);
+    ~DFMVfsManagerPrivate();
+
+    QScopedPointer<GVolumeMonitor, ScopedPointerGObjectUnrefDeleter> m_GVolumeMonitor;
+    DFMVfsAbstractEventHandler *m_handler = nullptr;
+    QPointer<QThread> m_threadOfEventHandler;
+
+    static void GVolumeMonitorMountAddedCb(GVolumeMonitor *, GMount *mount, DFMVfsManager* managerPointer);
+    static void GVolumeMonitorMountRemovedCb(GVolumeMonitor *, GMount *mount, DFMVfsManager* managerPointer);
+    static void GVolumeMonitorMountChangedCb(GVolumeMonitor *, GMount *mount, DFMVfsManager* managerPointer);
+
+
+    DFMVfsManager *q_ptr = nullptr;
+
+private:
+    void initConnect();
+};
+
+DFM_END_NAMESPACE
+
+#endif // DFMVFSMANAGER_P_H
