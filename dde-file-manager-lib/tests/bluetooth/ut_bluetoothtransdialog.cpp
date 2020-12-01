e303878d2b8221b25f1929b581da88a5338fcea8
Xu Shitong <xushitong@uniontech.com>
2020-12-01 15:07:40 +0800

    Title: chore ut for blutoothtransdialog, coverage reach up to 82.8%
    Description: and fix some cases which might cause crash problem.
    
    RootCause:Null
    
    Solution: new cases and more nullptr check.
    Change-Id: Id65d7408d1b860381b4c643b5dd6d7cd57f13fc5
    
    Former-commit-id: aff66138268e2e9bbadb67c7a60f46f05027fae0

--- dde-file-manager-lib/tests/bluetooth/ut_bluetoothtransdialog.cpp
+++ dde-file-manager-lib/tests/bluetooth/ut_bluetoothtransdialog.cpp
@@ -3,6 +3,9 @@
 #include <QStackedWidget>
 #include <QTimer>
 #include <DGuiApplicationHelper>
+#include <DListView>
+
+#include "stub.h"
 
 DGUI_USE_NAMESPACE
 
@@ -11,6 +14,26 @@ DGUI_USE_NAMESPACE
 
 #include "bluetooth/bluetoothtransdialog.h"
 #include "bluetooth/bluetoothmanager.h"
+#include "bluetooth/bluetoothadapter.h"
+#include "bluetooth/bluetoothmodel.h"
+
+namespace BluetoothUnitTest {
+    QMap<QString, const BluetoothAdapter *> (*adapters_stub)(void *) = [](void *) {
+        BluetoothAdapter *adapter = new BluetoothAdapter;
+        adapter->setId("1");
+        adapter->setName("local");
+        adapter->setPowered(true);
+
+        BluetoothDevice *dev = new BluetoothDevice;
+        dev->setId("1234");
+        dev->setIcon("phone");
+        adapter->addDevice(dev);
+
+        QMap<QString, const BluetoothAdapter *> map;
+        map.insert(adapter->id(), adapter);
+        return map;
+    };
+}
 
 namespace {
 class TestBluetoothTransDialog : public testing::Test
@@ -85,19 +108,72 @@ TEST_F(TestBluetoothTransDialog, tstLambdaSlots)
     });
     loop.exec();
 
+    dlg->m_currSessionPath = "123";
     BluetoothManager::instance()->transferCancledByRemote("123");
+    dlg->m_currSessionPath = "123";
     BluetoothManager::instance()->transferCancledByRemote("1234");
 
+    dlg->m_currSessionPath = "123";
     BluetoothManager::instance()->transferFailed("123", "123", "123");
+    dlg->m_currSessionPath = "123";
     BluetoothManager::instance()->transferFailed("1234", "123", "123");
 
     dlg->m_urls << "test";
     dlg->m_finishedUrls.clear();
+    dlg->m_currSessionPath = "123";
     BluetoothManager::instance()->fileTransferFinished("123", "123");
+    dlg->m_currSessionPath = "123";
     BluetoothManager::instance()->fileTransferFinished("1234", "123");
 
     BluetoothManager::instance()->transferEstablishFinish("123", "11234");
     BluetoothManager::instance()->transferEstablishFinish("", "11234");
 
     DGuiApplicationHelper::instance()->themeTypeChanged(DGuiApplicationHelper::ColorType::UnknownType);
+    DGuiApplicationHelper::instance()->themeTypeChanged(DGuiApplicationHelper::ColorType::DarkType);
+}
+
+TEST_F(TestBluetoothTransDialog, tstFindItemByIdRole)
+{
+    BluetoothDevice *dev = nullptr;
+    EXPECT_TRUE(nullptr == dlg->findItemByIdRole(dev));
+    EXPECT_TRUE(nullptr == dlg->findItemByIdRole("testNullptr"));
+}
+
+TEST_F(TestBluetoothTransDialog, tstSendFilesToDevice)
+{
+    void (*sendFiles_stub)(void *, QString, QStringList) = [](void *, QString, QStringList) {;};
+    Stub st;
+    st.set(ADDR(BluetoothModel, adapters), BluetoothUnitTest::adapters_stub);
+    st.set(static_cast<void(BluetoothManager::*)(const QString &, const QStringList &)>(&BluetoothManager::sendFiles), sendFiles_stub);
+
+    dlg->m_finishedUrls << "/usr/bin/dde-file-manager";
+    dlg->m_urls << "/usr/bin/dde-file-manager" << "/usr/bin/dde-desktop";
+    dlg->sendFilesToDevice("1234");
+    dlg->sendFilesToDevice("12345");
+
+    dlg->initConn();
+    emit BluetoothManager::instance()->model()->adapterAdded(nullptr);
+    dlg->updateDeviceList();
+
+    BluetoothDevice *dev = new BluetoothDevice;
+    dev->setId("testDev");
+    dlg->addDevice(dev);
+    dlg->removeDevice("testDev");
+
+    BluetoothAdapter *adapter = new BluetoothAdapter;
+    adapter->setId("1");
+    adapter->addDevice(dev);
+    BluetoothManager::instance()->model()->adapterRemoved(adapter);
+    BluetoothManager::instance()->model()->adapterRemoved(nullptr);
+
+    dlg->getStyledItem(nullptr);
+    dev->setPaired(true);
+    dev->setIcon("phone");
+    dev->setState(BluetoothDevice::StateConnected);
+    dlg->getStyledItem(dev);
+
+    delete dev;
+    delete adapter;
+
+    dlg->close();
 }
