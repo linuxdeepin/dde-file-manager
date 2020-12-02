edd81c899e9e71c012540e84378ea1379b7620e6
ChunLin Wang <wangchunlin@uniontech.com>
2020-12-02 18:21:47 +0800

    Title:chore 提高lib库都view文件夹中的单元测试覆盖率
    Description:
    
    RootCause:Null
    
    Solution:
    Change-Id: If0d0400ddf9cf4c542a3e68810684d09238c8be9
    
    Former-commit-id: d5420af47cd1cda0d8f7d1c88150d95237e9ddca

--- dde-file-manager-lib/tests/views/ut_dfmaddressbar.cpp
+++ dde-file-manager-lib/tests/views/ut_dfmaddressbar.cpp
@@ -8,8 +8,13 @@
 #include <durl.h>
 #include <dspinner.h>
 #include "views/dcompleterlistview.h"
+#include "interfaces/dfmcrumbbar.h"
+#include "controllers/searchhistroymanager.h"
+#include "stub.h"
 
 #define private public
+#define protected public
+
 #include "views/dfmaddressbar.h"
 DFM_USE_NAMESPACE
 
@@ -89,6 +94,7 @@ TEST_F(DFMAddressBarTest,end_play_animation)
 {
     ASSERT_NE(nullptr,m_bar);
 
+    m_bar->playAnimation();
     m_bar->stopAnimation();
     EXPECT_EQ(nullptr, m_bar->animationSpinner);
 }
@@ -112,7 +118,14 @@ TEST_F(DFMAddressBarTest,call_init_ui)
 TEST_F(DFMAddressBarTest,call_init_connections)
 {
     ASSERT_NE(nullptr,m_bar);
-    m_bar->initConnections();
+    emit m_bar->indicator->triggered();
+
+    m_bar->setText("");
+    emit m_bar->returnPressed();
+    m_bar->setText("dfmvault:/home");
+    emit m_bar->returnPressed();
+
+    emit m_bar->selectionChanged();
 }
 
 TEST_F(DFMAddressBarTest,set_in_dicator)
@@ -138,6 +151,10 @@ TEST_F(DFMAddressBarTest,call_do_complete)
 {
     ASSERT_NE(nullptr,m_bar);
 
+    Stub stub;
+    void (*ut_showMe)() = [](){};
+    stub.set(ADDR(DCompleterListView, showMe), ut_showMe);
+
     m_bar->doComplete();
     EXPECT_NE(nullptr, m_bar->completerView);
 }
@@ -152,7 +169,16 @@ TEST_F(DFMAddressBarTest,clear_completer_model)
 TEST_F(DFMAddressBarTest,update_completion_state)
 {
     ASSERT_NE(nullptr,m_bar);
+    Stub stub;
+    void (*ut_setStringList)() = [](){};
+    stub.set(ADDR(QStringListModel, setStringList), ut_setStringList);
 
+    DFMCrumbBar bar;
+    m_bar->setParent(&bar);
+
+    m_bar->updateCompletionState(QString(""));
+    m_bar->updateCompletionState(QString("/home"));
+    m_bar->setParent(nullptr);
 }
 
 TEST_F(DFMAddressBarTest,append_complete_model)
@@ -165,4 +191,124 @@ TEST_F(DFMAddressBarTest,append_complete_model)
     stringList << QString("test1") << QString("test2");
     m_bar->appendToCompleterModel(stringList);
     EXPECT_GE(m_bar->completerModel.rowCount(), 2);
+}
+
+TEST_F(DFMAddressBarTest,tst_insert_completion)
+{
+    ASSERT_NE(nullptr,m_bar);
+    m_bar->insertCompletion(QString(""));
+
+    m_bar->urlCompleter->setWidget(m_bar);
+    m_bar->insertCompletion(QString("/"));
+}
+
+TEST_F(DFMAddressBarTest,tst_completion_highlighted)
+{
+    ASSERT_NE(nullptr,m_bar);
+    m_bar->onCompletionHighlighted(QString(""));
+}
+
+TEST_F(DFMAddressBarTest,tst_completion_modelCount_changed)
+{
+    ASSERT_NE(nullptr,m_bar);
+    m_bar->onCompletionModelCountChanged();
+}
+
+TEST_F(DFMAddressBarTest,tst_text_edited)
+{
+    ASSERT_NE(nullptr,m_bar);
+
+    DFMCrumbBar bar;
+    m_bar->setParent(&bar);
+
+    m_bar->onTextEdited(QString(""));
+    m_bar->onTextEdited(QString("/"));
+    m_bar->setParent(nullptr);
+}
+
+TEST_F(DFMAddressBarTest,focus_InOut_event)
+{
+    ASSERT_NE(nullptr,m_bar);
+
+    QFocusEvent eventIn(QEvent::FocusIn);
+    m_bar->focusInEvent(&eventIn);
+
+    QFocusEvent eventOut1(QEvent::FocusOut, Qt::OtherFocusReason);
+    m_bar->focusOutEvent(&eventOut1);
+
+    QFocusEvent eventOut2(QEvent::FocusOut, Qt::NoFocusReason);
+    m_bar->focusOutEvent(&eventOut2);
+}
+
+TEST_F(DFMAddressBarTest,key_press_event)
+{
+    ASSERT_NE(nullptr,m_bar);
+
+    Stub stub;
+    static bool myVisible = true;
+    bool (*ut_isVisible)() = [](){return myVisible;};
+    stub.set(ADDR(QWidget, isVisible), ut_isVisible);
+
+    bool (*ut_removeSearchHistory)() = [](){return true;};
+    stub.set(ADDR(SearchHistroyManager, removeSearchHistory), ut_removeSearchHistory);
+
+    void (*ut_setStringList)() = [](){};
+    stub.set(ADDR(QStringListModel, setStringList), ut_setStringList);
+
+    QKeyEvent keyPressEvt_Esc(QEvent::KeyPress, Qt::Key_Escape, Qt::NoModifier);
+    m_bar->keyPressEvent(&keyPressEvt_Esc);
+
+    bool tempModel = m_bar->isHistoryInCompleterModel;
+    m_bar->isHistoryInCompleterModel = true;
+    QKeyEvent keyPressEvt_Del(QEvent::KeyPress, Qt::Key_Delete, Qt::ShiftModifier);
+    m_bar->keyPressEvent(&keyPressEvt_Del);
+    m_bar->isHistoryInCompleterModel = tempModel;
+    stub.reset(ADDR(SearchHistroyManager, removeSearchHistory));
+    stub.reset(ADDR(QStringListModel, setStringList));
+
+    QKeyEvent keyPressEvt_BackTab(QEvent::KeyPress, Qt::Key_Backtab, Qt::NoModifier);
+    m_bar->keyPressEvent(&keyPressEvt_BackTab);
+
+    QKeyEvent keyPressEvt_Return(QEvent::KeyPress, Qt::Key_Return, Qt::NoModifier);
+    m_bar->keyPressEvent(&keyPressEvt_Return);
+
+    QKeyEvent keyPressEvt_Tab(QEvent::KeyPress, Qt::Key_Tab, Qt::NoModifier);
+    m_bar->keyPressEvent(&keyPressEvt_Tab);
+
+    QKeyEvent keyPressEvt_Down(QEvent::KeyPress, Qt::Key_Down, Qt::NoModifier);
+    m_bar->keyPressEvent(&keyPressEvt_Down);
+
+    myVisible = false;
+    m_bar->keyPressEvent(&keyPressEvt_Tab);
+    m_bar->keyPressEvent(&keyPressEvt_Return);
+    m_bar->keyPressEvent(&keyPressEvt_Del);
+}
+
+TEST_F(DFMAddressBarTest,tst_paint_event)
+{
+    ASSERT_NE(nullptr,m_bar);
+
+    QPaintEvent event(QRect(0, 0, 200, 200));
+    m_bar->setText("");
+    m_bar->playAnimation();
+    m_bar->paintEvent(&event);
+}
+
+TEST_F(DFMAddressBarTest,tst_show_event)
+{
+    ASSERT_NE(nullptr,m_bar);
+    m_bar->show();
+}
+
+TEST_F(DFMAddressBarTest,input_method_event)
+{
+    ASSERT_NE(nullptr,m_bar);
+
+    Stub stub;
+    bool (*ut_hasSelectedText)() = [](){return true;};
+    stub.set(ADDR(QLineEdit, hasSelectedText), ut_hasSelectedText);
+
+    QInputMethodEvent event;
+    m_bar->inputMethodEvent(&event);
+
 }
