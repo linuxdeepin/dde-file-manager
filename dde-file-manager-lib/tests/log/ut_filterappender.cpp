0c5df3a37de05f4e9399201125914689caea4cee
mozart <luzhen@uniontech.com>
2020-12-03 14:35:54 +0800

    Title:chore 补充log单元测试
    Description:
    
    RootCause:Null
    
    Solution:
    Change-Id: I26bb177c2afaef24dddba7e2f0b94821fb9a6958
    
    Former-commit-id: d33ebe0739450572b41609d60bf6655b25730b13

--- dde-file-manager-lib/tests/log/ut_filterappender.cpp
+++ dde-file-manager-lib/tests/log/ut_filterappender.cpp
@@ -2,10 +2,16 @@
 #include <gmock/gmock-matchers.h>
 
 #include "QtTest/QTest"
+#include "Logger.h"
 
+#define protected public
+#define private public
 #include "log/filterAppender.h"
+#undef protected
+#undef private
 
 
+DCORE_USE_NAMESPACE
 namespace  {
     class TestFilterAppender : public testing::Test
     {
@@ -44,15 +50,72 @@ TEST_F(TestFilterAppender, log_file_limits)
 
 TEST_F(TestFilterAppender, tst_filter)
 {
-    QString filter = "something";
-    m_appender->addFilter(filter);
+    QString filter1 = "something1";
+    QString filter2 = "something2";
+    m_appender->addFilter(filter1);
+    m_appender->addFilter(filter2);
 
     QStringList filters = m_appender->getFilters();
     EXPECT_TRUE(filters.size() > 0);
-    EXPECT_EQ(filters.first(), filter);
+    EXPECT_EQ(filters.first(), filter1);
+
+    m_appender->removeFilter(filter1);
+    EXPECT_TRUE(m_appender->getFilters().size() == 1);
 
     m_appender->clearFilters();
     EXPECT_TRUE(m_appender->getFilters().size() == 0);
+}
+
+TEST_F(TestFilterAppender, tst_append)
+{
+    m_appender->setDatePattern(FilterAppender::HourlyRollover);
+    m_appender->m_rollOverTime = QDateTime::fromTime_t(0);
+    EXPECT_NO_FATAL_FAILURE(m_appender->append(QDateTime::currentDateTime(),
+                                               Logger::Trace, "", 1, nullptr, "", "outputmessage"));
+
+    m_appender->setDatePattern(FilterAppender::HalfDailyRollover);
+    m_appender->m_rollOverTime = QDateTime::fromTime_t(0);
+    EXPECT_NO_FATAL_FAILURE(m_appender->append(QDateTime::currentDateTime(),
+                                               Logger::Trace, "", 1, nullptr, "", "outputmessage"));
+
+    m_appender->setDatePattern(FilterAppender::DailyRollover);
+    m_appender->m_rollOverTime = QDateTime::fromTime_t(0);
+    EXPECT_NO_FATAL_FAILURE(m_appender->append(QDateTime::currentDateTime(),
+                                               Logger::Trace, "", 1, nullptr, "", "outputmessage"));
+
+    m_appender->setDatePattern(FilterAppender::WeeklyRollover);
+    m_appender->m_rollOverTime = QDateTime::fromTime_t(0);
+    EXPECT_NO_FATAL_FAILURE(m_appender->append(QDateTime::currentDateTime(),
+                                               Logger::Trace, "", 1, nullptr, "", "outputmessage"));
+
+    m_appender->setDatePattern(FilterAppender::MonthlyRollover);
+    m_appender->m_rollOverTime = QDateTime::fromTime_t(0);
+    EXPECT_NO_FATAL_FAILURE(m_appender->append(QDateTime::currentDateTime(),
+                                               Logger::Trace, "", 1, nullptr, "", "outputmessage"));
+
+    QString filter = "something";
+    m_appender->addFilter(filter);
+
+    EXPECT_NO_FATAL_FAILURE(m_appender->append(QDateTime::currentDateTime(),
+                                               Logger::Trace, "", 1, nullptr, "", filter));
+}
+
+TEST_F(TestFilterAppender, remove_old_files)
+{
+    m_appender->setLogFilesLimit(-1);
+    EXPECT_NO_FATAL_FAILURE(m_appender->removeOldFiles());
+
+    m_appender->setLogFilesLimit(2);
+    EXPECT_NO_FATAL_FAILURE(m_appender->removeOldFiles());
+}
+
+TEST_F(TestFilterAppender, tst_computeFrequency)
+{
+    EXPECT_NO_FATAL_FAILURE(m_appender->setDatePattern("'.'yyyy-MM-dd-hh-mm-zzz"));
+    EXPECT_NO_FATAL_FAILURE(m_appender->setDatePattern("'.'yyyy-MM-dd-hh-mm"));
+    EXPECT_NO_FATAL_FAILURE(m_appender->setDatePattern("'.'yyyy-MM-dd-hh"));
+    EXPECT_NO_FATAL_FAILURE(m_appender->setDatePattern("'.'yyyy-MM-dd"));
+    EXPECT_NO_FATAL_FAILURE(m_appender->setDatePattern("'.'yyyy-MM"));
 }
 
 
