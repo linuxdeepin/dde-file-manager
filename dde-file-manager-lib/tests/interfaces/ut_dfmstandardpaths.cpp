86ee727de5833e3b23e3849ef024ef1aa64cd6c3
yngty <yanghao@uniontech.com>
2020-12-08 20:21:14 +0800

    Title: chore: add dfmstandardpaths ut
    Description:
    
    RootCause:Null
    
    Solution:
    Change-Id: I99e6907131f39496c06a6c21dee148d79cfe5003
    
    Former-commit-id: ec62480f98c35a4ad8d827a17c8cf85eff1795df

--- dde-file-manager-lib/tests/interfaces/ut_dfmstandardpaths.cpp
+++ dde-file-manager-lib/tests/interfaces/ut_dfmstandardpaths.cpp
@@ -1,10 +1,14 @@
-#include "interfaces/dfmstandardpaths.h"
-
 #include <gtest/gtest.h>
 
 #include <QDir>
 #include <durl.h>
 #include <QDebug>
+#include <QApplication>
+
+#define private public
+#include "interfaces/dfmstandardpaths.h"
+#undef private
+
 
 namespace
 {
@@ -23,7 +27,7 @@ namespace
     };
 }
 
-TEST_F(DFMStandardPathsTest, location)
+TEST_F(DFMStandardPathsTest, test_location)
 {
     QString homePath = QDir::homePath();
     QString trashPath = DFMStandardPaths::location(DFMStandardPaths::StandardLocation::TrashPath);
@@ -52,11 +56,28 @@ TEST_F(DFMStandardPathsTest, location)
     ASSERT_EQ("/", rootPath);
     QString vaultPath = DFMStandardPaths::location(DFMStandardPaths::StandardLocation::Vault);
     ASSERT_EQ(DFMVAULT_ROOT, vaultPath);
+    QString translationPath = DFMStandardPaths::location(DFMStandardPaths::StandardLocation::TranslationPath);
+    ASSERT_EQ(translationPath, qApp->applicationDirPath() + "/translations");
+    QString templatesPath = DFMStandardPaths::location(DFMStandardPaths::StandardLocation::TemplatesPath);
+    ASSERT_EQ(templatesPath, qApp->applicationDirPath() + "/templates");
+    QString mimeTypePath = DFMStandardPaths::location(DFMStandardPaths::StandardLocation::MimeTypePath);
+    ASSERT_EQ(mimeTypePath, qApp->applicationDirPath() + "/mimetypes");
+    QString pluginsPath = DFMStandardPaths::location(DFMStandardPaths::StandardLocation::PluginsPath);
+    ASSERT_EQ(pluginsPath, QString::fromLocal8Bit(PLUGINDIR).split(':').last());
+    QString applicationConfigPath = DFMStandardPaths::location(DFMStandardPaths::StandardLocation::ApplicationConfigPath);
+    ASSERT_EQ(applicationConfigPath, QString("%1/%2/deepin/%3").arg(QDir::homePath(), ".config", QMAKE_TARGET));
+    QString applicationSharePath = DFMStandardPaths::location(DFMStandardPaths::StandardLocation::ApplicationSharePath);
+    ASSERT_EQ(applicationSharePath, APPSHAREDIR);
+    QString networkRootPath = DFMStandardPaths::location(DFMStandardPaths::StandardLocation::NetworkRootPath);
+    ASSERT_EQ(networkRootPath, NETWORK_ROOT);
+    QString userShareRootPath = DFMStandardPaths::location(DFMStandardPaths::StandardLocation::UserShareRootPath);
+    ASSERT_EQ(userShareRootPath, USERSHARE_ROOT);
+    QString computerRootPath = DFMStandardPaths::location(DFMStandardPaths::StandardLocation::ComputerRootPath);
+    ASSERT_EQ(computerRootPath, COMPUTER_ROOT);
 }
 
-TEST_F(DFMStandardPathsTest, fromStandardUrl)
+TEST_F(DFMStandardPathsTest, test_fromStandardUrl)
 {
-
     QString homeStandardPath = DFMStandardPaths::fromStandardUrl(DUrl("standard://home"));
     QString homePath = QDir::homePath();
     ASSERT_EQ(homeStandardPath, homePath);
@@ -64,7 +85,13 @@ TEST_F(DFMStandardPathsTest, fromStandardUrl)
     ASSERT_EQ(videosStandardPath, homePath + "/Videos");
 }
 
-TEST_F(DFMStandardPathsTest, toStandardUrl)
+TEST_F(DFMStandardPathsTest, test_fromStandardUrl_no_standard)
+{
+    QString noStandardPath = DFMStandardPaths::fromStandardUrl(DUrl("file://videos"));
+    ASSERT_TRUE(noStandardPath.isEmpty());
+}
+
+TEST_F(DFMStandardPathsTest, test_toStandardUrl)
 {
     QString homePath = QDir::homePath();
     DUrl homeStandardUrl = DFMStandardPaths::toStandardUrl(homePath);
@@ -73,3 +100,29 @@ TEST_F(DFMStandardPathsTest, toStandardUrl)
     ASSERT_EQ(videosStandardUrl, DUrl("standard://videos"));
 }
 
+TEST_F(DFMStandardPathsTest, test_getConfigPath)
+{
+    QString configPath;
+    QString projectName;
+#ifndef QMAKE_TARGET
+#define QMAKE_TARGET "test-dde-file-manager"
+    projectName = QMAKE_TARGET;
+    configPath = DFMStandardPaths::getConfigPath();
+#undef QMAKE_TARGET
+#else
+    projectName = QMAKE_TARGET;
+    configPath = DFMStandardPaths::getConfigPath();
+#endif
+    QFile file(configPath);
+    ASSERT_TRUE(file.exists());
+    ASSERT_EQ(configPath,  QString("%1/%2/deepin/%3").arg(QDir::homePath(), ".config", projectName));
+    file.remove();
+}
+
+TEST_F(DFMStandardPathsTest, test_DFMStandardPaths)
+{
+    DFMStandardPaths *p = new DFMStandardPaths();
+    ASSERT_NE(p, nullptr);
+    delete p;
+    p= nullptr;
+}
