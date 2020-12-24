b8d1e0bdd03a1707d87d80a3ee2f5946eb3a7122
Zhang Sheng <zhangsheng@uniontech.com>
2020-12-24 12:20:31 +0800

    Title:fix, 非开发者模式下，选择任意文件夹右键-管理员打开，没有反应
    Description:bug-59150
    [前置条件]
    
    非开发者模式下
    [步骤]
    
    1.选择任意文件夹右键-以管理员打开，观察现象
    [结果]
    
    1.没有任何反应
    [期望]
    
    1.出现提示或者影藏此选项
    
    RootCause: 非开发者模式未隐藏文件夹右键菜单中的“管理员打开”
    
    Solution: 文件夹右键菜单增加对非开发者模式的判断，以移除选项
    Change-Id: Iab020be887932410898ddc898906b4b582f1a190
    
    Former-commit-id: b062096ec684bcd372906c10390e9bb92263a2ae

--- dde-file-manager-lib/interfaces/dfilemenumanager.cpp
+++ dde-file-manager-lib/interfaces/dfilemenumanager.cpp
@@ -239,8 +239,8 @@ DFileMenu *DFileMenuManager::createNormalMenu(const DUrl &currentUrl, const DUrl
             actions.removeAll(MenuAction::TagFilesUseColor);
         }
 
-        // sp3 feature: root用户和服务器版本用户不需要以管理员身份打开的功能
-        if (DFMGlobal::isRootUser() || DFMGlobal::isServerSys()) {
+        // sp3 feature: root用户, 服务器版本用户, 非开发者模式均不需要以管理员身份打开的功能
+        if (DFMGlobal::isRootUser() || DFMGlobal::isServerSys() || !DFMGlobal::isDeveloperMode()) {
             actions.removeAll(MenuAction::OpenAsAdmin);
         }
 
