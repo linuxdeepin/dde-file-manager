33da8d9854c693d8dfeb9804db6bd71d0e5124b7
liuzhangjian <liuzhangjian@uniontech.com>
2020-12-28 11:31:03 +0800

    Title:chore 修改cppcheck检测脚本
    Description:添加dde-file-manager-lib/tests/目录过滤
    
    RootCause:Null
    
    Solution:
    Change-Id: Iaacdc6f0112f74ad4d373f11ce63ce7eea349e79
    
    Former-commit-id: a5f34dd2a4e02558caa5896bffabd7fa21164c67

--- tests/cppcheck-prj-running.sh
+++ tests/cppcheck-prj-running.sh
@@ -17,9 +17,9 @@ echo "start dde-file-manager cppcheck case"
 FILTER_SYMBOL=" -i "
 DDE_DESKTOP_FILTER_PATH=$FILTER_SYMBOL$PROJECT_FOLDER/dde-desktop/tests/
 DDE_FILE_MANAGER_FILTER_PATH=$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager/tests/
-DDE_FILE_MANAGER_LIB_FILTER_PATH=$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-lib/3rdParty/$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-lib/search/fsearch/$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-lib/vault/openssl/$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-lib/vault/qrencode/
+DDE_FILE_MANAGER_LIB_FILTER_PATH=$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-lib/tests/$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-lib/3rdParty/$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-lib/search/fsearch/$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-lib/vault/openssl/$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-lib/vault/qrencode/
 DDE_FILE_MANAGER_DAEMON_FILTER_PATH=$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-daemon/tests/
-DDE_FILE_MANAGER_PLUGINS_FILTER_PATH=$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-plugins/pluginPreview/dde-image-preview-plugin/tests$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-plugins/pluginPreview/dde-music-preview-plugin/tests$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-plugins/pluginPreview/dde-pdf-preview-plugin/tests$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-plugins/pluginPreview/dde-text-preview-plugin/tests$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-plugins/pluginPreview/dde-video-preview-plugin/tests$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-plugins/pluginView/tests
+DDE_FILE_MANAGER_PLUGINS_FILTER_PATH=$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-plugins/pluginPreview/dde-image-preview-plugin/tests/$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-plugins/pluginPreview/dde-music-preview-plugin/tests/$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-plugins/pluginPreview/dde-pdf-preview-plugin/tests/$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-plugins/pluginPreview/dde-text-preview-plugin/tests/$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-plugins/pluginPreview/dde-video-preview-plugin/tests/$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-manager-plugins/pluginView/tests/
 DDE_DOCK_PLUGINS_FILTER_PATH=$FILTER_SYMBOL$PROJECT_FOLDER/dde-dock-plugins/disk-mount/tests/
 DDE_FILE_THUMBNAIL_TOOL_FILTER_PATH=$FILTER_SYMBOL$PROJECT_FOLDER/dde-file-thumbnail-tool/video/tests/
 DEEPIN_ANYTHING_SERVER_PLUGINS_FILTER_PATH=$FILTER_SYMBOL$PROJECT_FOLDER/deepin-anything-server-plugins/dde-anythingmonitor/tests/
@@ -28,9 +28,9 @@ OTHER_FILTER_PATH=$FILTER_SYMBOL$BUILD_DIR$FILTER_SYMBOL$PROJECT_FOLDER/third-pa
 # report目录
 REPORT_FILE_PATH=$BUILD_DIR/cppcheck_dde-file-manager.xml
 # 打印cppcheck命令
-echo "cppcheck --enable=all --output-file="$REPORT_FILE_PATH "--xml" $DDE_DESKTOP_FILTER_PATH $DDE_FILE_MANAGER_FILTER_PATH $DDE_FILE_MANAGER_LIB_FILTER_PATH $DDE_FILE_MANAGER_DAEMON_FILTER_PATH $DDE_FILE_MANAGER_PLUGINS $DDE_DOCK_PLUGINS_FILTER_PATH $DDE_FILE_THUMBNAIL_TOOL_FILTER_PATH $DEEPIN_ANYTHING_SERVER_PLUGINS $OTHER_FILTER_PATH $PROJECT_FOLDER
+echo "cppcheck --enable=all --output-file="$REPORT_FILE_PATH "--xml" $DDE_DESKTOP_FILTER_PATH $DDE_FILE_MANAGER_FILTER_PATH $DDE_FILE_MANAGER_LIB_FILTER_PATH $DDE_FILE_MANAGER_DAEMON_FILTER_PATH $DDE_FILE_MANAGER_PLUGINS_FILTER_PATH $DDE_DOCK_PLUGINS_FILTER_PATH $DDE_FILE_THUMBNAIL_TOOL_FILTER_PATH $DEEPIN_ANYTHING_SERVER_PLUGINS_FILTER_PATH $OTHER_FILTER_PATH $PROJECT_FOLDER
 # 执行cppcheck命令
-cppcheck --enable=all --output-file=$REPORT_FILE_PATH --xml $DDE_DESKTOP_FILTER_PATH $DDE_FILE_MANAGER_FILTER_PATH $DDE_FILE_MANAGER_LIB_FILTER_PATH $DDE_FILE_MANAGER_DAEMON_FILTER_PATH $DDE_FILE_MANAGER_PLUGINS $DDE_DOCK_PLUGINS_FILTER_PATH $DDE_FILE_THUMBNAIL_TOOL_FILTER_PATH $DEEPIN_ANYTHING_SERVER_PLUGINS $OTHER_FILTER_PATH $PROJECT_FOLDER
+cppcheck --enable=all --output-file=$REPORT_FILE_PATH --xml $DDE_DESKTOP_FILTER_PATH $DDE_FILE_MANAGER_FILTER_PATH $DDE_FILE_MANAGER_LIB_FILTER_PATH $DDE_FILE_MANAGER_DAEMON_FILTER_PATH $DDE_FILE_MANAGER_PLUGINS_FILTER_PATH $DDE_DOCK_PLUGINS_FILTER_PATH $DDE_FILE_THUMBNAIL_TOOL_FILTER_PATH $DEEPIN_ANYTHING_SERVER_PLUGINS_FILTER_PATH $OTHER_FILTER_PATH $PROJECT_FOLDER
 
 echo "end dde-file-manager cppcheck case"
 
