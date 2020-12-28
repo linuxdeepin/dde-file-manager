c89fd3777745b3c160e6a859fa5ab84053bab278
liuzhangjian <liuzhangjian@uniontech.com>
2020-12-28 17:06:06 +0800

    Title:fix bug#59106 最近使用目录搜索结果错误
    Description:最近使用目录搜索结果错误
    
    RootCause:全文搜索不支持虚拟目录下的搜索，判断是虚拟目录后没有return
    
    Solution: 如果是在虚拟目录下搜索，全文搜索直接return
    Change-Id: Ibede758a879d94fb3d53788a8417fd304821ac29
    
    Former-commit-id: 1a17db79055250d84bdd74010e92fefe616f3a4b

--- dde-file-manager-lib/controllers/searchcontroller.cpp
+++ dde-file-manager-lib/controllers/searchcontroller.cpp
@@ -468,6 +468,7 @@ bool SearchDiriterator::hasNext() const
         DAbstractFileInfoPointer fileInfo = fileService->createFileInfo(nullptr, targetUrl);
         if (fileInfo->isVirtualEntry()) {
             hasExecuteFullTextSearch = true;
+            return true;
         }
 
         QString searchPath = fileInfo->filePath();
