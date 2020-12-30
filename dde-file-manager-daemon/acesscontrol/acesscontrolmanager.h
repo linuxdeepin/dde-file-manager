6d074ad6d3de893178f00aacf763617136af2cd0
Zhang Sheng <zhangsheng@uniontech.com>
2020-12-30 13:46:40 +0800

    fix: 非开发者模式，U盘格式化为ext3/ext4后，无法创建文件、文件夹
    Description: 设备被挂载后添加写权限，日志记录行为
    
    Log: 恢复设备挂载后修改权限的代码
    Bug: https://pms.uniontech.com/zentao/bug-view-60788.html
    Change-Id: I947f0e8e5d63831369aaa5f0ae9326f5d83a8f38
    
    Former-commit-id: e69c9884388506e30599f632fbb0fcd95b1409be

--- dde-file-manager-daemon/acesscontrol/acesscontrolmanager.h
+++ dde-file-manager-daemon/acesscontrol/acesscontrolmanager.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2020 ~ 2021 Deepin Technology Co., Ltd.
+ *
+ * Author:     zhangsheng <zhangsheng@uniontech.com>
+ *
+ * Maintainer: zhangsheng <zhangsheng@uniontech.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef ACESSCONTROLMANAGER_H
 #define ACESSCONTROLMANAGER_H
 
@@ -27,6 +48,7 @@ protected:
 
 private slots:
     void onFileCreated(const QString &path, const QString &name);
+    void chmodMountpoints(const QString &blockDevicePath, const QByteArray &mountPoint);
 
 private:
     AcessControlAdaptor *m_acessControlAdaptor = nullptr;
