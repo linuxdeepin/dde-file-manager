87168c7a9932085f3b6075a0501c6d3ffc2ba255
吕伍军 <lvwujun@uniontech.com>
2021-01-27 10:32:15 +0800

    fix: 多分区U盘，访问加密分区，输入正确的密码，仍然会提是没有没有密钥解锁设备
    实际上U盘分区已经成功解密并挂载了，但gio底层抛出操作false导致问题。当前阶段先根据挂载点解决问题，后续将研究gio彻底解决问题
    
    Log: 解密成功不应该弹出错误信息
    Bug: https://pms.uniontech.com/zentao/bug-view-62158.html
    Change-Id: I7c12ffad6fb6213814197fa21dd1033ac407f4f2
    
    Former-commit-id: 5d086794e32657c987f4ac67acbb08e8714064d6

--- src/dde-file-manager-lib/gvfs/gvfsmountmanager.h
+++ src/dde-file-manager-lib/gvfs/gvfsmountmanager.h
@@ -161,6 +161,9 @@ public:
 
     void autoMountAllDisks();
 
+private:
+    static bool try_to_get_mounted_point(GVolume *volume);
+
 signals:
     void loadDiskInfoFinished();
     void mount_added(const QDiskInfo& diskInfo);
