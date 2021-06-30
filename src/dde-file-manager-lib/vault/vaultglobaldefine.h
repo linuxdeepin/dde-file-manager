36871f666a8e6be8ddaaaf20801a4c4e3f13d39e
lixiang <lixianga@uniontech.com>
2021-06-30 14:10:10 +0800

    fix: 非首次开启保险箱进入的时候，点击信息栏，上锁时间字段值应该显示为最近一次上锁的时间，而不是访问时间
    记录上锁时间的文件出现两个导致没有访问到正确上锁时间
    
    Log: 访问上锁时间的文件访问与写入的文件不是同一个
    Bug: https://pms.uniontech.com/zentao/bug-view-85686.html
    Change-Id: Ib48420e7d3b14cc8d3978c77ad099fd086360c45

--- src/dde-file-manager-lib/vault/vaultglobaldefine.h
+++ src/dde-file-manager-lib/vault/vaultglobaldefine.h
@@ -54,7 +54,8 @@
 #define GET_COMPRESSOR_PID_SHELL(x)     QString("ps -xo pid,cmd | grep /usr/bin/deepin-compressor | grep ")+x+QString(" | grep -v grep | awk '{print $1}'")
 
 //! 保险箱时间配置文件
-#define VAULT_TIME_CONFIG_FILE          QString("/../dde-file-manager/vaultTimeConfig.json")
+#define VAULT_TIME_CONFIG_FILE          QString("/../dde-file-manager/vaultTimeConfig")
+#define VAULT_TIME_CONFIG_FILE_MIME     QString("/../dde-file-manager/vaultTimeConfig.json")
 
 enum EN_VaultState {
     NotExisted = 0,
