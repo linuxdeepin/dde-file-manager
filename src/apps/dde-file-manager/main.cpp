bb1aeafa751889ba7f60baf188c7a61ac512432f
Zhang Sheng <zhangsheng@uniontech.com>
2021-08-10 11:28:47 +0800

    refactor: init project
    初始化项目目录结构
    
    Log:
    Change-Id: Ib6cb1ed876c59c6734a7fdfe04508f45ff8960d1

---
+++ src/apps/dde-file-manager/main.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2020 ~ 2021 Uniontech Software Technology Co., Ltd.
+ *
+ * Author:     zhangsheng<zhangsheng@uniontech.com>
+ *
+ * Maintainer: max-lv<lvwujun@uniontech.com>
+ *             lanxuesong<lanxuesong@uniontech.com>
+ *             xushitong<xushitong@uniontech.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QDebug>
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug() << "Hello, dde-file-manager.";
+
+    return a.exec();
+}
