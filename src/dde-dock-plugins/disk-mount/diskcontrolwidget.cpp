ad313fa635c13762d07c5f15d3d466220678452f
lanxuesong <lanxuesong@uniontech.com>
2021-09-11 14:01:53 +0800

    fix: 修复特定条件下加密盘挂载问题
    已经解锁一次的加密盘，在重新挂载时就不需要秘钥了，如果这时重启dock就会自动挂载上，所以需要做排查处理
    
    Log: 优化加密盘挂载流程
    Change-Id: Ic1f39929635145eacd2778158de0af0e5e63cc9a

--- src/dde-dock-plugins/disk-mount/diskcontrolwidget.cpp
+++ src/dde-dock-plugins/disk-mount/diskcontrolwidget.cpp
@@ -170,6 +170,8 @@ void DiskControlWidget::doStartupAutoMount()
         }
 
         if (blDev->isEncrypted()) continue;
+        // 已经解锁一次的加密盘，在重新挂载时就不需要秘钥了，如果这时重启dock就会自动挂载上，所以这里扔需要排除加密明文设备
+        if (blDev->cryptoBackingDevice().length() > 1) continue;
         if (blDev->hintIgnore()) continue;
 
         QList<QByteArray> mountPoints = blDev->mountPoints();
