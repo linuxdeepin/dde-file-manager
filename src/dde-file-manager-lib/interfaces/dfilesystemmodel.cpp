5ed54411193639e09cb34baa82969245b4b68823
liyigang <liyigang@uniontech.com>
2022-01-12 10:32:11 +0800

    fix: 【AMD，ARM】文件默认排序规则非按自然数正序排列
    修改排序算法时，判断条件修改错误。修改为正确的判断条件
    
    Log: 修改排序反了
    Bug: https://pms.uniontech.com/zentao/bug-view-110942.html
    Change-Id: I13af7291871128b793e12a01c00fab8c53461539

--- src/dde-file-manager-lib/interfaces/dfilesystemmodel.cpp
+++ src/dde-file-manager-lib/interfaces/dfilesystemmodel.cpp
@@ -59,6 +59,8 @@
 static int FindInsertPosInOrderList(const FileSystemNodePointer &needNode,
         const QList<FileSystemNodePointer> &list, const DAbstractFileInfo::CompareFunction &sortFun,
         const Qt::SortOrder &order, const bool *isCancel){
+    if (!sortFun)
+        return list.count();
     int begin = 0;
     int end = list.count();
     int row = (begin + end)/2;
@@ -74,7 +76,7 @@ static int FindInsertPosInOrderList(const FileSystemNodePointer &needNode,
 
         const FileSystemNodePointer &node = list.at(row);
         if (isDir) {
-            if (node->fileInfo->isDir() && sortFun(needNode->fileInfo, node->fileInfo, order)) {
+            if (node->fileInfo->isDir() && !sortFun(needNode->fileInfo, node->fileInfo, order)) {
                 begin = row;
                 row = (end + begin + 1) / 2 ;
                 if (row >= end)
@@ -84,7 +86,7 @@ static int FindInsertPosInOrderList(const FileSystemNodePointer &needNode,
                 row = (end + begin) / 2 ;
             }
         } else {
-            if (node->fileInfo->isDir() || sortFun(needNode->fileInfo, node->fileInfo, order)) {
+            if (node->fileInfo->isDir() || !sortFun(needNode->fileInfo, node->fileInfo, order)) {
                 begin = row;
                 row = (end + begin + 1) / 2 ;
                 if (row >= end)
@@ -767,7 +769,7 @@ begin:
         }
         const DUrl &fileUrl = fileInfo->fileUrl();
 
-        if (v.first == AddFile || v.first == AppendFile) {
+        if (fileInfo->hasOrderly() && (v.first == AddFile || v.first == AppendFile)) {
             if (rootNode->childContains(fileUrl))
                 continue;
 
@@ -2963,7 +2965,6 @@ void DFileSystemModel::addFile(const DAbstractFileInfoPointer &fileInfo)
         beginInsertRows(createIndex(parentNode, 0), row == -1 ? parentNode->childrenCount() : row,
                         row == -1 ? parentNode->childrenCount() : row);
 
-        
         parentNode->insertChildren(row == -1 ? parentNode->childrenCount() : row, fileUrl, node, d->rootNodeManager->isInsertCaches());
 
         endInsertRows();
