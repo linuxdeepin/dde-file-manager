4372a4d4e1848f5f86600aa3a9d4e0c50fea4c81
Zhang Yu <zhangyub@uniontech.com>
2022-03-21 14:53:51 +0800

    task: 修改desktop文件获取路径
    修改desktop文件路径支持环境变量XDG_DATA_DIRS
    
    Log: 支持XDG_DATA_DIRS设置应用目录
    
    Task: https://pms.uniontech.com/task-view-99991.html
    Bug: https://pms.uniontech.com/bug-view-111903.html
    Influence: 桌面，文管
    Change-Id: Ie4073a8414246f7d113d08d2cf73bfc4c54b84e8

--- src/dde-file-manager-lib/shutil/mimesappsmanager.cpp
+++ src/dde-file-manager-lib/shutil/mimesappsmanager.cpp
@@ -558,12 +558,16 @@ QStringList MimesAppsManager::getrecommendedAppsFromMimeWhiteList(const DUrl &ur
 QStringList MimesAppsManager::getApplicationsFolders()
 {
     QStringList desktopFolders;
-    desktopFolders << QString("/usr/share/applications/")
-                   << QString("/usr/local/share/applications/")
-                   << QString("/usr/share/gnome/applications/")
-                   << QString("/var/lib/flatpak/exports/share/applications")
-                   << QDir::homePath() + QString("/.local/share/flatpak/exports/share/applications")
-                   << QDir::homePath() + QString( "/.local/share/applications" );
+    desktopFolders << QString("/usr/share/applications")
+                   << QString("/usr/local/share/applications")
+                   << QString("/usr/share/gnome/applications");
+    // env for XDG_DATA_DIRS
+    for (const QString &path : QStandardPaths::standardLocations(QStandardPaths::ApplicationsLocation)) {
+      if (desktopFolders.contains(path))
+          continue;
+      desktopFolders.append(path);
+    }
+
     return desktopFolders;
 }
 
