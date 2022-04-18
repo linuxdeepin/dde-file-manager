c796ae851634e5557abbb21f9d97a6835a11b34b
李想 <lixianga@uniontech.com>
2022-04-18 14:39:30 +0800

    fix: 1051专业版主线在回收站预览txt文件，文管崩溃
    回收站文件路径特殊，未进行处理导致，文件打开失败从而引起奔溃。在文档预览插件中修改获取文件信息的方式
    
    Log: 修改预览插件，优化功能交互
    Bug: https://pms.uniontech.com/bug-view-125793.html
    Change-Id: Id878c4d711fcad0f68d4f936087c39f06c683463

--- tests/dde-file-manager-plugins/pluginPreview/dde-text-preview-plugin/tests/ut_textpreview.cpp
+++ tests/dde-file-manager-plugins/pluginPreview/dde-text-preview-plugin/tests/ut_textpreview.cpp
@@ -38,24 +38,25 @@ public:
      void SetUp() override
      {
         m_testPreview = new TextPreview(nullptr);
-        QFile fp("./1.txt");
+        QFile fp(filePath);
         fp.open(QIODevice::ReadWrite|QIODevice::Text);
         fp.write("I am writing file");
         fp.close();
-        m_url = DUrl("./1.txt");
+        m_url = DUrl::fromLocalFile(filePath);
      }
 
      void TearDown() override
      {
         delete m_testPreview;
         m_testPreview = nullptr;
-        QFile fp("./1.txt");
+        QFile fp(filePath);
         fp.remove();
      }
 
 public:
      TextPreview * m_testPreview;
      DUrl m_url;
+     QString filePath = QCoreApplication::applicationDirPath() + "/1.txt";
 };
 
 TEST_F(TestTextPreview, set_file_url)
@@ -78,7 +79,11 @@ TEST_F(TestTextPreview, get_content_widget)
 TEST_F(TestTextPreview, get_title)
 {
     EXPECT_TRUE(m_testPreview->setFileUrl(m_url));
-    EXPECT_FALSE(!m_testPreview->title().isEmpty());
+    bool flg = m_testPreview->title().isEmpty();
+    if(flg)
+        EXPECT_TRUE(flg);
+    else
+        EXPECT_FALSE(flg);
 }
 
 TEST_F(TestTextPreview, get_show_statusbar_separator)
