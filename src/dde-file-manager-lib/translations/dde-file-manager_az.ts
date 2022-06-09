ff29e97d25cef7845524d7452f1b828f7de3bdd4
Gong Heng <gongheng@uniontech.com>
2022-06-09 14:57:22 +0800

    fix: 更新翻译
    更新翻译.
    
    Log: 更新翻译
    Change-Id: I5f262c5c345fb2eafd56f62edf6b3acec2f2907f

--- src/dde-file-manager-lib/translations/dde-file-manager_az.ts
+++ src/dde-file-manager-lib/translations/dde-file-manager_az.ts
@@ -356,12 +356,12 @@
     <message>
         <location filename="../dialogs/connecttoserverdialog.cpp" line="263"/>
         <source>My Favorites</source>
-        <translation type="unfinished"/>
+        <translation>Seçilmişlər</translation>
     </message>
     <message>
         <location filename="../dialogs/connecttoserverdialog.cpp" line="364"/>
         <source>No favorites yet</source>
-        <translation type="unfinished"/>
+        <translation>Heç bir seçilmiş yoxdur</translation>
     </message>
     <message>
         <location filename="../dialogs/connecttoserverdialog.cpp" line="326"/>
