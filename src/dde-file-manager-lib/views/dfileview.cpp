622ae96848acc64dc3e8cee9473719cbde14f890
liuyangming <liuyangming@uniontech.com>
2022-06-29 11:03:28 +0800

    fix: 多用户场景下共享内存占用问题
    在共享内存被一个进程使用并且未释放的情况下，其他进程是无法attached该共享内存的，在共享内存无法使用的情况下需要走常规的数据获取流程。
    
    Log: 修复多用户场景下无法拖拽文件的问题
    
    Bug: https://pms.uniontech.com/bug-view-144135.html
    Change-Id: I6af476e97217170bf11eb4fbdc74f0edb39b318c

--- src/dde-file-manager-lib/views/dfileview.cpp
+++ src/dde-file-manager-lib/views/dfileview.cpp
@@ -1867,10 +1867,7 @@ void DFileView::dragEnterEvent(QDragEnterEvent *event)
 
     //由于普通用户无法访问root用户的共享内存，跨用户的情况使用从mimedata中取url的方式
     bool sameUser = DFMGlobal::isMimeDatafromCurrentUser(event->mimeData());
-    if (sameUser) {
-        if (!fetchDragEventUrlsFromSharedMemory())
-            return;
-    } else {
+    if (!sameUser || !fetchDragEventUrlsFromSharedMemory()) {
         m_urlsForDragEvent = event->mimeData()->urls();
     }
 
