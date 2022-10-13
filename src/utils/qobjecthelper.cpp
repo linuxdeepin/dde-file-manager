2a0cad7afd80fffd2cf2dedd62e249a28399a716
liuzhangjian <liuzhangjian@uniontech.com>
2022-10-13 14:02:54 +0800

    chore: update license header
    update license header
    
    Log: update license header

--- src/utils/qobjecthelper.cpp
+++ src/utils/qobjecthelper.cpp
@@ -1,7 +1,23 @@
-// SPDX-FileCopyrightText: 2020 UnionTech Software Technology Co., Ltd.
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
+/* This file is part of qjson
+  *
+  * Copyright (C) 2009 Till Adam <adam@kde.org>
+  * Copyright (C) 2009 Flavio Castelli <flavio@castelli.name>
+  *
+  * This library is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU Lesser General Public
+  * License version 2.1, as published by the Free Software Foundation.
+  *
+  *
+  * This library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  * Lesser General Public License for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public License
+  * along with this library; see the file COPYING.LIB.  If not, write to
+  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  * Boston, MA 02110-1301, USA.
+  */
 
 #include "qobjecthelper.h"
 
