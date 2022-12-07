0410fcd69a6998017efdd510bb3df3bc201438c0
misonlan <lanxuesong@uniontech.com>
2022-12-07 09:10:49 +0800

    fix: [view] fix address bar lost focus, view focus error (#702)
    when address bar lost focus, set focus in crumbbar
    
    Log: fix bug
    
    Bug: https://pms.uniontech.com/bug-view-162759.html

--- src/dde-file-manager-lib/interfaces/dfmcrumbbar.cpp
+++ src/dde-file-manager-lib/interfaces/dfmcrumbbar.cpp
@@ -255,6 +255,7 @@ void DFMCrumbBarPrivate::initConnections()
     q->connect(addressBar, &DFMAddressBar::lostFocus, q, [q, this]() {
         if (crumbController && !qobject_cast<DFileManagerWindow *>(q->window())->isAdvanceSearchBarVisible()) {
             crumbController->processAction(DFMCrumbInterface::AddressBarLostFocus);
+            q->setFocus();
         }
     });
 
