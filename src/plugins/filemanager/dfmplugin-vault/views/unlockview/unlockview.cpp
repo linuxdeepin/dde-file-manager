74be908acf1153b361182cff2c5b0025b55cd138
Gong Heng <gongheng@uniontech.com>
2022-12-13 10:09:17 +0800

    fix: [vault] change translate
    change translate.
    
    Log: change translate
    Change-Id: I0a2cb78f3caabdb285a967f8d7a79cadddb0e7e4

--- src/plugins/filemanager/dfmplugin-vault/views/unlockview/unlockview.cpp
+++ src/plugins/filemanager/dfmplugin-vault/views/unlockview/unlockview.cpp
@@ -211,7 +211,7 @@ void UnlockView::onVaultUlocked(int state)
                 }
             }
         } else if (state == static_cast<int>(ErrorCode::kWrongPassword)) {
-            DDialog dialog(tr("Password error"), "", this);
+            DDialog dialog(tr("Wrong password"), "", this);
             dialog.setIcon(QIcon::fromTheme("dialog-warning"));
             dialog.addButton(tr("OK", "button"), true, DDialog::ButtonRecommend);
             dialog.exec();
