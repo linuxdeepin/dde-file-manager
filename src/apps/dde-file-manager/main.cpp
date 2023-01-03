ad23bd9b131eb6c469faa81bced7db91c7ecf210
lanxuesong <lanxuesong@uniontech.com>
2023-01-03 10:40:25 +0800

    fix: [trash] fix no content displayed when cd to trash directly
    remove plugin-trash from lazy load list
    
    Log: fix bug
    
    Bug: https://pms.uniontech.com/bug-view-177935.html
    Bug: https://pms.uniontech.com/bug-view-180513.html
    Change-Id: I7cce5716b2f332d05853519cecedc31e8976fe5f

--- src/apps/dde-file-manager/main.cpp
+++ src/apps/dde-file-manager/main.cpp
@@ -118,8 +118,8 @@ static bool pluginsLoad()
     // TODO(zhangs): use config
     static const QStringList kLazyLoadPluginNames { "dfmplugin-emblem", "dfmplugin-burn", "dfmplugin-dirshare",
                                                     "dfmplugin-tag", "dfmplugin-avfsbrowser", "dfmplugin-myshares",
-                                                    "dfmplugin-smbbrowser", "dfmplugin-recent", "dfmplugin-trash",
-                                                    "dfmplugin-search", "dfmplugin-vault", "dfmplugin-filepreview" };
+                                                    "dfmplugin-smbbrowser", "dfmplugin-recent", "dfmplugin-search",
+                                                    "dfmplugin-vault", "dfmplugin-filepreview" };
     if (DTK_CORE_NAMESPACE::DSysInfo::isCommunityEdition())
         DPF_NAMESPACE::LifeCycle::initialize({ kFmPluginInterface, kCommonPluginInterface }, pluginsDirs, { "dfmplugin-vault" }, kLazyLoadPluginNames);
     else
