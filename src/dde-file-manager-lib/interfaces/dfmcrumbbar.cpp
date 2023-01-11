3fdb4c9aaa055fe79dfdd1b4a4c2a49328ac68ec
liuzhangjian <liuzhangjian@uniontech.com>
2023-01-11 10:31:30 +0800

    fix: After the search is completed, select a result and press Ctrl+A, but can not select all
    The focus is setted to the DFMCrumbBar, when it lost form the Addressbar
    
    Log: fix bug
    Bug: https://pms.uniontech.com/bug-view-178887.html

--- src/dde-file-manager-lib/interfaces/dfmcrumbbar.cpp
+++ src/dde-file-manager-lib/interfaces/dfmcrumbbar.cpp
@@ -255,7 +255,6 @@ void DFMCrumbBarPrivate::initConnections()
     q->connect(addressBar, &DFMAddressBar::lostFocus, q, [q, this]() {
         if (crumbController && !qobject_cast<DFileManagerWindow *>(q->window())->isAdvanceSearchBarVisible()) {
             crumbController->processAction(DFMCrumbInterface::AddressBarLostFocus);
-            q->setFocus();
         }
     });
 
