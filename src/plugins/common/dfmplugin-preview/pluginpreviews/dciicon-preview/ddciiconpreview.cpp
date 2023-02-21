7647918b32ed7d8a58c5de2afe987e42fa8833e6
max <lvwujun@uniontech.com>
2023-02-21 21:43:13 +0800

    fix: fix ddciicon compile issues
    fix ddciicon compile issues
    
    Log: fix ddciicon compile issues
    Bug: no, crp pkg issues
    Change-Id: I2654fee840beb115613eb0f08f5512c837f6ebdb

--- src/plugins/common/dfmplugin-preview/pluginpreviews/dciicon-preview/ddciiconpreview.cpp
+++ src/plugins/common/dfmplugin-preview/pluginpreviews/dciicon-preview/ddciiconpreview.cpp
@@ -8,6 +8,7 @@
 #include "dfm-base/base/schemefactory.h"
 #include "dfm-base/interfaces/abstractfileinfo.h"
 #include "dfm-base/mimetype/dmimedatabase.h"
+#include "dfm-base/utils/fileutils.h"
 
 #include <dtkgui_config.h>
 
