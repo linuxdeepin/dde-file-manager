1a77f49665116bef7121a93c781e806f1f218f7d
Zhang Sheng <zhangsheng@uniontech.com>
2023-04-07 14:37:45 +0800

    refactor: load core plguin of `dde-file-manager-server`
    as title
    
    Log: refactor `dde-file-manager-server`

--- src/plugins/server/core/serverplugin-core/core.cpp
+++ src/plugins/server/core/serverplugin-core/core.cpp
@@ -4,15 +4,29 @@
 
 #include "core.h"
 
+#include <dfm-base/dfm_global_defines.h>
+#include <dfm-base/base/urlroute.h>
+#include <dfm-base/base/schemefactory.h>
+#include <dfm-base/file/local/syncfileinfo.h>
+#include <dfm-base/file/local/asyncfileinfo.h>
+#include <dfm-base/file/local/localdiriterator.h>
+#include <dfm-base/file/local/localfilewatcher.h>
+
 SERVERPCORE_BEGIN_NAMESPACE
+DFMBASE_USE_NAMESPACE
 
 void Core::initialize()
 {
+    UrlRoute::regScheme(Global::Scheme::kFile, "/");
+    UrlRoute::regScheme(Global::Scheme::kAsyncFile, "/");
+    InfoFactory::regClass<SyncFileInfo>(Global::Scheme::kFile);
+    InfoFactory::regClass<AsyncFileInfo>(Global::Scheme::kAsyncFile);
+    DirIteratorFactory::regClass<LocalDirIterator>(Global::Scheme::kFile);
+    WatcherFactory::regClass<LocalFileWatcher>(Global::Scheme::kFile);
 }
 
 bool Core::start()
 {
-
     return true;
 }
 
