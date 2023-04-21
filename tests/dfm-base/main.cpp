ad421736a6b6bb71fa4c59bda960e4d32f5954cc
Zhang Sheng <zhangsheng@uniontech.com>
2023-04-21 17:07:40 +0800

    tests: [UT] modify cmake of `dfm-framework`
    add all source code
    
    Log: ut

--- tests/dfm-base/main.cpp
+++ tests/dfm-base/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
     delete appins;
 
 #ifdef ENABLE_TSAN_TOOL
-    __sanitizer_set_report_path("../../../asan_dde-file-manager.log");
+    __sanitizer_set_report_path("../../../asan_base.log");
 #endif
 
     return ret;
