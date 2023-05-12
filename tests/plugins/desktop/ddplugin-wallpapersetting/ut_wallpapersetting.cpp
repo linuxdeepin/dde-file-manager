d01888b5fc805de9a83d4dd04c0cdef5316dfa2d
ZHANGTINGAN <zhangtingan@uniontech.com>
2023-05-12 13:32:11 +0800

    test:  [ut] add test for wallpapersettings
    1.add ut
    2.Correct incorrect words “sreensaver”
    
    Log: addut

--- tests/plugins/desktop/ddplugin-wallpapersetting/ut_wallpapersetting.cpp
+++ tests/plugins/desktop/ddplugin-wallpapersetting/ut_wallpapersetting.cpp
@@ -6,10 +6,12 @@
 #include "private/wallpapersettings_p.h"
 #include "wallaperpreview.h"
 #include "desktoputils/ddpugin_eventinterface_helper.h"
-#include <QRect>
-
+#include "desktoputils/widgetutil.h"
 #include <dfm-base/dfm_desktop_defines.h>
 #include <dfm-framework/dpf.h>
+#include "wlsetplugin.h"
+
+#include <QRect>
 #include <QBoxLayout>
 
 #include "stubext.h"
@@ -18,22 +20,6 @@
 DFMBASE_USE_NAMESPACE
 DPF_USE_NAMESPACE
 DDP_WALLPAERSETTING_USE_NAMESPACE
-
-class UT_wallPaperSettings : public testing::Test
-{
-protected:
-    virtual void SetUp() override
-    {
-        setting = new WallpaperSettings("testSetting");
-    }
-    virtual void TearDown() override
-    {
-        delete setting;
-        stub.clear();
-    }
-    WallpaperSettings *setting = nullptr;
-    stub_ext::StubExt stub;
-};
 
 TEST(wallPaperSettings, wallpapersettings)
 {
@@ -58,7 +44,6 @@ TEST(wallPaperSettings, wallpapersettings)
 
 TEST(wallPaperSettings, Init)
 {
-    WallpaperSettings *setting = new WallpaperSettings("testSetting");
     stub_ext::StubExt stub;
     stub.set_lamda(&WallpaperSettingsPrivate::initUI, []() {
         return;
@@ -81,7 +66,7 @@ TEST(wallPaperSettings, Init)
         }
     }
 
-    setting->init();
+    WallpaperSettings *setting = new WallpaperSettings("testSetting", WallpaperSettings::Mode::ScreenSaverMode);
     WallpaperSettingsPrivate *d = setting->d;
     EXPECT_EQ(d->wmInter->service(), QString("com.deepin.wm"));
     EXPECT_TRUE(d->regionMonitor);
@@ -105,6 +90,226 @@ TEST(wallPaperSettings, Init)
             EXPECT_TRUE(ptr->handlerList.isEmpty());
         }
     }
+}
+
+TEST(WallpaperSettingsPrivate, initUi)
+{
+    stub_ext::StubExt stub;
+    stub.set_lamda(&WallpaperSettings::init, []() {
+        return;
+    });
+    WallpaperSettings *setting = new WallpaperSettings("testSetting", WallpaperSettings::Mode::ScreenSaverMode);
+
+    bool initCloseButton = false;
+    bool initCarousel = false;
+    bool initScreensaver = false;
+    bool relayout = false;
+
+    stub.set_lamda(&WallpaperSettingsPrivate::initCloseButton, [&initCloseButton]() {
+        initCloseButton = true;
+        return;
+    });
+    stub.set_lamda(&WallpaperSettingsPrivate::initCarousel, [&initCarousel]() {
+        initCarousel = true;
+        return;
+    });
+    stub.set_lamda(&WallpaperSettingsPrivate::initScreenSaver, [&initScreensaver]() {
+        initScreensaver = true;
+        return;
+    });
+    stub.set_lamda(&WallpaperSettingsPrivate::relaylout, [&relayout]() {
+        relayout = true;
+        return;
+    });
+    stub.set_lamda(&WallpaperSettingsPrivate::initPreivew, []() {
+        return;
+    });
+
+    bool connect1 = false;
+    bool connect2 = false;
+    bool connect3 = false;
+
+    stub.set_lamda(&WallpaperSettingsPrivate::handleNeedCloseButton, [&connect1]() {
+        connect1 = true;
+        return;
+    });
+    stub.set_lamda(&WallpaperSettingsPrivate::onItemPressed, [&connect2]() {
+        connect2 = true;
+        return;
+    });
+    stub.set_lamda(&WallpaperSettingsPrivate::setMode, [&connect3]() {
+        connect3 = true;
+        return;
+    });
+
+    stub.set_lamda(&ddplugin_desktop_util::enableScreensaver, []() {
+        return true;
+    });
+
+    setting->d->initUI();
+    emit setting->d->wallpaperList->mouseOverItemChanged("", QPoint());
+    emit setting->d->wallpaperList->itemPressed("");
+    emit setting->d->switchModeControl->buttonToggled(new DButtonBoxButton("Wallpaper", setting), true);
+
+    EXPECT_TRUE(initCloseButton);
+    EXPECT_TRUE(initCarousel);
+    EXPECT_TRUE(initScreensaver);
+
+    EXPECT_TRUE(connect1);
+    EXPECT_TRUE(connect2);
+    EXPECT_TRUE(connect3);
+
+    EXPECT_FALSE(setting->d->switchModeControl->buttonList().at(0)->isChecked());
+    EXPECT_TRUE(setting->d->switchModeControl->buttonList().at(1)->isChecked());
+
+    delete setting;
+
+    setting = new WallpaperSettings("testSetting", WallpaperSettings::Mode::WallpaperMode);
+    setting->d->initUI();
+
+    EXPECT_TRUE(setting->d->switchModeControl->buttonList().at(0)->isChecked());
+    EXPECT_FALSE(setting->d->switchModeControl->buttonList().at(1)->isChecked());
+    delete setting;
+}
+
+TEST(WallpaperSettingsPrivate, initCloseButton)
+{
+    stub_ext::StubExt stub;
+    stub.set_lamda(&WallpaperSettings::init, []() {
+        return;
+    });
+    WallpaperSettings *setting = new WallpaperSettings("testSetting", WallpaperSettings::Mode::ScreenSaverMode);
+
+    bool connect = false;
+    stub.set_lamda(&WallpaperSettingsPrivate::onCloseButtonClicked, [&connect]() {
+        connect = true;
+        return;
+    });
+
+    EXPECT_NO_FATAL_FAILURE(setting->d->initCloseButton());
+    EXPECT_TRUE(setting->d->closeButton->isHidden());
+    setting->d->closeButton->click();
+    EXPECT_TRUE(connect);
+    delete setting;
+}
+
+TEST(WallpaperSettingsPrivate, initCarousel)
+{
+    stub_ext::StubExt stub;
+    stub.set_lamda(&WallpaperSettings::init, []() {
+        return;
+    });
+    WallpaperSettings *setting = new WallpaperSettings("testSetting", WallpaperSettings::Mode::ScreenSaverMode);
+
+    bool connect1 = false;
+    bool connect2 = false;
+    stub.set_lamda(&WallpaperSettingsPrivate::carouselTurn, [&connect1]() {
+        connect1 = true;
+        return;
+    });
+    stub.set_lamda(&WallpaperSettingsPrivate::switchCarousel, [&connect2]() {
+        connect2 = true;
+        return;
+    });
+    EXPECT_NO_FATAL_FAILURE(setting->d->initCarousel());
+    EXPECT_TRUE(setting->d->carouselCheckBox);
+    EXPECT_TRUE(setting->d->carouselControl);
+    emit setting->d->carouselCheckBox->click();
+    emit setting->d->carouselControl->buttonToggled(new DButtonBoxButton("Wallpaper", setting), true);
+
+    EXPECT_TRUE(connect1);
+    EXPECT_TRUE(connect2);
+
+    delete setting;
+}
+
+TEST(WallpaperSettingsPrivate, initScreenSaver)
+{
+    stub_ext::StubExt stub;
+    stub.set_lamda(&WallpaperSettings::init, []() {
+        return;
+    });
+    WallpaperSettings *setting = new WallpaperSettings("testSetting", WallpaperSettings::Mode::ScreenSaverMode);
+
+    bool connect1 = false;
+    bool connect2 = false;
+    stub.set_lamda(&WallpaperSettingsPrivate::switchWaitTime, [&connect1]() {
+        connect1 = true;
+    });
+    stub.set_lamda(&ScreenSaverIfs::setLockScreenAtAwake, [&connect2]() {
+        __DBG_STUB_INVOKE__
+        connect2 = true;
+    });
+
+    EXPECT_NO_FATAL_FAILURE(setting->d->initScreenSaver());
+    EXPECT_TRUE(setting->d->lockScreenBox);
+    EXPECT_TRUE(setting->d->waitControl);
+    emit setting->d->waitControl->buttonToggled(new DButtonBoxButton("Wallpaper", setting), true);
+    emit setting->d->lockScreenBox->toggled(true);
+
+    EXPECT_TRUE(connect1);
+    EXPECT_TRUE(connect2);
+
+    delete setting;
+}
+
+TEST(WallpaperSettingsPrivate, initpreview)
+{
+    stub_ext::StubExt stub;
+    stub.set_lamda(&WallpaperSettings::init, []() {
+        return;
+    });
+    WallpaperSettings *setting = new WallpaperSettings("testSetting", WallpaperSettings::Mode::ScreenSaverMode);
+
+    bool init = false;
+    stub.set_lamda(&WallaperPreview::init, [&init]() {
+        init = true;
+        return;
+    });
+
+    setting->d->initPreivew();
+    EXPECT_TRUE(init);
+    delete setting;
+}
+
+class UT_wallPaperSettings : public testing::Test
+{
+protected:
+    virtual void SetUp() override
+    {
+        setting = new WallpaperSettings("testSetting");
+    }
+    virtual void TearDown() override
+    {
+        delete setting;
+        stub.clear();
+    }
+    WallpaperSettings *setting = nullptr;
+    stub_ext::StubExt stub;
+};
+
+TEST_F(UT_wallPaperSettings, privateEventFilter)
+{
+    QKeyEvent *event = new QKeyEvent(QEvent::KeyPress, Qt::Key_Enter, Qt::KeyboardModifier::NoModifier);
+
+    bool emit1 = false;
+    stub.set_lamda(&WallpaperSettingsPrivate::carouselTurn, [&emit1]() {
+        emit1 = true;
+        return;
+    });
+
+    setting->d->eventFilter(setting->d->carouselCheckBox, event);
+    EXPECT_TRUE(emit1);
+
+    emit1 = false;
+    event->k = Qt::Key_Return;
+    setting->d->eventFilter(setting->d->carouselCheckBox, event);
+    EXPECT_TRUE(emit1);
+
+    emit1 = false;
+    event->k = Qt::Key_9;
+    setting->d->eventFilter(setting->d->carouselCheckBox, event);
+    EXPECT_FALSE(emit1);
 }
 
 TEST_F(UT_wallPaperSettings, timeFormat)
@@ -308,5 +513,193 @@ TEST_F(UT_wallPaperSettings, refresh)
         setting->refreshList();
         EXPECT_FALSE(loadWallpaper);
         EXPECT_TRUE(loadScrennSaver);
+    }
+}
+
+TEST_F(UT_wallPaperSettings, loadScreensaver)
+{
+    stub.set_lamda(&ComDeepinScreenSaverInterface::ConfigurableItems, []() {
+        __DBG_STUB_INVOKE__
+        return QDBusPendingReply<QStringList>();
+    });
+
+    QStringList list;
+    list << "screenSaver1"
+         << "screenSaver2"
+         << "flurry";
+    stub.set_lamda(&ComDeepinScreenSaverInterface::allScreenSaver, [&list]() {
+        __DBG_STUB_INVOKE__
+        return list;
+    });
+
+    bool closeLoading = false;
+    stub.set_lamda(&WallpaperSettings::closeLoading, [&closeLoading]() {
+        closeLoading = true;
+    });
+
+    stub.set_lamda(&ComDeepinScreenSaverInterface::GetScreenSaverCover, []() {
+        __DBG_STUB_INVOKE__
+        return QDBusPendingReply<QString>();
+    });
+    QString currentItem = "screenSaver1";
+    stub.set_lamda(&ComDeepinScreenSaverInterface::currentScreenSaver, [&currentItem]() {
+        __DBG_STUB_INVOKE__
+        return currentItem;
+    });
+
+    stub.set_lamda(&WallpaperList::updateItemThumb, []() {
+        return;
+    });
+    bool setindex = false;
+    stub.set_lamda(&WallpaperList::setCurrentIndex, [&setindex]() {
+        setindex = true;
+        return;
+    });
+    bool emit1 = false;
+    stub.set_lamda(&WallpaperItem::pressed, [&emit1]() {
+        emit1 = true;
+        return;
+    });
+
+    bool connect1 = false;
+    bool connect2 = false;
+    bool connect3 = false;
+    stub.set_lamda(&WallpaperSettingsPrivate::onItemButtonClicked, [&connect1]() {
+        connect1 = true;
+    });
+    stub.set_lamda(&WallpaperSettingsPrivate::onItemTab, [&connect2]() {
+        connect2 = true;
+    });
+    stub.set_lamda(&WallpaperSettingsPrivate::onItemBacktab, [&connect3]() {
+        connect3 = true;
+    });
+
+    {
+        EXPECT_TRUE(setting->d->screenSaverIfs->isValid());
+        setting->loadScreenSaver();
+        EXPECT_TRUE(closeLoading);
+        EXPECT_EQ(setting->d->wallpaperList->items.size(), 2);
+        EXPECT_TRUE(emit1);
+        EXPECT_FALSE(setindex);
+
+        emit setting->d->wallpaperList->items.at(0)->buttonClicked(nullptr, QString(""));
+        emit setting->d->wallpaperList->items.at(0)->tab(nullptr);
+        emit setting->d->wallpaperList->items.at(0)->backtab(nullptr);
+        EXPECT_TRUE(connect1);
+        EXPECT_TRUE(connect2);
+        EXPECT_TRUE(connect3);
+    }
+    {
+        currentItem = "123";
+        emit1 = false;
+        setindex = false;
+
+        setting->loadScreenSaver();
+        EXPECT_FALSE(emit1);
+        EXPECT_TRUE(setindex);
+    }
+    {
+        closeLoading = false;
+        setindex = false;
+        stub.set_lamda(&QDBusAbstractInterface::isValid, []() {
+            return false;
+        });
+        list.clear();
+        setting->loadScreenSaver();
+        EXPECT_FALSE(closeLoading);
+        EXPECT_FALSE(setindex);
+    }
+}
+
+TEST_F(UT_wallPaperSettings, applyToDesktop)
+{
+    setting->d->screenName = "test1";
+    setting->d->currentSelectedWallpaper = "test2";
+
+    bool call = false;
+    QList<QVariant> input;
+    stub.set_lamda(
+            &QDBusAbstractInterface::asyncCallWithArgumentList,
+            [&call, &input](QDBusAbstractInterface *&, const QString &method, const QList<QVariant> &args) {
+                if (method == "SetMonitorBackground") {
+                    call = true;
+                    input = args;
+                }
+                return QDBusPendingCall::fromError(QDBusError());
+            });
+    bool emited = false;
+    QObject::connect(setting, &WallpaperSettings::backgroundChanged, [&emited]() {
+        emited = true;
+    });
+    {
+
+        setting->applyToDesktop();
+        EXPECT_EQ(input.at(0).value<QString>(), QString("test1"));
+        EXPECT_EQ(input.at(1).value<QString>(), QString("test2"));
+        EXPECT_TRUE(call);
+        EXPECT_TRUE(emited);
+    }
+    {
+        call = false;
+        emited = false;
+        input.clear();
+        setting->d->currentSelectedWallpaper = "";
+        setting->applyToDesktop();
+        EXPECT_FALSE(call);
+        EXPECT_TRUE(input.isEmpty());
+        EXPECT_FALSE(emited);
+    }
+    {
+        call = false;
+        emited = false;
+        input.clear();
+        setting->d->currentSelectedWallpaper = "test1";
+        setting->d->appearanceIfs->deleteLater();
+        setting->d->appearanceIfs = nullptr;
+        setting->applyToDesktop();
+        EXPECT_TRUE(input.isEmpty());
+        EXPECT_FALSE(call);
+        EXPECT_FALSE(emited);
+    }
+}
+
+TEST_F(UT_wallPaperSettings, applyToGreeter)
+{
+    setting->d->currentSelectedWallpaper = "test2";
+    bool call = false;
+    QList<QVariant> input;
+    stub.set_lamda(
+            &QDBusAbstractInterface::asyncCallWithArgumentList,
+            [&call, &input](QDBusAbstractInterface *&, const QString &method, const QList<QVariant> &args) {
+                if (method == "Set") {
+                    call = true;
+                    input = args;
+                }
+                return QDBusPendingCall::fromError(QDBusError());
+            });
+
+    {
+        setting->applyToGreeter();
+        EXPECT_EQ(input.at(0).value<QString>(), QString("greeterbackground"));
+        EXPECT_EQ(input.at(1).value<QString>(), QString("test2"));
+        EXPECT_TRUE(call);
+    }
+    {
+        call = false;
+        input.clear();
+        setting->d->currentSelectedWallpaper = "";
+        setting->applyToGreeter();
+        EXPECT_FALSE(call);
+        EXPECT_TRUE(input.isEmpty());
+    }
+    {
+        call = false;
+        input.clear();
+        setting->d->currentSelectedWallpaper = "test1";
+        setting->d->appearanceIfs->deleteLater();
+        setting->d->appearanceIfs = nullptr;
+        setting->applyToGreeter();
+        EXPECT_TRUE(input.isEmpty());
+        EXPECT_FALSE(call);
     }
 }
