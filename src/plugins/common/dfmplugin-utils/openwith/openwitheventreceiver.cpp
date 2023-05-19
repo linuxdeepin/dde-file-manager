e269ed32e88eacf16145829185eed84b20181b87
Gong Heng <gongheng@uniontech.com>
2023-05-19 10:56:30 +0800

    fix: [desktop] On desktop, click "win + d", the openwith dialog hide.
    the openwith dialog should model diskplay like old.
    so let the openwith dialog model display.
    
    Log: fix issues
    Bug: https://pms.uniontech.com/bug-view-199389.html

--- src/plugins/common/dfmplugin-utils/openwith/openwitheventreceiver.cpp
+++ src/plugins/common/dfmplugin-utils/openwith/openwitheventreceiver.cpp
@@ -7,6 +7,8 @@
 
 #include <dfm-framework/dpf.h>
 
+#include <dfm-base/widgets/filemanagerwindowsmanager.h>
+
 using namespace dfmplugin_utils;
 
 void OpenWithEventReceiver::initEventConnect()
@@ -14,12 +16,26 @@ void OpenWithEventReceiver::initEventConnect()
     dpfSlotChannel->connect("dfmplugin_utils", "slot_OpenWith_ShowDialog", this, &OpenWithEventReceiver::showOpenWithDialog);
 }
 
-void OpenWithEventReceiver::showOpenWithDialog(const QList<QUrl> &urls)
+void OpenWithEventReceiver::showOpenWithDialog(quint64 winId, const QList<QUrl> &urls)
 {
-    OpenWithDialog *d = new OpenWithDialog(urls);
+    QWidget *parentWidget { nullptr };
+    if (winId != 0) {
+        parentWidget  = FMWindowsIns.findWindowById(winId);
+        if (!parentWidget) {
+            QWidgetList topWidgets = qApp->topLevelWidgets();
+            auto find = std::find_if(topWidgets.begin(), topWidgets.end(), [winId](QWidget *w){
+                    return  w->internalWinId() == winId;
+            });
+            if (find != topWidgets.end()) {
+                parentWidget = *find;
+            }
+        }
+    }
+
+    OpenWithDialog *d = new OpenWithDialog(urls, parentWidget);
     d->setAttribute(Qt::WA_DeleteOnClose);
     d->setDisplayPosition(OpenWithDialog::Center);
-    d->open();
+    d->exec();
 }
 
 OpenWithEventReceiver::OpenWithEventReceiver(QObject *parent)
