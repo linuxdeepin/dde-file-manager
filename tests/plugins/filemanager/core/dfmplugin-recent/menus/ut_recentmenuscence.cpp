229f817dd689ad8c75ac6c5ae9a25aefcca56242
liujinchang <liujinchang@uniontech.com>
2023-08-01 10:23:01 +0800

    fix: [UT] add recent ut
    add recent ut
    
    Log: as des
    Task: https://pms.uniontech.com/task-view-278227.html

--- tests/plugins/filemanager/core/dfmplugin-recent/menus/ut_recentmenuscence.cpp
+++ tests/plugins/filemanager/core/dfmplugin-recent/menus/ut_recentmenuscence.cpp
@@ -9,6 +9,8 @@
 #include "utils/recentmanager.h"
 #include <dfm-base/base/application/application.h>
 #include <dfm-base/dfm_menu_defines.h>
+#include <dfm-base/interfaces/fileinfo.h>
+#include <dfm-base/base/schemefactory.h>
 #include <dfm-framework/event/event.h>
 #include "plugins/common/core/dfmplugin-menu/menu_eventinterface_helper.h"
 #include "plugins/common/core/dfmplugin-menu/menuscene/action_defines.h"
@@ -17,6 +19,7 @@
 #include <QPaintEvent>
 #include <QPainter>
 #include <QMenu>
+#include <QAction>
 
 DFMBASE_USE_NAMESPACE
 using namespace dfmplugin_recent;
@@ -26,7 +29,8 @@ class RecentMenuSceneTest : public testing::Test
 protected:
     virtual void SetUp() override
     {
-        scene = new RecentMenuScene();
+        auto creator = new RecentMenuCreator();
+        scene = static_cast<dfmplugin_recent::RecentMenuScene *>(creator->create());
         d = scene->d.data();
     }
     virtual void TearDown() override
@@ -35,7 +39,7 @@ protected:
         delete scene;
     }
 
-private:
+protected:
     stub_ext::StubExt stub;
     RecentMenuScene *scene { nullptr };
     RecentMenuScenePrivate *d { nullptr };
@@ -54,7 +58,11 @@ TEST_F(RecentMenuSceneTest, initialize)
     EXPECT_FALSE(scene->initialize({ { "onDesktop", true } }));
     EXPECT_TRUE(scene->initialize({ { "isEmptyArea", true } }));
     EXPECT_FALSE(scene->initialize({ { "indexFlags", true } }));
-
+    d->focusFile = QUrl::fromLocalFile("/hello/world");
+    stub.set_lamda(VADDR(AbstractMenuScenePrivate, initializeParamsIsValid), [] {
+        return true;
+    });
+    EXPECT_TRUE(scene->initialize({ { "isEmptyArea", true } }));
     urls.takeFirst();
     EXPECT_NO_FATAL_FAILURE(scene->initialize({ { "isEmptyArea", false },
                                                 { "selectFiles", QVariant::fromValue<QList<QUrl>>(urls) } }));
@@ -65,6 +73,8 @@ TEST_F(RecentMenuSceneTest, create)
     EXPECT_FALSE(scene->create(nullptr));
 
     QMenu menu;
+    EXPECT_NO_FATAL_FAILURE(scene->create(&menu));
+    d->isEmptyArea = true;
     EXPECT_NO_FATAL_FAILURE(scene->create(&menu));
 }
 
@@ -117,22 +127,73 @@ TEST_F(RecentMenuSceneTest, updateMenu)
 {
     QMenu menu;
     menu.addSeparator();
-    auto act = menu.addAction("Select all");
-    act->setProperty(ActionPropertyKey::kActionID, dfmplugin_menu::ActionID::kCut);
-    RecentMenuScene scene;
-    scene.d->isEmptyArea = true;
-    EXPECT_NO_FATAL_FAILURE(scene.d->updateMenu(&menu));
-    scene.d->isEmptyArea = false;
-    EXPECT_NO_FATAL_FAILURE(scene.d->updateMenu(&menu));
+    auto act = menu.addAction("1");
+    auto act1 = menu.addAction("2");
+    auto act2 = menu.addAction("3");
+    auto act3 = menu.addAction("4");
+    act->setProperty(ActionPropertyKey::kActionID, "remove");
+    act1->setProperty(ActionPropertyKey::kActionID, "open-file-location");
+    act2->setProperty(ActionPropertyKey::kActionID, "sort-by-path");
+    act3->setProperty(ActionPropertyKey::kActionID, "sort-by-lastRead");
+    d->predicateAction.insert("hello", act);
+    d->predicateAction.insert("remove", act);
+    d->predicateAction.insert("open-file-location", act);
+    d->predicateAction.insert("sort-by-path", act);
+    d->predicateAction.insert("sort-by-lastRead", act);
+    d->isEmptyArea = true;
+    EXPECT_NO_FATAL_FAILURE(d->updateMenu(&menu));
+    d->isEmptyArea = false;
+    EXPECT_NO_FATAL_FAILURE(d->updateMenu(&menu));
 }
 
 TEST_F(RecentMenuSceneTest, updateSubMenu)
 {
     DPF_USE_NAMESPACE
+    int flag = 0;
+    stub.set_lamda(&QAction::setChecked, [&flag] {
+        __DBG_STUB_INVOKE__
+        flag++;
+    });
     QMenu menu;
-    typedef QVariant (EventChannelManager::*Push1)(const QString &, const QString &, const quint64 &);
+    auto act = menu.addAction("1");
+    auto act1 = menu.addAction("2");
+    act->setProperty(ActionPropertyKey::kActionID, "remove");
+    act1->setProperty(ActionPropertyKey::kActionID, "sort-by-time-modified");
+    d->predicateAction[RecentActionID::kSortByPath] = act;
+    d->predicateAction[RecentActionID::kSortByLastRead] = act1;
+    typedef QVariant (EventChannelManager::*Push1)(const QString &, const QString &, quint64);
     auto push1 = static_cast<Push1>(&EventChannelManager::push);
-    stub.set_lamda(push1, [] { __DBG_STUB_INVOKE__ return QVariant(); });
+    stub.set_lamda(push1, [&flag] {
+        __DBG_STUB_INVOKE__
+        flag++;
+        return QVariant();
+    });
+    d->updateSubMenu(&menu);
 
-    EXPECT_NO_FATAL_FAILURE(d->updateSubMenu(&menu));
+    QMenu menu1;
+    auto act2 = menu1.addAction("1");
+    act2->setProperty(ActionPropertyKey::kActionID, "sort-by-time-modified");
+    stub.set_lamda(push1, [&flag] {
+        __DBG_STUB_INVOKE__
+        flag++;
+        return QVariant(Global::ItemRoles::kItemFilePathRole);
+    });
+    d->updateSubMenu(&menu);
+
+    QMenu menu2;
+    auto act3 = menu2.addAction("1");
+    act3->setProperty(ActionPropertyKey::kActionID, "sort-by-time-modified");
+    stub.set_lamda(push1, [&flag] {
+        __DBG_STUB_INVOKE__
+        flag++;
+        return QVariant(Global::ItemRoles::kItemFileLastReadRole);
+    });
+    d->updateSubMenu(&menu2);
+    EXPECT_TRUE(flag == 3);
+}
+
+TEST_F(RecentMenuSceneTest, updateState)
+{
+    QMenu menu;
+    EXPECT_NO_FATAL_FAILURE(scene->updateState(&menu));
 }
