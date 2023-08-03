81caa0f918b483b54f8713e31e3bd3e87444adb6
Gong Heng <gongheng@uniontech.com>
2023-08-03 14:01:09 +0800

    test: [file-preview] add UT of file preview
    add UT of file preview

--- tests/plugins/common/dfmplugin-preview/filepreview/ut_filepreview.cpp
+++ tests/plugins/common/dfmplugin-preview/filepreview/ut_filepreview.cpp
@@ -3,25 +3,63 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "stubext.h"
-#include "addr_any.h"
-
-#include "plugins/common/dfmplugin-preview/filepreview/filepreview.h"
-#include <dfm-base/base/configs/dconfig/dconfigmanager.h>
+#include "filepreview.h"
+#include "events/fileprevieweventreceiver.h"
+#include "utils/previewhelper.h"
 
 #include <gtest/gtest.h>
+
+#include <dfm-base/base/configs/dconfig/dconfigmanager.h>
+
+#include <dfm-framework/event/event.h>
+
+DPFILEPREVIEW_USE_NAMESPACE
 DFMBASE_USE_NAMESPACE
-using namespace dfmplugin_filepreview;
-class UT_FilePreview : public testing::Test
+DPF_USE_NAMESPACE
+
+TEST(UT_FilePreview, initialize)
 {
-public:
-    virtual void SetUp() override {}
-    virtual void TearDown() override {}
-};
-TEST_F(UT_FilePreview, Start)
-{
-    QString err;
+    bool isOk { false };
+
     stub_ext::StubExt stub;
-    stub.set_lamda(&DConfigManager::addConfig, [] { __DBG_STUB_INVOKE__ return true; });
-    EXPECT_TRUE(DConfigManager::instance()->addConfig("org.deepin.dde.file-manager.preview", &err));
-    stub.clear();
+    stub.set_lamda(&FilePreviewEventReceiver::connectService, [ &isOk ]{
+        isOk = true;
+    });
+
+    FilePreview plugin;
+    plugin.initialize();
+
+    EXPECT_TRUE(isOk);
+}
+
+TEST(UT_FilePreview, start)
+{
+    stub_ext::StubExt stub;
+    stub.set_lamda(&DConfigManager::addConfig, []{
+        return false;
+    });
+    stub.set_lamda(&PreviewHelper::bindConfig, []{});
+
+    FilePreview plugin;
+    bool isOk = plugin.start();
+
+    EXPECT_TRUE(isOk);
+}
+
+TEST(UT_FilePreview, onConfigChanged)
+{
+    bool isOk { false };
+
+    stub_ext::StubExt stub;
+    typedef bool(EventDispatcherManager::*FuncType)(const QString &, const QString &);
+    stub.set_lamda(static_cast<FuncType>(&EventDispatcherManager::publish), [ &isOk ]{
+        isOk = true;
+        return true;
+    });
+
+    FilePreview plugin;
+    plugin.onConfigChanged("UT_TEST", "UT");
+    plugin.onConfigChanged(ConfigInfos::kConfName, ConfigInfos::kMtpThumbnailKey);
+
+    EXPECT_TRUE(isOk);
 }
