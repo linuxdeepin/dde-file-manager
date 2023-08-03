fce1b0de9d781ee880829d0657470a8bb3a691f1
Gong Heng <gongheng@uniontech.com>
2023-08-03 14:01:09 +0800

    test: [file-preview] add UT of file preview
    add UT of file preview

--- tests/plugins/common/dfmplugin-preview/filepreview/events/ut_fileprevieweventreceiver.cpp
+++ tests/plugins/common/dfmplugin-preview/filepreview/events/ut_fileprevieweventreceiver.cpp
@@ -3,31 +3,50 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "stubext.h"
-#include "addr_any.h"
-
-#include "plugins/common/dfmplugin-preview/filepreview/utils/previewdialogmanager.h"
-#include "plugins/common/dfmplugin-preview/filepreview/utils/previewhelper.h"
+#include "events/fileprevieweventreceiver.h"
+#include "utils/previewhelper.h"
+#include "utils/previewdialogmanager.h"
 
 #include <dfm-base/base/configs/dconfig/dconfigmanager.h>
 
-#include <dfm-framework/dpf.h>
+#include <dfm-framework/event/event.h>
 
 #include <gtest/gtest.h>
 
 DFMBASE_USE_NAMESPACE
-using namespace dfmplugin_filepreview;
-class UT_FilePreviewEventReceiver : public testing::Test
+DPFILEPREVIEW_USE_NAMESPACE
+DPF_USE_NAMESPACE
+
+TEST(UT_FilePreviewEventReceiver, connectService)
 {
-public:
-    virtual void SetUp() override {}
-    virtual void TearDown() override {}
-};
-TEST_F(UT_FilePreviewEventReceiver, ShowFilePreview)
-{
-    const QList<QUrl>inputs1 = { QUrl::fromLocalFile("/") };
-    const QList<QUrl>inputs2 = { QUrl::fromLocalFile("/Hello") };
+    bool isOk { false };
+
     stub_ext::StubExt stub;
-    stub.set_lamda(&PreviewHelper::isPreviewEnabled, [] { __DBG_STUB_INVOKE__ return true; });
-    stub.set_lamda(&PreviewDialogManager::showPreviewDialog, [](PreviewDialogManager *,const quint64, const QList<QUrl> &, const QList<QUrl>){});
-    EXPECT_NO_FATAL_FAILURE(PreviewDialogManager::instance()->showPreviewDialog(0,inputs1,inputs2));
+    typedef bool(EventChannelManager::*FuncType)(const QString &, const QString &, FilePreviewEventReceiver *, void(FilePreviewEventReceiver::*)(quint64, const QList<QUrl> &, QList<QUrl>));
+    stub.set_lamda(static_cast<FuncType>(&EventChannelManager::connect), [ &isOk ]{
+        isOk = true;
+        return true;
+    });
+
+    FilePreviewEventReceiver::instance()->connectService();
+
+    EXPECT_TRUE(isOk);
 }
+
+TEST(UT_FilePreviewEventReceiver, showFilePreview)
+{
+    bool isOk { false };
+
+    stub_ext::StubExt stub;
+    stub.set_lamda(&PreviewHelper::isPreviewEnabled, []{
+        return true;
+    });
+    stub.set_lamda(&PreviewDialogManager::showPreviewDialog, [ &isOk ]{
+        isOk = true;
+    });
+
+    FilePreviewEventReceiver::instance()->showFilePreview(0, QList<QUrl>() << QUrl("file:///UT_TEST"), QList<QUrl>() << QUrl("file:///UT_TEST1"));
+
+    EXPECT_TRUE(isOk);
+}
+
