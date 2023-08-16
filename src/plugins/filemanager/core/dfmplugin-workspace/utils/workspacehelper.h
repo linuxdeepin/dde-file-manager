9ddbd9948a43872576c68bcac815122571c6ce00
liuzhangjian <liuzhangjian@uniontech.com>
2023-08-16 16:06:21 +0800

    fix: [menu] fix issue of search menu
    In some scenarios, the search mune is problematic
    
    Log: fxi issue
    Bug: https://pms.uniontech.com/bug-view-214905.html

--- src/plugins/filemanager/core/dfmplugin-workspace/utils/workspacehelper.h
+++ src/plugins/filemanager/core/dfmplugin-workspace/utils/workspacehelper.h
@@ -70,6 +70,7 @@ public:
     void selectAll(quint64 windowId);
     void setSort(quint64 windowId, DFMBASE_NAMESPACE::Global::ItemRoles role);
     DFMBASE_NAMESPACE::Global::ItemRoles sortRole(quint64 windowId);
+    QList<DFMGLOBAL_NAMESPACE::ItemRoles> columnRoles(quint64 windowId);
 
     bool reigsterViewRoutePrehandler(const QString &scheme, const FileViewRoutePrehaldler prehandler);
     bool haveViewRoutePrehandler(const QString &scheme) const;
