baec1d8e3cc5284c73c6402bc0acdfe818d83e39
liuyangming <liuyangming@uniontech.com>
2023-08-23 16:08:08 +0800

    fix: [fileoperation]open symlink dir issue in trash
    cd src dir when open symlink dir in trash
    
    Log: fix open symlink dir issue
    Bug: https://pms.uniontech.com/bug-view-215689.html

--- src/plugins/filemanager/core/dfmplugin-workspace/utils/fileoperatorhelper.cpp
+++ src/plugins/filemanager/core/dfmplugin-workspace/utils/fileoperatorhelper.cpp
@@ -101,7 +101,7 @@ void FileOperatorHelper::openFilesByMode(const FileView *view, const QList<QUrl>
             if (fileInfoPtr->isAttributes(OptInfoType::kIsDir)) {
                 QUrl dirUrl = url;
                 if (fileInfoPtr->isAttributes(OptInfoType::kIsSymLink))
-                    dirUrl = fileInfoPtr->urlOf(UrlInfoType::kRedirectedFileUrl);
+                    dirUrl = QUrl::fromLocalFile(fileInfoPtr->pathOf(PathInfoType::kSymLinkTarget));
 
                 if (mode == DirOpenMode::kOpenNewWindow) {
                     WorkspaceEventCaller::sendOpenWindow({ dirUrl });
