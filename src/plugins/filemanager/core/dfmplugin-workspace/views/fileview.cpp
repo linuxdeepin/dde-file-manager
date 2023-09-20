8cd70adfc717506f0929adec9c6bc568f062b89c
liuyangming <liuyangming@uniontech.com>
2023-09-20 13:32:38 +0800

    fix: [workspace]show editor issue
    click one of multi selected items should not show rename editor
    
    Log: fix rename editor issue
    Bug: https://pms.uniontech.com/bug-view-219275.html

--- src/plugins/filemanager/core/dfmplugin-workspace/views/fileview.cpp
+++ src/plugins/filemanager/core/dfmplugin-workspace/views/fileview.cpp
@@ -920,6 +920,9 @@ void FileView::onHeaderViewSectionChanged(const QUrl &url)
 
 bool FileView::edit(const QModelIndex &index, QAbstractItemView::EditTrigger trigger, QEvent *event)
 {
+    if (selectedIndexCount() > 1)
+            return false;
+
     return DListView::edit(index, trigger, event);
 }
 
