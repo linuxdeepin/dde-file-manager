202a626869caca9ea254eb7340a41054713fb9bc
Zhang Yu <zhangyub@uniontech.com>
2023-10-16 11:30:08 +0800

    revert: remove dcc plugin
    move dcc plugin to dde-file-manager-external.

--- src/external/dde-control-center-plugins/wallpapersetting/lupdate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# SPDX-FileCopyrightText: 2022 - 2023 UnionTech Software Technology Co., Ltd.
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-lupdate ./ -ts -no-obsolete translations/*.ts
