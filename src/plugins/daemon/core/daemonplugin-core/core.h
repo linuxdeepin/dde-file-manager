542adb76de22ccf585b0be0a5de946e1741e12b5
xust <xushitong@uniontech.com>
2023-11-07 10:46:17 +0800

    feat: [daemon] add public slot event
    add polkit checkAuthorization function as the core event.
    add hook to acquire the device password.
    
    Log: add core event.

--- src/plugins/daemon/core/daemonplugin-core/core.h
+++ src/plugins/daemon/core/daemonplugin-core/core.h
@@ -16,9 +16,16 @@ class Core : public DPF_NAMESPACE::Plugin
     Q_OBJECT
     Q_PLUGIN_METADATA(IID "org.deepin.plugin.daemon" FILE "core.json")
 
+    DPF_EVENT_NAMESPACE(DAEMONPCORE_NAMESPACE)
+    // slots
+    DPF_EVENT_REG_SLOT(slot_Polkit_CheckAuth)
+
 public:
     virtual void initialize() override;
     virtual bool start() override;
+
+protected:
+    void bindEvents();
 };
 
 DAEMONPCORE_END_NAMESPACE
