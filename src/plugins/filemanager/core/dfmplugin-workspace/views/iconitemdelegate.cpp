832d3ecad16b9e516bd5808d11471fcaed12dc53
liyigang <liyigang1@uniontech.com>
2023-12-20 16:13:50 +0800

    fix: There are files with long file names in the safe, and the file management is stuck when renaming
    Inconsistent reading string length and setting whether it is a long file name
    
    Log: There are files with long file names in the safe, and the file management is stuck when renaming
    Bug: https://pms.uniontech.com/bug-view-234987.html

--- src/plugins/filemanager/core/dfmplugin-workspace/views/iconitemdelegate.cpp
+++ src/plugins/filemanager/core/dfmplugin-workspace/views/iconitemdelegate.cpp
@@ -694,9 +694,10 @@ void IconItemDelegate::setEditorData(QWidget *editor, const QModelIndex &index) 
 
     bool showSuffix = Application::instance()->genericAttribute(Application::kShowedFileSuffix).toBool();
     const QString &filePath = index.data(kItemFilePathRole).toString();
+    const QString &filename = index.data(kItemNameRole).toString();
 
     // if file is in dlnfs' path, use char count rather than byte count to limit the filename length
-    if (DeviceUtils::isSubpathOfDlnfs(filePath))
+    if (DeviceUtils::isSubpathOfDlnfs(filePath) || filename.toLocal8Bit().length() > NAME_MAX)
         item->setCharCountLimit();
 
     QString suffix = index.data(kItemFileSuffixOfRenameRole).toString();
