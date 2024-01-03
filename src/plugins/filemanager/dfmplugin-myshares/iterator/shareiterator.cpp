9fe2bafb1c50fd777853d3a5c51cb510d8409d55
liuyangming <liuyangming@uniontech.com>
2024-01-03 18:19:06 +0800

    fix: [workspace]tree view issue
    support expand file item in my-share view.
    
    Log: fix tree view issue
    Bug: https://pms.uniontech.com/bug-view-235073.html

--- src/plugins/filemanager/dfmplugin-myshares/iterator/shareiterator.cpp
+++ src/plugins/filemanager/dfmplugin-myshares/iterator/shareiterator.cpp
@@ -9,6 +9,8 @@
 
 #include <dfm-base/base/urlroute.h>
 #include <dfm-base/base/schemefactory.h>
+#include <dfm-base/utils/universalutils.h>
+#include <dfm-base/file/local/localdiriterator.h>
 
 #include <dfm-framework/event/event.h>
 
@@ -17,8 +19,10 @@ DFMBASE_USE_NAMESPACE
 
 ShareIterator::ShareIterator(const QUrl &url, const QStringList &nameFilters, QDir::Filters filters, QDirIterator::IteratorFlags flags)
     : AbstractDirIterator(url, nameFilters, filters, flags),
-      d(new ShareIteratorPrivate(this))
+      d(new ShareIteratorPrivate(this, url))
 {
+    if (!UniversalUtils::urlEquals(url, ShareUtils::rootUrl()))
+        d->proxy = new LocalDirIterator(ShareUtils::convertToLocalUrl(url), nameFilters, filters, flags);
 }
 
 ShareIterator::~ShareIterator()
@@ -27,24 +31,38 @@ ShareIterator::~ShareIterator()
 
 QUrl ShareIterator::next()
 {
+    if (d->proxy)
+        return ShareUtils::makeShareUrl(d->proxy->next().path());
+
     if (d->shares.isEmpty())
         return {};
+
     d->currentInfo = d->shares.takeFirst();
+
     return fileUrl();
 }
 
 bool ShareIterator::hasNext() const
 {
+    if (d->proxy)
+        return d->proxy->hasNext();
+
     return !d->shares.isEmpty();
 }
 
 QString ShareIterator::fileName() const
 {
+    if (d->proxy)
+        return d->proxy->fileName();
+
     return d->currentInfo.value(ShareInfoKeys::kName).toString();
 }
 
 QUrl ShareIterator::fileUrl() const
 {
+    if (d->proxy)
+        return ShareUtils::makeShareUrl(d->proxy->fileUrl().path());
+
     return ShareUtils::makeShareUrl(d->currentInfo.value(ShareInfoKeys::kPath).toString());
 }
 
@@ -55,13 +73,17 @@ const FileInfoPointer ShareIterator::fileInfo() const
 
 QUrl ShareIterator::url() const
 {
+    if (d->rootUrl.isValid())
+        return d->rootUrl;
+
     return ShareUtils::rootUrl();
 }
 
-ShareIteratorPrivate::ShareIteratorPrivate(ShareIterator *qq)
+ShareIteratorPrivate::ShareIteratorPrivate(ShareIterator *qq, const QUrl &url)
     : q(qq)
 {
     shares = dpfSlotChannel->push("dfmplugin_dirshare", "slot_Share_AllShareInfos").value<QList<QVariantMap>>();
+    rootUrl = url;
 }
 
 ShareIteratorPrivate::~ShareIteratorPrivate()
