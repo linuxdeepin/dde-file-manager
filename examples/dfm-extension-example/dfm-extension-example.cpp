bb2e75e926ae73a1c9771ea5b9a5c2ca7a58e0cd
Zhang Sheng <zhangsheng@uniontech.com>
2024-01-29 18:08:02 +0800

    chore: [example] update example macro
    update example macro
    
    Log: update example macro

--- examples/dfm-extension-example/dfm-extension-example.cpp
+++ examples/dfm-extension-example/dfm-extension-example.cpp
@@ -4,18 +4,29 @@
 #include "myemblemiconplugin.h"
 #include "mywindowplugin.h"
 
+#ifdef DFMEXT_VERSION
+#    if (DFMEXT_VERSION >= DFMEXT_VERSION_CHECK(6, 0, 0))
+#        define NEW_VERSION
+#    endif
+#endif
+
 // 右键菜单的扩展
 static DFMEXT::DFMExtMenuPlugin *myMenu { nullptr };
 // 角标的扩展
 static DFMEXT::DFMExtEmblemIconPlugin *myEmblemIcon { nullptr };
+
+#ifdef NEW_VERSION
 // 窗口的扩展
 static DFMEXT::DFMExtWindowPlugin *myWindow { nullptr };
+#endif
 
 extern "C" void dfm_extension_initiliaze()
 {
     myMenu = new Exapmle::MyMenuPlugin;
     myEmblemIcon = new Exapmle::MyEmblemIconPlugin;
+#ifdef NEW_VERSION
     myWindow = new Exapmle::MyWindowPlugin;
+#endif
 }
 
 extern "C" void dfm_extension_shutdown()
@@ -35,11 +46,9 @@ extern "C" DFMEXT::DFMExtEmblemIconPlugin *dfm_extension_emblem()
     return myEmblemIcon;
 }
 
-#ifdef DFMEXT_VERSION
-#    if (DFMEXT_VERSION >= DFMEXT_VERSION_CHECK(6, 0, 0))
+#ifdef NEW_VERSION
 extern "C" DFMEXT::DFMExtWindowPlugin *dfm_extension_window()
 {
     return myWindow;
 }
-#    endif
 #endif
