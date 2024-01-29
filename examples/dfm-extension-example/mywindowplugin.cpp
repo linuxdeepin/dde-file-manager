723b26e7fb2474115e66530a622c977afb33731a
Zhang Sheng <zhangsheng@uniontech.com>
2024-01-29 16:41:15 +0800

    chore: [example] update example for `DFMExtMenuPlugin`
    update example for `DFMExtMenuPlugin`
    
    Log:  update example for `DFMExtMenuPlugin`

---
+++ examples/dfm-extension-example/mywindowplugin.cpp
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "mywindowplugin.h"
+
+#include <iostream>
+
+namespace Exapmle {
+
+MyWindowPlugin::MyWindowPlugin()
+{
+    registerWindowOpened([this](uint64_t winId) {
+        windowOpened(winId);
+    });
+    registerWindowClosed([this](uint64_t winId) {
+        windowClosed(winId);
+    });
+    registerFirstWindowOpened([this](uint64_t winId) {
+        firstWindowOpened(winId);
+    });
+    registerLastWindowClosed([this](uint64_t winId) {
+        lastWindowClosed(winId);
+    });
+    registerWindowUrlChanged([this](uint64_t winId, const std::string &urlString) {
+        windowUrlChanged(winId, urlString);
+    });
+}
+
+void MyWindowPlugin::windowOpened(uint64_t winId)
+{
+    std::cerr << "-->Ext:" << __PRETTY_FUNCTION__ << winId << std::endl;
+}
+
+void MyWindowPlugin::windowClosed(uint64_t winId)
+{
+    std::cerr << "-->Ext:" << __PRETTY_FUNCTION__ << winId << std::endl;
+}
+
+void MyWindowPlugin::firstWindowOpened(uint64_t winId)
+{
+    std::cerr << "-->Ext:" << __PRETTY_FUNCTION__ << winId << std::endl;
+}
+
+void MyWindowPlugin::lastWindowClosed(uint64_t winId)
+{
+    std::cerr << "-->Ext:" << __PRETTY_FUNCTION__ << winId << std::endl;
+}
+
+void MyWindowPlugin::windowUrlChanged(uint64_t winId, const std::string &urlString)
+{
+    std::cerr << "-->Ext:" << __PRETTY_FUNCTION__ << winId << urlString << std::endl;
+}
+
+}   // namespace Exapmle
