e9ede613946831d63f6f5a8f2b857c5235d77ef8
Zhang Sheng <zhangsheng@uniontech.com>
2024-01-29 16:41:15 +0800

    chore: [example] update example for `DFMExtMenuPlugin`
    update example for `DFMExtMenuPlugin`
    
    Log:  update example for `DFMExtMenuPlugin`

--- examples/dfm-extension-example/dfm-extension-example.cpp
+++ examples/dfm-extension-example/dfm-extension-example.cpp
@@ -2,22 +2,27 @@
 
 #include "mymenuplugin.h"
 #include "myemblemiconplugin.h"
+#include "mywindowplugin.h"
 
 // 右键菜单的扩展
 static DFMEXT::DFMExtMenuPlugin *myMenu { nullptr };
 // 角标的扩展
 static DFMEXT::DFMExtEmblemIconPlugin *myEmblemIcon { nullptr };
+// 窗口的扩展
+static DFMEXT::DFMExtWindowPlugin *myWindow { nullptr };
 
 extern "C" void dfm_extension_initiliaze()
 {
     myMenu = new Exapmle::MyMenuPlugin;
     myEmblemIcon = new Exapmle::MyEmblemIconPlugin;
+    myWindow = new Exapmle::MyWindowPlugin;
 }
 
 extern "C" void dfm_extension_shutdown()
 {
     delete myMenu;
     delete myEmblemIcon;
+    delete myWindow;
 }
 
 extern "C" DFMEXT::DFMExtMenuPlugin *dfm_extension_menu()
@@ -29,3 +34,12 @@ extern "C" DFMEXT::DFMExtEmblemIconPlugin *dfm_extension_emblem()
 {
     return myEmblemIcon;
 }
+
+#ifdef DFMEXT_VERSION
+#    if (DFMEXT_VERSION >= DFMEXT_VERSION_CHECK(6, 0, 0))
+extern "C" DFMEXT::DFMExtWindowPlugin *dfm_extension_window()
+{
+    return myWindow;
+}
+#    endif
+#endif
---
+++ examples/dfm-extension-example/mywindowplugin.h
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef MYWINDOWPLUGIN_H
+#define MYWINDOWPLUGIN_H
+
+#include <dfm-extension/window/dfmextwindowplugin.h>
+
+namespace Exapmle {
+
+class MyWindowPlugin : public DFMEXT::DFMExtWindowPlugin
+{
+public:
+    MyWindowPlugin();
+
+    void windowOpened(std::uint64_t winId) DFM_FAKE_OVERRIDE;
+    void windowClosed(std::uint64_t winId) DFM_FAKE_OVERRIDE;
+    void firstWindowOpened(std::uint64_t winId) DFM_FAKE_OVERRIDE;
+    void lastWindowClosed(std::uint64_t winId) DFM_FAKE_OVERRIDE;
+    void windowUrlChanged(std::uint64_t winId, const std::string &urlString) DFM_FAKE_OVERRIDE;
+};
+
+}   // namespace Exapmle
+
+#endif   // MYWINDOWPLUGIN_H
