7993006670d4f9b590dfa57f6f8dca355033250e
Gong Heng <gongheng@uniontech.com>
2024-02-29 16:08:58 +0800

    fix: [vault] hide file invalid in vault
    1. When first create .hidden file, hide file invalid.
    2. change the create logic to update the .hidden file.
    
    Log: fix issue
    Bug: https://pms.uniontech.com/bug-view-244729.html

--- src/plugins/filemanager/dfmplugin-vault/events/vaulteventreceiver.h
+++ src/plugins/filemanager/dfmplugin-vault/events/vaulteventreceiver.h
@@ -27,7 +27,6 @@ public slots:
     void handleCurrentUrlChanged(const quint64 &winId, const QUrl &url);
     bool handleSideBarItemDragMoveData(const QList<QUrl> &urls, const QUrl &url, Qt::DropAction *action);
     bool handleShortCutPasteFiles(const quint64 &winId, const QList<QUrl> &fromUrls, const QUrl &to);
-    void handleHideFilesResult(const quint64 &winId, const QList<QUrl> &urls, bool ok);
     bool changeUrlEventFilter(quint64 windowId, const QUrl &url);
     bool handlePathtoVirtual(const QList<QUrl> files, QList<QUrl> *virtualFiles);
     bool detailViewIcon(const QUrl &url, QString *iconName);
