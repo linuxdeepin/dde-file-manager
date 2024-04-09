5af6f8967e3874fb9cbe233a40174487dfdc5720
Gong Heng <gongheng@uniontech.com>
2024-04-09 16:52:11 +0800

    fix: [extension-menu] The interface of insertAction() is not valid
    1. add the logic of insertAction() function.
    
    Log: fix issue
    Bug: https://pms.uniontech.com/bug-view-249329.html

---
+++ src/plugins/common/dfmplugin-utils/extensionimpl/menuimpl/dfmextmenucache.h
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2022 - 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef DFMEXTMENUCACHE_H
+#define DFMEXTMENUCACHE_H
+
+#include "dfmplugin_utils_global.h"
+
+#include <QAction>
+
+namespace dfmplugin_utils {
+
+class DFMExtMenuCache : public QObject
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(DFMExtMenuCache)
+
+public:
+    static DFMExtMenuCache &instance();
+    QList<QPair<QAction *, QAction *>> extMenuSortRules {};
+
+private:
+    DFMExtMenuCache();
+};
+
+}
+
+#endif   // DFMEXTMENUCACHE_H
