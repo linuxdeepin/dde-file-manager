5329047ec9deb79f9337d7ca9bc3dff8f8b980f5
Zhang Sheng <zhangsheng@uniontech.com>
2024-04-29 13:19:44 +0800

    feat: [ui] add border for thumbnail icon
    add border for thumbnail icon
    
    Log: add border for thumbnail icon
    
    Task: https://pms.uniontech.com/task-view-337513.html

--- src/dfm-base/utils/iconutils.h
+++ src/dfm-base/utils/iconutils.h
@@ -1,13 +1,21 @@
-// SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2023 - 2024 UnionTech Software Technology Co., Ltd.
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #ifndef ICONUTILS_H
 #define ICONUTILS_H
 
-#define CORNER_MARK_LINK QIcon::fromTheme("emblem-symbolic-link"));
-#define CORNER_MARK_LOCKED QIcon::fromTheme("emblem-locked");
-#define CORNER_MARK_UNREADED QIcon::fromTheme("emblem-unreadable");
-#define CORNER_MARK_SHARED QIcon::fromTheme("emblem-shared"));
+#include <dfm-base/dfm_base_global.h>
 
-#endif // ICONUTILS_H
+#include <QPixmap>
+
+DFMBASE_BEGIN_NAMESPACE
+
+namespace IconUtils {
+QPixmap renderIconBackground(const QSize &size);
+QPixmap addShadowToPixmap(const QPixmap &originalPixmap, int shadowOffsetY, qreal blurRadius, qreal shadowOpacity);
+}   // end namespace IconUtils
+
+DFMBASE_END_NAMESPACE
+
+#endif   // ICONUTILS_H
