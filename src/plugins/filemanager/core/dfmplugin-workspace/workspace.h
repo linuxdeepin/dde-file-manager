c681a4926516389f6bb2e52c16f317a59c76b4c1
liyigang <liyigang1@uniontech.com>
2024-05-24 10:04:16 +0800

    fix: After checking the box, the document management selection window will not be able to enter any directory when a new window is opened
    Set filedailog to only open in the current window
    
    Log: After checking the box, the document management selection window will not be able to enter any directory when a new window is opened
    Bug: https://pms.uniontech.com/bug-view-255979.html

--- src/plugins/filemanager/core/dfmplugin-workspace/workspace.h
+++ src/plugins/filemanager/core/dfmplugin-workspace/workspace.h
@@ -66,6 +66,7 @@ class Workspace : public dpf::Plugin
     DPF_EVENT_REG_SLOT(slot_View_SetReadOnly)
     DPF_EVENT_REG_SLOT(slot_View_SetFilter)
     DPF_EVENT_REG_SLOT(slot_View_GetFilter)
+    DPF_EVENT_REG_SLOT(slot_View_SetAlwaysOpenInCurrentWindow)
 
     DPF_EVENT_REG_SLOT(slot_Model_SetCustomFilterData)
     DPF_EVENT_REG_SLOT(slot_Model_SetCustomFilterCallback)
