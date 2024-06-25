05dc94fa647912a43696e2e5e500107e9c406df3
liyigang <liyigang1@uniontech.com>
2024-06-25 13:49:43 +0800

    fix: Unable to drag files to disc
    Determine if there is a drop error
    
    Log: Unable to drag files to disc
    Bug: https://pms.uniontech.com/bug-view-260801.html

--- src/plugins/filemanager/dfmplugin-optical/mastered/masteredmediafileinfo.cpp
+++ src/plugins/filemanager/dfmplugin-optical/mastered/masteredmediafileinfo.cpp
@@ -171,9 +171,19 @@ bool MasteredMediaFileInfo::canAttributes(const CanableInfoType type) const
 
 Qt::DropActions MasteredMediaFileInfo::supportedOfAttributes(const SupportedType type) const
 {
-    if (type == SupportType::kDrop)
+    if (type == SupportType::kDrop) {
         if (!OpticalHelper::isBurnEnabled())
             return Qt::IgnoreAction;
+
+        if (isAttributes(OptInfoType::kIsWritable)) {
+            return Qt::CopyAction | Qt::MoveAction | Qt::LinkAction;
+        }
+
+        if (d->canDrop()) {
+            return Qt::CopyAction | Qt::MoveAction;
+        }
+        return Qt::IgnoreAction;
+    }
     return ProxyFileInfo::supportedOfAttributes(type);
 }
 
