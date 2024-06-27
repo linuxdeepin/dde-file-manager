09fed192f0fa5908f07fb32d1bc08e19c5a0f7d0
jiangyangyang <jiangyangyang@uniontech.com>
2024-06-27 20:41:51 +0800

    fix: Fix the issue of permissions not being able to take effect properly
    The permissions set need to be restarted in the document management system to take effect
    
    Log: Correctly obtaining file permissions by refreshing file information
    
    Bug: https://pms.uniontech.com/bug-view-259897.html

--- src/plugins/filemanager/core/dfmplugin-workspace/views/fileview.cpp
+++ src/plugins/filemanager/core/dfmplugin-workspace/views/fileview.cpp
@@ -1586,7 +1586,9 @@ void FileView::contextMenuEvent(QContextMenuEvent *event)
 
             selectionModel()->select(index, QItemSelectionModel::Select);
         }
-
+        auto info = model()->fileInfo(index);
+        if (info)
+            info->updateAttributes();
         d->viewMenuHelper->showNormalMenu(index, model()->flags(index));
     }
 }
