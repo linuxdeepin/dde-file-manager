ce616e8d59f2aa93d955618d7c291591aa203707
xust <xushitong@uniontech.com>
2024-07-15 16:35:59 +0800

    feat: [desktop] add upgrade unit.
    as title.
    
    Log: as title.

---
+++ src/tools/upgrade/units/desktoporganizeupgradeunit.h
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+#ifndef DESKTOPORGANIZEUPGRADEUNIT_H
+#define DESKTOPORGANIZEUPGRADEUNIT_H
+
+#include "core/upgradeunit.h"
+
+#include <dfm-base/dfm_log_defines.h>
+
+namespace dfm_upgrade {
+class DesktopOrganizeUpgradeUnit : public UpgradeUnit
+{
+public:
+    DesktopOrganizeUpgradeUnit();
+
+    virtual QString name() override;
+    virtual bool initialize(const QMap<QString, QString> &args) override;
+    virtual bool upgrade() override;
+
+private:
+    QString cfgPath;
+};
+}
+
+#endif   // DESKTOPORGANIZEUPGRADEUNIT_H
