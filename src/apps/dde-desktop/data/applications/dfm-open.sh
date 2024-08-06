65b638fadb8ca6bf78809bead0db4fc2a80429cf
Zhang Sheng <zhangsheng@uniontech.com>
2024-08-06 10:14:24 +0800

    refacotr: update v23 os-vresion
    The system version number will continue to evolve, the new version of this DFM do not have to consider the compatibility of v20
    
    log: as title

--- src/apps/dde-desktop/data/applications/dfm-open.sh
+++ src/apps/dde-desktop/data/applications/dfm-open.sh
@@ -4,7 +4,6 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-sys_name=$(grep "^SystemName=" /etc/os-version | cut -d'=' -f2)
 sys_ver=$(grep "^MajorVersion=" /etc/os-version | cut -d'=' -f2)
 
 # this is for adapt the new ApplicationManager.
@@ -12,15 +11,15 @@ sys_ver=$(grep "^MajorVersion=" /etc/os-version | cut -d'=' -f2)
 # the cgroups of the opened app is not belongs to dde-file-manager,
 # which cause the opened apps in dock panel is not dde-file-manager,
 # and the new opened dde-file-manager window also wrapped into trash/computer entry.
-# the 'xdg-open' in V23 accept the computer/trash scheme,
+# the 'xdg-open' >= V23 accept the computer/trash scheme,
 # So, after opening a directory with xdg-open,
 # the application's cgroup can be categorized into the file manager as expected.
 
-if [ "$sys_name" = "Deepin" ] && [ "$sys_ver" = "23" ]; then
+if [ "$sys_ver" -ge "23" ]; then
 	echo "open $1 with xdg-open."
-	xdg-open $1
+	xdg-open "$1"
 else
 	echo "open $1 with gio open."
-	gio open $1
+	gio open "$1"
 fi
 
