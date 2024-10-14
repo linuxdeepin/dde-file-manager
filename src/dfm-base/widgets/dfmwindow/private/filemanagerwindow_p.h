f554bd49c4b299bae458ca892f75c621a2b25503
Zhang Sheng <zhangsheng@uniontech.com>
2024-10-14 21:14:13 +0800

    feat: window layout to detailspace
    1. fix sidebar sise changed when resize window
    2. change detailspace layout
    
    Log: window layout to detailspace

--- src/dfm-base/widgets/dfmwindow/private/filemanagerwindow_p.h
+++ src/dfm-base/widgets/dfmwindow/private/filemanagerwindow_p.h
@@ -46,14 +46,17 @@ protected:
     static constexpr int kMinimumWindowHeight { 300 };
     static constexpr int kDefaultWindowWidth { 1100 };
     static constexpr int kDefaultWindowHeight { 700 };
-    static constexpr int kMinimumLeftWidth { 40 };
+    static constexpr int kMinimumLeftWidth { 80 };
     static constexpr int kMaximumLeftWidth { 600 };
     static constexpr int kDefaultLeftWidth { 200 };
 
     QFrame *centralView { nullptr };   // Central area (all except sidebar)
     QFrame *rightArea { nullptr };
+
     QHBoxLayout *midLayout { nullptr };
     QVBoxLayout *rightLayout { nullptr };
+    QHBoxLayout *rightBottomLayout { nullptr };
+
     Splitter *splitter { nullptr };
     AbstractFrame *titleBar { nullptr };
     AbstractFrame *sideBar { nullptr };
