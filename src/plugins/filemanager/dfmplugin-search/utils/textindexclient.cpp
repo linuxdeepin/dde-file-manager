06dd9db5160c6a3ac87e74ec136a467bc8e3a79f
Zhang Sheng <zhangsheng@uniontech.com>
2024-11-29 15:00:00 +0800

    chore: update license headers and file paths
    - Add missing SPDX license headers to TextIndexClient files
    - Update 3rdparty dependency paths in dep5 file
    - Move fulltext and fsearch libraries to root 3rdparty directory
    - Fix copyright information formatting
    
    This change standardizes license headers and reorganizes 3rdparty
    dependency locations for better project structure.
    
    Log: License

--- src/plugins/filemanager/dfmplugin-search/utils/textindexclient.cpp
+++ src/plugins/filemanager/dfmplugin-search/utils/textindexclient.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "textindexclient.h"
 #include "textindex_interface.h"
 
--- src/plugins/filemanager/dfmplugin-search/utils/textindexclient.h
+++ src/plugins/filemanager/dfmplugin-search/utils/textindexclient.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #ifndef TEXTINDEXCLIENT_H
 #define TEXTINDEXCLIENT_H
 
