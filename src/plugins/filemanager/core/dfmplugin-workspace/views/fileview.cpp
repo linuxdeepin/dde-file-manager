7ff2a0b3a5650c41882b46185ac87fef37a52269
Lighto-Ku <liuyangming@uniontech.com>
2024-12-05 15:39:30 +0800

    fix: adjust scroll behavior in different view modes
    1. Calculate correct content height for list/tree mode to ensure proper scrolling
    
    Log: fix list view issue
    Bug: https://pms.uniontech.com/bug-view-291285.html

--- src/plugins/filemanager/core/dfmplugin-workspace/views/fileview.cpp
+++ src/plugins/filemanager/core/dfmplugin-workspace/views/fileview.cpp
@@ -1557,6 +1557,7 @@ QList<ItemRoles> FileView::getColumnRoles() const
 
 void FileView::updateGeometries()
 {
+    int totalHeight = 0;
     if (isIconViewMode()) {
         int iconVerticalTopMargin = 0;
 #ifdef DTKWIDGET_CLASS_DSizeMode
@@ -1565,13 +1566,21 @@ void FileView::updateGeometries()
         if (!d->isResizeEvent
                 || (d->isResizeEvent && d->lastContentHeight > 0 && d->lastContentHeight != contentsSize().height()))
             resizeContents(contentsSize().width(), contentsSize().height() + iconVerticalTopMargin);
-        d->lastContentHeight = contentsSize().height();
+
+        totalHeight = contentsSize().height();
+        d->lastContentHeight = totalHeight;
+    } else {
+        int rowCount = model()->rowCount(rootIndex());
+        int listHeight = rowCount * itemSizeHint().height() + kListModeBottomMargin;
+        int contentHeight = contentsSize().height();
+        totalHeight = listHeight > contentHeight ? listHeight : contentHeight;
     }
+
     if (!d->headerView || !d->allowedAdjustColumnSize) {
         return DListView::updateGeometries();
     }
 
-    resizeContents(d->headerView->length(), contentsSize().height());
+    resizeContents(d->headerView->length(), totalHeight);
     DListView::updateGeometries();
 }
 
