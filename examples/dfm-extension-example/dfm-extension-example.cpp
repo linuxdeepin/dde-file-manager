3ed63462510ed7231d9da9214cea9ac78833bb6e
Zhang Sheng <zhangsheng@uniontech.com>
2025-02-11 14:20:20 +0800

    chore: update License
    use LGPG for dfm-extension
    
    Log:

--- examples/dfm-extension-example/dfm-extension-example.cpp
+++ examples/dfm-extension-example/dfm-extension-example.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 ~ 2025 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 #include <dfm-extension/dfm-extension.h>
 
 #include "mymenuplugin.h"
--- src/dfm-extension/menu/private/dfmextmenuprivate.cpp
+++ src/dfm-extension/menu/private/dfmextmenuprivate.cpp
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2021 - 2023 UnionTech Software Technology Co., Ltd.
 //
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 
 #include "dfmextmenuprivate.h"
 
@@ -8,10 +8,8 @@ USING_DFMEXT_NAMESPACE
 
 DFMExtMenuPrivate::DFMExtMenuPrivate()
 {
-
 }
 
 DFMExtMenuPrivate::~DFMExtMenuPrivate()
 {
-
 }
