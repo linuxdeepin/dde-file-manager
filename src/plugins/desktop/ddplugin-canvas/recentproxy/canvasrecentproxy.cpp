405d588faf7fec905c9063a0ba229441d8a6c2ac
dengzhongyuan <dengzhongyuan@uniontech.com>
2025-04-10 14:34:05 +0800

    fix: Add recent file proxy to enhance file management functionality
    - Introduce CanvasRecentProxy in CanvasManager to support recent files management.
    - Subscribe to events related to file deletion and moving to trash, to update the recent files list in real-time.
    
    bug: https://pms.uniontech.com/bug-view-309675.html

---
+++ src/plugins/desktop/ddplugin-canvas/recentproxy/canvasrecentproxy.cpp
@@ -0,0 +1,32 @@
+#include "canvasrecentproxy.h"
+#include <QDBusMessage>
+#include <QDBusConnection>
+#include <QDBusError>
+#include <QVariant>
+#include <QDBusPendingCall>
+#include <QUrl>
+
+namespace ddplugin_canvas {
+
+void CanvasRecentProxy::handleReloadRecentFiles(const QList<QUrl> &srcUrls, bool ok, const QString &errMsg)
+{
+    Q_UNUSED(errMsg);
+    if (!ok || !srcUrls.isEmpty())
+        return;
+
+    QDBusMessage message = QDBusMessage::createMethodCall("org.deepin.Filemanager.Daemon",
+                                                          "/org/deepin/Filemanager/Daemon/RecentManager",
+                                                          "org.deepin.Filemanager.Daemon.RecentManager",
+                                                          "Reload");
+    QDBusConnection::sessionBus().asyncCall(message);
+}
+
+CanvasRecentProxy::CanvasRecentProxy(QObject *parent)
+    : QObject(parent)
+{
+}
+
+CanvasRecentProxy::~CanvasRecentProxy()
+{
+}
+} // namespace ddplugin_canvas
---
+++ src/plugins/desktop/ddplugin-canvas/recentproxy/canvasrecentproxy.h
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2022 - 2023 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef CANVASRECENTPROXY_H
+#define CANVASRECENTPROXY_H
+
+#include <QObject>
+
+namespace ddplugin_canvas {
+
+class CanvasRecentProxy : public QObject
+{
+    Q_OBJECT
+public:
+    explicit CanvasRecentProxy(QObject *parent = nullptr);
+    ~CanvasRecentProxy() override;
+
+public slots:
+    void handleReloadRecentFiles(const QList<QUrl> &srcUrls, bool ok, const QString &errMsg);
+};
+
+} // namespace ddplugin_canvas
+
+#endif // CANVASRECENTPROXY_H
