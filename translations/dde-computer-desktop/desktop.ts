c0abb70911a0e1264ec99b32d983220742baa2b2
golf66 <69233028+hudeng-go@users.noreply.github.com>
2025-04-17 16:45:19 +0800

    Update desktop.ts
    Signed-off-by: golf66 <69233028+hudeng-go@users.noreply.github.com>

--- translations/dde-computer-desktop/desktop.ts
+++ translations/dde-computer-desktop/desktop.ts
@@ -1,3 +1,4 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1"><context><name>desktop</name><message><location filename="Desktop Entry]GenericName" line="0"/><location filename="Desktop Entry]Name" line="0"/><source>Computer</source><translation type="unfinished"/></message><message><location filename="Desktop Entry]Comment" line="0"/><source>Show basic info of the computer.</source><translation type="unfinished"/></message></context></TS>
+  
