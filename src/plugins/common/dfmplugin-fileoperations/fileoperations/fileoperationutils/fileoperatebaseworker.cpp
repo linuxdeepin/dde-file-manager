c9d645e7a0237229fbbc6605599a92680d6cea1b
Liu Zhangjian <liuzhangjian@uniontech.com>
2025-05-15 14:14:17 +0800

    fix: add delayed removal of copying file URLs via OperatorsFileUtils
    - Introduced OperatorsFileUtils singleton class to handle delayed removal of copying file URLs.
    - Replaced direct calls to FileUtils::removeCopyingFileUrl with OperatorsFileUtils::delayRemoveCopyingFile in FileOperateBaseWorker.
    - Utilized QTimer to delay the removal by 500ms, ensuring safer file operation handling.
    
    Log: add delayed removal of copying file URLs via OperatorsFileUtils
    Bug: https://pms.uniontech.com/bug-view-310043.html

--- src/plugins/common/dfmplugin-fileoperations/fileoperations/fileoperationutils/fileoperatebaseworker.cpp
+++ src/plugins/common/dfmplugin-fileoperations/fileoperations/fileoperationutils/fileoperatebaseworker.cpp
@@ -378,7 +378,7 @@ bool FileOperateBaseWorker::copyAndDeleteFile(const DFileInfoPointer &fromInfo, 
         }
         if (ok)
             cutAndDeleteFiles.append(fromInfo);
-        FileUtils::removeCopyingFileUrl(url);
+        OperatorsFileUtils::instance()->delayRemoveCopyingFile(url);
     }
 
     toInfo->initQuerier();
@@ -910,7 +910,7 @@ bool FileOperateBaseWorker::doCopyLocalByRange(const DFileInfoPointer fromInfo, 
 
     FileUtils::cacheCopyingFileUrl(targetUrl);
     DoCopyFileWorker::NextDo nextDo = copyOtherFileWorker->doCopyFileByRange(fromInfo, toInfo, skip);
-    FileUtils::removeCopyingFileUrl(targetUrl);
+    OperatorsFileUtils::instance()->delayRemoveCopyingFile(targetUrl);
     return nextDo == DoCopyFileWorker::NextDo::kDoCopyNext;
 }
 
@@ -938,7 +938,7 @@ bool FileOperateBaseWorker::doCopyOtherFile(const DFileInfoPointer fromInfo, con
     }
     if (ok)
         syncFiles.append(targetUrl);
-    FileUtils::removeCopyingFileUrl(targetUrl);
+    OperatorsFileUtils::instance()->delayRemoveCopyingFile(targetUrl);
 
     return ok;
 }
