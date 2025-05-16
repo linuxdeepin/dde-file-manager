4b450e5c56eb56805485223981151e6f9eb78b9c
xushitong <xushitong@uniontech.com>
2025-05-16 14:33:55 +0800

    feat: add AppImage thumbnail support and update related logic
    - Introduced a new function `appimageThumbnailCreator` to handle the creation of thumbnails for AppImage files.
    - Updated `ThumbnailFactory` to register the new AppImage thumbnail creator.
    - Enhanced `ThumbnailHelper` to support AppImage MIME type and size limits.
    - Added necessary logging for better debugging and error handling during AppImage processing.
    
    This change improves the application's ability to generate thumbnails for AppImage files, enhancing user experience with better visual representation of these file types.
    
    Log: add AppImage thumbnail support and update related logic

--- src/plugins/desktop/ddplugin-organizer/delegate/collectionitemdelegate.cpp
+++ src/plugins/desktop/ddplugin-organizer/delegate/collectionitemdelegate.cpp
@@ -424,6 +424,10 @@ bool CollectionItemDelegate::isThumnailIconIndex(const QModelIndex &index) const
 
     FileInfoPointer info { parent()->model()->fileInfo(index) };
     if (info) {
+        // appimage 不显示缩略图底板
+        if (info->nameOf(NameInfoType::kMimeTypeName) == Global::Mime::kTypeAppAppimage)
+            return false;
+
         const auto &attribute { info->extendAttributes(ExtInfoType::kFileThumbnail) };
         if (attribute.isValid() && !attribute.value<QIcon>().isNull())
             return true;
