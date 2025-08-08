a12134324af5eacb78ab2db145252eecc8d40747
Tian Shilin <tianshilin@uniontech.com>
2025-08-08 15:22:07 +0800

    fix: update translations of lo
    log:

---
+++ translations/dde-home-desktop/desktop_lo.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>desktop</name>
+    <message>
+        <location filename="Desktop Entry]GenericName" line="0"/>
+        <location filename="Desktop Entry]Name" line="0"/>
+        <source>Home</source>
+        <translation>ໜ້າຫຼັກ</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]Comment" line="0"/>
+        <source>Open home directory</source>
+        <translation>ເປີດໄຟລ໌ໜ້າຫຼັກ</translation>
+    </message>
+</context>
+</TS>
