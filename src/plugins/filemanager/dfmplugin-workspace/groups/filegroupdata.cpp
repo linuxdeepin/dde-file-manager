4c79ddfae091548acc7d8ed4e727e09769a0709c
Zhang Sheng <zhangsheng@uniontech.com>
2025-11-03 08:53:55 +0800

    chore: add LICENSE to filegroupdata.cpp
    add LICENSE to filegroupdata.cpp
    
    Log:

--- src/plugins/filemanager/dfmplugin-workspace/groups/filegroupdata.cpp
+++ src/plugins/filemanager/dfmplugin-workspace/groups/filegroupdata.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "filegroupdata.h"
 
 #include <dfm-base/dfm_global_defines.h>
