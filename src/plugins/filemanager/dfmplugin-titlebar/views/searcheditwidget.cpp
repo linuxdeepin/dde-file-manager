a718ba7e092ed2257fb33eee6155ba1484b217c4
tianshilin@uniontech.com <tianshilin@uniontech.com>
2026-08-18 15:51:12 +0800

    feat(titlebar): add AT-SPI accessible names for titlebar controls
    Assign object and accessible names to titlebar and address bar
    interactive controls for AT automation.
    
    为标题栏与地址栏交互控件添加稳定的 objectName 与
    accessibleName，便于 AT 自动化定位。
    
    Log: 为标题栏控件添加AT-SPI可访问名称
    Influence: 不影响业务逻辑，仅提升控件的无障碍访问能力。
    Change-Id: Id586d8c57fdbd16b218c3782fab27ad20e491fa7

--- src/plugins/filemanager/dfmplugin-titlebar/views/searcheditwidget.cpp
+++ src/plugins/filemanager/dfmplugin-titlebar/views/searcheditwidget.cpp
@@ -319,6 +319,8 @@ void SearchEditWidget::initUI()
 
     // search button
     searchButton = new CustomDIconButton(this);
+    searchButton->setObjectName("SearchButton");
+    searchButton->setAccessibleName("SearchButton");
     searchButton->setIcon(QIcon::fromTheme("dfm_search_button"));
     searchButton->setFixedSize(kToolButtonSize, kToolButtonSize);
     searchButton->setIconSize(QSize(kToolButtonIconSize, kToolButtonIconSize));
@@ -329,12 +331,16 @@ void SearchEditWidget::initUI()
 
     // search edit
     searchEdit = new DSearchEdit(this);
+    searchEdit->setObjectName("SearchEdit");
+    searchEdit->setAccessibleName("SearchEdit");
     searchEdit->setVisible(true);
     // searchEdit->setFocusPolicy(Qt::StrongFocus);
     searchEdit->lineEdit()->setFocusPolicy(Qt::ClickFocus);
 
     // advanced search button
     advancedButton = new CustomDToolButton(this);
+    advancedButton->setObjectName("AdvancedButton");
+    advancedButton->setAccessibleName("AdvancedButton");
     advancedButton->setIcon(QIcon::fromTheme("dfm_view_filter"));
     advancedButton->setFixedSize(kToolButtonSize, kToolButtonSize);
     advancedButton->setFocusPolicy(Qt::NoFocus);
